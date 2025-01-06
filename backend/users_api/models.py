73e838a8f0d64556be8855bbc50aecee456b1e24
= <=>
2025-01-06 19:47:45 +0800

    Added Email OTP attribute to User model

--- backend/users_api/models.py
+++ backend/users_api/models.py
@@ -7,5 +7,6 @@ class UserAuthModel(AbstractBaseUser):
 	email = models.EmailField(max_length=80)
 
 	email_verified = models.BooleanField()
+	email_otp = models.CharField(max_length=6, null=True, blank=True)
 
 	REQUIRED_FIELDS = [username, email]	
