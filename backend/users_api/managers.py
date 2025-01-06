5b744778c05a4f0b6ba5694bdb5a2af4d8674413
= <=>
2025-01-06 19:46:20 +0800

    Configured settings.py file

--- backend/users_api/managers.py
+++ backend/users_api/managers.py
@@ -4,3 +4,13 @@ class CustomUserManager(BaseUserManager):
 	def create_user(self, username, email, password, **extra_fields):
 		if not email:
 			raise ValueError("Email must be set!")
+		
+		if not username:
+			raise ValueError("Username must be set!")
+		
+		email = self.normalize_email(email)
+		user = self.model(email=email, username=username **extra_fields)
+		user.set_password(password)
+		user.save()
+
+		return user
