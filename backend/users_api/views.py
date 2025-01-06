d19ea05e0091cb2035642f8ac89a19644786e5f2
= <=>
2025-01-06 20:09:27 +0800

    Created user registration view

--- backend/users_api/views.py
+++ backend/users_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+
+from rest_framework import generics
+from rest_framework.permissions import AllowAny
+
+from .serializers import UserSerializer
+
 
 # Create your views here.
+class CreateUserView(generics.CreateAPIView):
+	user = get_user_model()
+
+	queryset = user.objects.all()
+	serializer_class = UserSerializer
+	permission_classes = [AllowAny]
