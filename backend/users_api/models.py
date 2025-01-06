586b08efc07e52e2ef4fa9aa23c10ee664f5bf74
= <=>
2025-01-06 20:27:38 +0800

    Remove required fields

--- backend/users_api/models.py
+++ backend/users_api/models.py
@@ -4,13 +4,13 @@ from .managers import CustomUserManager
 
 # Create your models here.
 class UserAuthModel(AbstractBaseUser):
-	username = models.CharField(max_length=80)
-	email = models.EmailField(max_length=80)
+	username = models.CharField(max_length=80, blank=False)
+	email = models.EmailField(max_length=80, blank=False)
 
 	email_verified = models.BooleanField()
 	email_otp = models.CharField(max_length=6, null=True, blank=True)
 
 	USERNAME_FIELD = email
-	REQUIRED_FIELDS = [username, email]	
+	REQUIRED_FIELDS = []	
 
 	objects = CustomUserManager()
