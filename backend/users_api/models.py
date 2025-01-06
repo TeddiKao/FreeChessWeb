4cbbe328b2c21c3a811e6ccea3bdd0620b2d6991
= <=>
2025-01-06 20:17:40 +0800

    Set email field as unique identifer

--- backend/users_api/models.py
+++ backend/users_api/models.py
@@ -10,6 +10,7 @@ class UserAuthModel(AbstractBaseUser):
 	email_verified = models.BooleanField()
 	email_otp = models.CharField(max_length=6, null=True, blank=True)
 
+	USERNAME_FIELD = email
 	REQUIRED_FIELDS = [username, email]	
 
 	objects = CustomUserManager()
