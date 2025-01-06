a69d87c7c51f7604e22b34c707b368ce5041fd4a
= <=>
2025-01-06 20:16:08 +0800

    Added urls.py file for users_api

--- backend/users_api/urls.py
+++ backend/users_api/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from .views import CreateUserView
 
 urlpatterns = [
-	path("create-user", CreateUserView.as_view(), name="create_user"),
-	path()
+	path("create-user/", CreateUserView.as_view(), name="create_user"),
+	path("token/get/", TokenObtainPairView.as_view(), name="get_token"),
+	path("token/refresh/", TokenRefreshView.as_view(), name="refresh_token") 
 ]
