bbc43cac2b6a396d02f4a22147cbc82832d6d72d
= <=>
2025-01-06 20:24:16 +0800

    Fixed UserSerializer to use global auth model

--- backend/users_api/serializers.py
+++ backend/users_api/serializers.py
@@ -1,11 +1,9 @@
 from rest_framework.serializers import ModelSerializer
 from django.contrib.auth import get_user_model
 
-from .models import UserAuthModel
-
 class UserSerializer(ModelSerializer):
 	class Meta:
-		model = UserAuthModel
+		model = get_user_model()
 		fields = ["id", "username", "email", "password"]
 		extra_kwargs = {
 			"password": {
