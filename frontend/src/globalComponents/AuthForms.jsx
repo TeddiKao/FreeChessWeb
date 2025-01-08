d9ceed3236d1c3946bbd06bb9069d23d6864bdd4
= <=>
2025-01-08 08:13:28 +0800

    Added authentication form HTML

--- frontend/src/globalComponents/AuthForms.jsx
+++ frontend/src/globalComponents/AuthForms.jsx
@@ -1,22 +1,75 @@
-function AuthForm({ method, url }) {
-	const formTitle = method === "Login" ? "Login" : "Signup"
-	
-	let formSubtitleHTML = null;
-	if (method === "Login") {
-		formSubtitleHTML = (
-			<p>Don't have an account yet? <a href="/signup">Sign up</a></p>
-		)
-	} else {
-		formSubtitleHTML = (
-			<p>Already have an account? <a href="/login">Login</a></p>
-		)
-	}
+import { useState } from "react";
 
-	return (
-		<div className="auth-form-container">
-			<h1>{formTitle}</h1>
-		</div>
-	)
+import api from "../api.js";
+
+function AuthForm({ method, url }) {
+    const [email, setEmail] = useState("");
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    function handleEmailChange(event) {
+        setEmail(event.target.value);
+    }
+
+    function handleUsernameChange(event) {
+        setUsername(event.target.value);
+    }
+
+    function handlePasswordChange(event) {
+        setPassword(event.target.value);
+    }
+
+    function handleFormSubmit(event) {}
+
+    let formSubtitleHTML = null;
+    if (method === "Login") {
+        formSubtitleHTML = (
+            <p>
+                Don't have an account yet? <a href="/signup">Sign up</a>
+            </p>
+        );
+    } else {
+        formSubtitleHTML = (
+            <p>
+                Already have an account? <a href="/login">Login</a>
+            </p>
+        );
+    }
+
+    const formTitle = method === "Login" ? "Login" : "Signup";
+
+    return (
+        <div className="auth-form-container">
+            <h1>{formTitle}</h1>
+            <form className="form" onSubmit={handleFormSubmit}>
+                <input
+                    type="email"
+                    value={email}
+                    onChange={handleEmailChange}
+					placeholder="Email"
+                />
+                <br />
+
+                <input
+                    type="text"
+                    value={username}
+                    onChange={handleUsernameChange}
+					placeholder="Username"
+                />
+                <br />
+
+                <input
+                    type="password"
+                    value={password}
+                    onChange={handlePasswordChange}
+					placeholder="Password"
+                />
+                <br />
+
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    );
 }
 
-export default AuthForm
+export default AuthForm;
