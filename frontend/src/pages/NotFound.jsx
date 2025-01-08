86cc0a3fa6524c2238f551efd2d3e52503789717
= <=>
2025-01-08 10:55:05 +0800

    Allow users to return to home page after reaching 404 page

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,8 +1,11 @@
+import "../styles/not-found.css"
+
 function NotFound() {
 	return (
-		<div>
-			<h1>404 not found</h1>
+		<div className="not-found-page-container">
+			<h1 className="not-found-heading">404 not found</h1>
 			<p>The page you were looking for was not found</p>
+			<a className="home-page-link" href="/">Return to home page</a>
 		</div>
 	)
 }
