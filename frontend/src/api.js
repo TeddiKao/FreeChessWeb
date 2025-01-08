86e035256f96e5b104553b5d158648826ab73782
= <=>
2025-01-08 08:27:57 +0800

    Fixed bug with Axios API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
 	baseURL: import.meta.env.VITE_API_URL
 })
 
-api.interceptors.request(
+api.interceptors.request.use(
 	(config) => {
 		const accessToken = localStorage.getItem(ACCESS_TOKEN)
 		if (accessToken) {
