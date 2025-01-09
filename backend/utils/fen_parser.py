c6a22d9fc4e1f9fec392fb433cf7124453911947
= <=>
2025-01-09 11:29:55 +0800

    Add functionality so that board placement can be parsed from FEN string

--- backend/utils/fen_parser.py
+++ backend/utils/fen_parser.py
@@ -4,11 +4,35 @@ letter_to_piece_mapping = {
 	"b": "Bishop",
 	"n": "Knight",
 	"k": "King",
+	"p": "Pawn"
 }
 
 def parse_board_placement(board_placement_string: str):
-	for letter in board_placement_string:
-		pass
+	current_square_index = 63
+	current_rank = 8
+
+	piece_placements = {}
+
+	for character in board_placement_string:
+		if character.isalpha() and character != "/":
+			piece_color = "White" if character.isupper() else "Black"
+			piece_type = letter_to_piece_mapping[character.lower()]
+
+			piece_placements[f"{current_square_index}"] = {
+				"color": piece_color,
+				"piece": piece_type
+			}
+
+			current_square_index -= 1
+		
+		elif character.isdigit():
+			current_square_index -= int(character)
+
+		elif character == "/":
+			current_rank -= 1
+			current_square_index = (8 * current_rank) - 1
+
+	return piece_placements
 
 
 def parse_fen(fen_string: str):
@@ -20,6 +44,9 @@ def parse_fen(fen_string: str):
 	
 	parsed_board_placement_string = parse_board_placement(board_placement_string)
 
+	return {
+		"board_placement": parsed_board_placement_string
+	}
 
 	
-parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+print(parse_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"))
