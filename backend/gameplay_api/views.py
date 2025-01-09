7667d7434372428922672388774eb8e9c92cd123
= <=>
2025-01-09 13:51:50 +0800

    Added Parse FEN view

--- backend/gameplay_api/views.py
+++ backend/gameplay_api/views.py
@@ -1,4 +1,13 @@
 from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from .utils import fen_parser
 
 # Create your views here.
 class ParseFENView(APIView):
+	def get(self, request, *args, **kwargs):
+		parsed_fen = fen_parser.parse_fen(request)
+
+		return Response(parsed_fen, status=status.HTTP_200_OK)
+
