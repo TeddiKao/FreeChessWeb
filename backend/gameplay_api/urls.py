f1cbbc5296b9fc38dc9fbf4a95c07e8d4a5dcb5a
= <=>
2025-01-11 08:59:45 +0800

    Add view for creating chess game

--- backend/gameplay_api/urls.py
+++ backend/gameplay_api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import ParseFENView
+from .views import ParseFENView, StartChessGameView
 
 urlpatterns = [
-	path("parse-fen/", ParseFENView.as_view(), name="parse_fen")
+	path("parse-fen/", ParseFENView.as_view(), name="parse_fen"),
+	path("start-chess-game/", StartChessGameView.as_view(), name="start_chess_game")
 ]
--- backend/gameplay_api/views.py
+++ backend/gameplay_api/views.py
@@ -1,14 +1,27 @@
 from rest_framework.decorators import APIView
+from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework import status
+
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 from .utils import fen_parser
 
 # Create your views here.
 class ParseFENView(APIView):
+	permission_classes = [IsAuthenticated]
+
 	def get(self, request, *args, **kwargs):
 		raw_fen_string = request.query_params.get("raw_fen_string")
 		parsed_fen = fen_parser.parse_fen(raw_fen_string)
 
 		return Response(parsed_fen, status=status.HTTP_200_OK)
 
+class StartChessGameView(generics.CreateAPIView):
+	permission_classes = [IsAuthenticated]
+
+	def perform_create(self, serializer):
+		if serializer.is_valid():
+			serializer.save()
+		else:
+			print(serializer.errors)
