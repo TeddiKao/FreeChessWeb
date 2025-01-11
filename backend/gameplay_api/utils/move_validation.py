b75cb3af1db2d5c8759d1b059f151000c0c81623
= <=>
2025-01-11 09:15:17 +0800

    Added view for validating moves

---
+++ backend/gameplay_api/utils/move_validation.py
@@ -0,0 +1,6 @@
+# Queen, rook or bishop
+def validate_sliding_pieces_move(move_infoo):
+	pass
+
+def validate_move(current_fen, move_info):
+	pass	
--- backend/gameplay_api/views.py
+++ backend/gameplay_api/views.py
@@ -5,7 +5,7 @@ from rest_framework import status
 
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
-from .utils import fen_parser
+from .utils import fen_parser, move_validation
 
 # Create your views here.
 class ParseFENView(APIView):
@@ -16,6 +16,23 @@ class ParseFENView(APIView):
 		parsed_fen = fen_parser.parse_fen(raw_fen_string)
 
 		return Response(parsed_fen, status=status.HTTP_200_OK)
+
+
+class ValidateMoveView(APIView):
+	permission_classes = [IsAuthenticated]
+
+	def get(self, request, *args, **kwargs):
+		move_info = request.query_params.get("move_info")
+
+		# The user must send a parsed FEN string and not the raw FEN string
+		parsed_fen_string = request.query_params.get("parsed_fen_string")
+		is_move_valid = not not move_validation.validate_move(parsed_fen_string, move_info)
+		
+		if is_move_valid:
+			return Response({"is_valid": True}, status=status.HTTP_200_OK)
+		else:
+			return Response({"is_valid": False}, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
 
 class StartChessGameView(generics.CreateAPIView):
 	permission_classes = [IsAuthenticated]
