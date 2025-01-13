c0e7f656b6aa380d010b130518ba5f8e8651e435
= <=>
2025-01-13 19:46:16 +0800

    Rename route for selecting time control

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -58,7 +58,7 @@ function App() {
                         />
 
                         <Route
-                            path="/play"
+                            path="/select-time-control"
                             element={
                                 <ProtectedRoute>
                                     <Play />
