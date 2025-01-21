cb35b7150b5d047921a218b7548c20d1a5ad3b92
= <=>
2025-01-21 19:28:50 +0800

    Remove time_created property from queue model

--- backend/matchmaking_api/models.py
+++ backend/matchmaking_api/models.py
@@ -3,4 +3,3 @@ from django.db import models
 # Create your models here.
 class Queue(models.Model):
 	players = models.JSONField(default=list)
-	time_created = models.DateTimeField(auto_now_add=True)
