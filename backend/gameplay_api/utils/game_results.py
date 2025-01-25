5069cc3ecc328ccf36080768ddeaf1eca76c93f9
= <=>
2025-01-25 18:09:12 +0800

    Refactor checkmate detection function for type hinting and remove debug print statements

--- backend/gameplay_api/utils/game_results.py
+++ backend/gameplay_api/utils/game_results.py
@@ -31,12 +31,11 @@ def get_is_stalemated(board_placement: dict, castling_rights: dict, king_color: 
 	return False
 
 def get_is_checkmated(board_placement: dict, castling_rights: dict, king_color: str) -> bool:
-	all_legal_moves = []
+	all_legal_moves: list = []
 
 	king_position = get_king_position(board_placement, king_color)
-	
-	print(king_color)
-	king_in_check = is_king_in_check(board_placement, king_color, f"{king_position}")
+
+	king_in_check: bool = is_king_in_check(board_placement, king_color, f"{king_position}")
 	
 	for square in board_placement.keys():
 		piece_color: str = board_placement[square]["piece_color"]
@@ -45,7 +44,7 @@ def get_is_checkmated(board_placement: dict, castling_rights: dict, king_color: 
 		if piece_color.lower() != king_color.lower():
 			continue
 
-		move_info = {
+		move_info: dict = {
 			"piece_color": piece_color,
 			"piece_type": piece_type,
 			"starting_square": square
@@ -54,7 +53,7 @@ def get_is_checkmated(board_placement: dict, castling_rights: dict, king_color: 
 		all_legal_moves += get_legal_moves(move_info, board_placement, castling_rights)
 
 	
-	legal_moves_num = len(all_legal_moves)
+	legal_moves_num: int = len(all_legal_moves)
 
 	if legal_moves_num == 0:
 		if king_in_check:
