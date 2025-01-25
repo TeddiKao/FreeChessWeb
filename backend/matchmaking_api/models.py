41b4a0fa0d206962ce7b9e270c7e7bb6ce7d2fac
= <=>
2025-01-25 18:34:38 +0800

    Add Player model to handle user profiles and online status in matchmaking

--- backend/matchmaking_api/models.py
+++ backend/matchmaking_api/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class Player(models.Model):
+	user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+	is_searching = models.BooleanField(default=False)
+	is_online = models.BooleanField(default=False)
+
 class Queue(models.Model):
 	players = models.JSONField(default=list)
