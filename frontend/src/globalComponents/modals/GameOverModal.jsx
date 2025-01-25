31893fc1bac31e2517cf40a728f983ea9324e843
= <=>
2025-01-25 15:11:08 +0800

    Add GameOverModal component for displaying game end status

---
+++ frontend/src/globalComponents/modals/GameOverModal.jsx
@@ -0,0 +1,9 @@
+function GameOverModal({ visible, gameEndCause, gameWinner }) {
+	return (
+		<div className="game-over-modal">
+			<p>Game over</p>
+		</div>
+	)
+}
+
+export default GameOverModal;
