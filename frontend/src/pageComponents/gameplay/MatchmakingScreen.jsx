8698bf5227bf14a2ca29878f083b0df356c2f9f1
= <=>
2025-01-26 15:13:02 +0800

    Refactor MatchmakingScreen to include time control display and cancel button

--- frontend/src/pageComponents/gameplay/MatchmakingScreen.jsx
+++ frontend/src/pageComponents/gameplay/MatchmakingScreen.jsx
@@ -1,32 +1,44 @@
-import { useEffect, useState } from "react"
-import api from "../../api.js"
+import { useEffect, useState } from "react";
+import api from "../../api.js";
+import { displayTimeControl } from "../../utils/timeUtils";
 
 function MatchmakingScreen({ timeControlInfo: { baseTime, increment } }) {
-	const [matchmakingStatus, setMatchmakingStatus] = useState("Finding match")
-	
-	useEffect(() => {
-		async function findMatch() {
-			try {
-				const response = await api.post("/matchmaking-api/match-player/")
-				if (response.data["player_found"]) {
-					setMatchmakingStatus("Found player")
-				}
+    const [matchmakingStatus, setMatchmakingStatus] = useState("Finding match");
 
-			} catch (error) {
-				console.log(error)
-			}
-		}
+    useEffect(() => {
+        async function findMatch() {
+            try {
+                const response = await api.post(
+                    "/matchmaking-api/match-player/"
+                );
+                if (response.data["player_found"]) {
+                    setMatchmakingStatus("Found player");
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
 
-		const findMatchInterval = setInterval(() => {
-			findMatch().catch((error) => {
-				console.log(error);
-			})
-		}, 1000)
+        const findMatchInterval = setInterval(() => {
+            findMatch().catch((error) => {
+                console.log(error);
+            });
+        }, 1000);
 
-		return () => {
-			clearInterval(findMatchInterval)
-		}
-	}, [])
+        return () => {
+            clearInterval(findMatchInterval);
+        };
+    }, []);
+
+    return (
+        <div className="matchmaking-screen-container">
+            <h1>{matchmakingStatus}</h1>
+            <p className="time-control">
+                {displayTimeControl({ baseTime, increment })}
+            </p>
+			<button className="cancel-matchmaking">Cancel</button>
+        </div>
+    );
 }
 
-export default MatchmakingScreen
+export default MatchmakingScreen;
