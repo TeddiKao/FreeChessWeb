dd49ba8496c402608c246b14f1d1860bb1048770
= <=>
2025-01-26 12:25:19 +0800

    Add WaitingPlayer model to track users waiting for matchmaking

--- backend/matchmaking_api/models.py
+++ backend/matchmaking_api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+class WaitingPlayer(models.Model):
+	user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+	timestamp = models.DateTimeField(auto_now_add=True)
