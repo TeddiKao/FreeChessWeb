01bd27c37f6ac71f6b4cd7e5b76cb6a2a02b20a6
= <=>
2025-01-26 12:34:26 +0800

    Implement matchmaking logic to pair players and create ChessGame instances

--- backend/matchmaking_api/views.py
+++ backend/matchmaking_api/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import WaitingPlayer
+from gameplay_api.models import ChessGame
+
+class MatchmakingView(APIView):
+	def post(self, request):
+		player = request.user
+		waiting_player = WaitingPlayer.objects.first()
+
+		if waiting_player:
+			opponent = waiting_player.user
+			waiting_player.delete()
+
+			if random.choice([True, False]):
+				white_player = player
+				black_player = opponent
+			else:
+				white_player = opponent
+				black_player = player
+
+			ChessGame.objects.create(white_player=white_player, black_player=black_player)
+		else:
+			WaitingPlayer.objects.create(user=request.user)
+
