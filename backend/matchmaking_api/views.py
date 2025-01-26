927a862b3c31655e1011eefe609b97f2007421a5
= <=>
2025-01-26 14:58:54 +0800

    Enhance matchmaking response with player status and waiting notifications

--- backend/matchmaking_api/views.py
+++ backend/matchmaking_api/views.py
@@ -1,7 +1,9 @@
 import random
 
-from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
 from .models import WaitingPlayer
 from gameplay_api.models import ChessGame
 
@@ -22,6 +24,19 @@ class MatchmakingView(APIView):
 				black_player = player
 
 			ChessGame.objects.create(white_player=white_player, black_player=black_player)
+
+			response_data = {
+				"matchmaking_status": "Found player",
+				"player_found": opponent.username
+			}
+
+			return Response(response_data, status=status.HTTP_200_OK)
 		else:
+			response_data = {
+				"matchmaking_status": "Waiting",
+				"player_found": None
+			}
+
 			WaitingPlayer.objects.create(user=request.user)
+			return Response(response_data, status=status.HTTP_202_ACCEPTED)
 
