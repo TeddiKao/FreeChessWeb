cd5296b1e915c223e69fa96d8228c9ac0558dc4f
= <=>
2025-01-26 14:44:25 +0800

    Add matchmaking API routing and define match-player endpoint

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
 	path("users_api/", include("users_api.urls")),
-	path("gameplay_api/", include("gameplay_api.urls"))
+	path("gameplay_api/", include("gameplay_api.urls")),
+	path("matchmaking_api/", include("matchmaking_api.urls")),
 ]
