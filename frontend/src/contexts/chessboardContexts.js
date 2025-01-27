9184b620abab4299ef28a8139bdf9f51840629c4
= <=>
2025-01-27 08:46:48 +0800

    Format export statements in chessboardContexts.js for improved readability

--- frontend/src/contexts/chessboardContexts.js
+++ frontend/src/contexts/chessboardContexts.js
@@ -4,4 +4,8 @@ const GameEndedSetterContext = createContext(null);
 const GameEndedCauseSetterContext = createContext(null);
 const GameWinnerSetterContext = createContext(null);
 
-export { GameEndedSetterContext, GameEndedCauseSetterContext, GameWinnerSetterContext};
+export {
+    GameEndedSetterContext,
+    GameEndedCauseSetterContext,
+    GameWinnerSetterContext,
+};
