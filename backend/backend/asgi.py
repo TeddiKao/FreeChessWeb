5debf2fcc47dcd6c19fac5b7fc2ccaff7b68b6a3
= <=>
2025-01-29 07:56:27 +0800

    Add matchmaking websocket routing and consumer integration

--- backend/backend/asgi.py
+++ backend/backend/asgi.py
@@ -13,10 +13,13 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import users_api.routing
+import matchmaking_api.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
+websocket_routes = users_api.routing.websocket_urlpatterns + matchmaking_api.routing.websocket_urlpatterns
+
 application = ProtocolTypeRouter({
 	"http": get_asgi_application(),
-	"websocket": URLRouter(users_api.routing.websocket_urlpatterns)
+	"websocket": URLRouter(websocket_routes)
 })
