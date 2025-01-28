e1fe63af8ae1cc527b4531f7846dfe473b69c65e
= <=>
2025-01-28 19:59:43 +0800

    Format time control constants for consistency and readability

--- frontend/src/constants/timeControls.js
+++ frontend/src/constants/timeControls.js
@@ -1,81 +1,86 @@
 const bulletTimeControls = [
-	{
-		id: 1,
-		baseTime: 60,
-		increment: 0,
-	},
+    {
+        id: 1,
+        baseTime: 60,
+        increment: 0,
+    },
 
-	{
-		id: 2,
-		baseTime: 60,
-		increment: 1,
-	},
+    {
+        id: 2,
+        baseTime: 60,
+        increment: 1,
+    },
 
-	{
-		id: 3,
-		baseTime: 120,
-		increment: 0,
-	},
+    {
+        id: 3,
+        baseTime: 120,
+        increment: 0,
+    },
 
-	{
-		id: 4,
-		baseTime: 120,
-		increment: 1,
-	}
+    {
+        id: 4,
+        baseTime: 120,
+        increment: 1,
+    },
 ];
 
 const blitzTimeControls = [
-	{
-		id: 5,
-		baseTime: 180,
-		increment: 0,
-	},
+    {
+        id: 5,
+        baseTime: 180,
+        increment: 0,
+    },
 
-	{
-		id: 6,
-		baseTime: 180,
-		increment: 2,
-	},
+    {
+        id: 6,
+        baseTime: 180,
+        increment: 2,
+    },
 
-	{
-		id: 7,
-		baseTime: 300,
-		increment: 0,
-	},
+    {
+        id: 7,
+        baseTime: 300,
+        increment: 0,
+    },
 ];
 
 const rapidTimeControls = [
-	{
-		id: 8,
-		baseTime: 600,
-		increment: 0,
-	},
+    {
+        id: 8,
+        baseTime: 600,
+        increment: 0,
+    },
 
-	{
-		id: 9,
-		baseTime: 600,
-		increment: 2,
-	},
+    {
+        id: 9,
+        baseTime: 600,
+        increment: 2,
+    },
 
-	{
-		id: 10,
-		baseTime: 600,
-		increment: 5,
-	},
+    {
+        id: 10,
+        baseTime: 600,
+        increment: 5,
+    },
 
-	{
-		id: 11,
-		baseTime: 900,
-		increment: 10,
-	},
+    {
+        id: 11,
+        baseTime: 900,
+        increment: 10,
+    },
 ];
 
 const classicalTimeControls = [
-	{
-		id: 12,
-		baseTime: 90 * 60,
-		increment: 30,
-	}
+    {
+        id: 12,
+        baseTime: 90 * 60,
+        increment: 30,
+    },
 ];
 
-export { bulletTimeControls, blitzTimeControls, rapidTimeControls, classicalTimeControls }
+export {
+    bulletTimeControls,
+    blitzTimeControls,
+    rapidTimeControls,
+    classicalTimeControls,
+};
