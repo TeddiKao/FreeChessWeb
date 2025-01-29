49103ef98a663791c8de47a9ed95b0b4fed405db
= <=>
2025-01-29 20:30:47 +0800

    Fix import statement for token constants to include file extension

--- frontend/src/utils/tokenUtils.js
+++ frontend/src/utils/tokenUtils.js
@@ -1,4 +1,4 @@
-import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants/tokens"
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../constants/tokens.js"
 
 function getAccessToken() {
 	return localStorage.getItem(ACCESS_TOKEN);
