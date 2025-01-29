0a53b0da7f093264bc286f2af30c26d054192d81
= <=>
2025-01-29 20:31:50 +0800

    Fix regex pattern in WebSocket URL routing for matchmaking server

--- backend/matchmaking_api/routing.py
+++ backend/matchmaking_api/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-	re_path("ws/matchmaking-server/", consumers.MatchmakingConsumer.as_asgi())
+	re_path(r"ws/matchmaking-server/", consumers.MatchmakingConsumer.as_asgi())
 ]
