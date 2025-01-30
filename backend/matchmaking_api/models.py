390def2430f6ec54429fa579f3fe4644476f98be
= <=>
2025-01-30 19:02:25 +0800

    Add matched_user field to WaitingPlayer model and implement async username retrieval

--- backend/matchmaking_api/models.py
+++ backend/matchmaking_api/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from channels.db import database_sync_to_async
 
 class WaitingPlayer(models.Model):
-	user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
+	user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name="linked_user")
+	matched_user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE, related_name="matched_user", null=True, blank=True)
 	timestamp = models.DateTimeField(auto_now_add=True)
+
+	@database_sync_to_async
+	def get_username(self):
+		return self.user.username
