831f2422adccf674d39ec04337517818d8d6828a
= <=>
2025-02-01 12:36:16 +0800

    Add PassAndPlay component for handling game state and rendering

---
+++ frontend/src/pages/Protected/PassAndPlay.jsx
@@ -0,0 +1,71 @@
+function PassAndPlay() {
+	const [parsedFEN, setParsedFEN] = useState(null);
+    const location = useLocation();
+
+    const [gameEnded, setGameEnded] = useState(false);
+    const [gameEndedCause, setGameEndedCause] = useState(null);
+    const [gameWinner, setGameWinner] = useState(null);
+
+    useEffect(() => {}, [gameEnded, gameEnded, gameWinner]);
+
+    useEffect(() => {
+        getParsedFEN();
+    }, []);
+
+    if (!location.state) {
+        return <Navigate to={"/select-time-control"} />;
+    }
+
+    const timeControlBaseTime = location.state.baseTime;
+    const timeControlIncrement = location.state.increment;
+
+    const startingPositionFEN =
+        "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+    async function getParsedFEN() {
+        try {
+            const fetchedFEN = await fetchFen(startingPositionFEN);
+            setParsedFEN(fetchedFEN);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
+    return (
+        <GameEndedSetterContext.Provider value={setGameEnded}>
+            <GameEndedCauseSetterContext.Provider value={setGameEndedCause}>
+                <GameWinnerSetterContext.Provider value={setGameWinner}>
+                    <div className="playing-interface-container">
+                        <div className="top-timer-wrapper">
+                            <Timer
+                                playerColor="black"
+                                position="top"
+                                timeInSeconds={timeControlBaseTime}
+                            />
+                        </div>
+
+                        <Chessboard
+                            parsed_fen_string={parsedFEN}
+                            orientation="White"
+                        />
+                        <GameOverModal
+                            visible={gameEnded}
+                            gameEndCause={gameEndedCause}
+                            gameWinner={gameWinner}
+                        />
+
+                        <div className="bottom-timer-wrapper">
+                            <Timer
+                                playerColor="white"
+                                position="bottom"
+                                timeInSeconds={timeControlBaseTime}
+                            />
+                        </div>
+                    </div>
+                </GameWinnerSetterContext.Provider>
+            </GameEndedCauseSetterContext.Provider>
+        </GameEndedSetterContext.Provider>
+	)
+}
+
+export default PassAndPlay;
