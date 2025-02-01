c4ef12a7a391e135afd8ee29f9b8f99f33e194c0
= <=>
2025-02-01 17:02:59 +0800

    Send game start notification to user upon connection in GameConsumer

--- backend/gameplay_api/consumers.py
+++ backend/gameplay_api/consumers.py
@@ -1,7 +1,14 @@
+import json
+
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 class GameConsumer(AsyncWebsocketConsumer):
 	async def connect(self):
+		await self.send(json.dumps({
+			"type": "game_started",
+			"user": self.scope["user"]
+		}))
+
 		await self.accept()
 
 	async def disconnect(self, code):
