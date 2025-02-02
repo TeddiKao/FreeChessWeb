cebbad572fb50a3108269f1527a056ed1c18a9c9
= <=>
2025-02-02 20:29:21 +0800

    Enhance GameConsumer to manage game room connections and handle move events via WebSocket

--- backend/gameplay_api/consumers.py
+++ backend/gameplay_api/consumers.py
@@ -7,6 +7,7 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 class GameConsumer(AsyncWebsocketConsumer):
 	async def update_position(self, new_position):
 		pass
+
 	async def connect(self):
 		query_string: bytes = self.scope.get("query_string", b"")
 		decoded_query_string = query_string.decode()
@@ -15,7 +16,12 @@ class GameConsumer(AsyncWebsocketConsumer):
 
 		await self.accept()
 
-		self.room_group_name = None
+		self.room_group_name = f"game_{game_id}"
+
+		await self.channel_layer.group_add(
+			self.room_group_name,
+			self.channel_name
+		)
 
 		await self.send(json.dumps({
 			"type": "game_started",
@@ -27,8 +33,24 @@ class GameConsumer(AsyncWebsocketConsumer):
 		pass
 
 	async def receive(self, text_data):
+		print(text_data)
+
+		await self.channel_layer.group_send(
+			self.room_group_name,
+			{
+				"type": "move_received",
+				"move_data": text_data,
+				"move_made_by": self.scope["user"].username
+			}
+		)
+
+		print("Handled!")
+
+	async def move_received(self, event):
+		print("Move received!")
+
 		await self.send(json.dumps({
-			"type": "received_move",
-			"move_data": json.loads(text_data),
-			"sent_by": self.scope["user"].username
+			"type": "move_made",
+			"move_data": json.loads(event["move_data"]),
+			"move_made_by": event["move_made_by"]
 		}))
