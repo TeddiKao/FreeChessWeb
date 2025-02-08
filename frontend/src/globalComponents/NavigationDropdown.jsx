0aacdd18bb874077f0c443c65dc00a5da2d58b6e
= <=>
2025-02-08 14:32:43 +0800

    Add basic logic for navigation dropdown

---
+++ frontend/src/globalComponents/NavigationDropdown.jsx
@@ -0,0 +1,23 @@
+function NavigationDropdown({ navigationLinks, isVisible }) {
+	if (!isVisible) {
+		return null;
+    }
+
+    function generateLinks() {
+        return navigationLinks.map(({ linkName, linkPath }, linkIndex) => {
+            return (
+                <a
+                    className="navigation-dropdown-link"
+                    key={linkIndex}
+                    href={linkPath}
+                >
+                    {linkName}
+                </a>
+            );
+        });
+    }
+
+    return (
+        <div className="navigation-dropdown-container">{generateLinks()}</div>
+    );
+}
