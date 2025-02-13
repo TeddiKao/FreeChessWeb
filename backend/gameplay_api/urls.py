8bf205ed7993ed3bf400e67f980b885dac401c1d
= <=>
2025-02-14 07:39:03 +0800

    Add path to UpdateSettingsView in urls.py file

--- backend/gameplay_api/urls.py
+++ backend/gameplay_api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 	path("start-chess-game/", StartChessGameView.as_view(), name="start_chess_game"),
 	path("get-ongoing-chess-game/", GetOngoingGameView.as_view(), name="get_ongoing_chess_game"),
 	path("make-move-view/<int:pk>", MakeMoveView.as_view(), name="make_move_view"),
-	path("get-gameplay-settings/", GetGameplaySettingsView.as_view(), name="get_gameplay_settings")
+	path("get-gameplay-settings/", GetGameplaySettingsView.as_view(), name="get_gameplay_settings"),
+	path("update-gameplay-settings/", UpdateSettingsView.as_view(), name="update_gameplay_settings")
 ]
