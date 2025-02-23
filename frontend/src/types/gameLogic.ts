6a488815128e890b12a59e447f59bcab5d6f84ed
= <=>
2025-02-23 10:44:42 +0800

    Add types related to game logic

---
+++ frontend/src/types/gameLogic.ts
@@ -0,0 +1,34 @@
+import { ChessboardSquareIndex } from "./general"
+
+type SquareInfo = {
+	piece_color: string,
+	piece_type: string,
+	starting_square?: ChessboardSquareIndex
+}
+
+type BoardPlacement = {
+	[key: string]: SquareInfo
+}
+
+type CastlingRightsInfo = {
+	[key: string]: boolean
+}
+
+type CastlingRights = {
+	[key: string]: CastlingRightsInfo
+}
+
+type ParsedFENString = {
+	board_placement: BoardPlacement,
+	castling_rights: CastlingRights,
+	en_passant_target_square: ChessboardSquareIndex | null,
+	halfmove_clock: number,
+	fullmove_number: number
+}
+
+type PieceInfo = {
+	piece_color: string,
+	piece_type: string
+}
+
+export type { ParsedFENString, PieceInfo, BoardPlacement, CastlingRights }
