fb8298d094edb16d310197990bc1b0d383bb6862
= <=>
2025-02-23 14:58:59 +0800

    Changed context files from JS to TS

---
+++ frontend/src/contexts/chessboardContexts.ts
@@ -0,0 +1,19 @@
+import { createContext } from "react";
+import { OptionalValue, StateSetterFunction } from "../types/general";
+
+const GameEndedSetterContext =
+    createContext<OptionalValue<StateSetterFunction<boolean>>>(null);
+const GameEndedCauseSetterContext =
+    createContext<OptionalValue<StateSetterFunction<OptionalValue<string>>>>(
+        null
+    );
+const GameWinnerSetterContext =
+    createContext<OptionalValue<StateSetterFunction<OptionalValue<string>>>>(
+        null
+    );
+
+export {
+    GameEndedSetterContext,
+    GameEndedCauseSetterContext,
+    GameWinnerSetterContext,
+};
