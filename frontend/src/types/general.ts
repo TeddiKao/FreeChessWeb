a621f5ff62c65902f6dcc59dbff9e51f4a8ec73c
= <=>
2025-02-23 11:19:23 +0800

    Add resuable type for React useRef hook

--- frontend/src/types/general.ts
+++ frontend/src/types/general.ts
@@ -1,8 +1,9 @@
-import { Dispatch, SetStateAction } from "react";
+import { Dispatch, MutableRefObject, SetStateAction } from "react";
 
 type StateSetterFunction<T>  = Dispatch<SetStateAction<T>>
 type ChessboardSquareIndex = string | number
 
 type OptionalValue<T> = T | null 
+type RefObject<T> = MutableRefObject<T>
 
-export type { StateSetterFunction, ChessboardSquareIndex, OptionalValue }
+export type { StateSetterFunction, ChessboardSquareIndex, OptionalValue, RefObject }
