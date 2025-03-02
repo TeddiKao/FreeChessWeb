20f96121c24ca4226dbc8b143eff0fad8386e484
= <=>
2025-03-02 12:31:47 +0800

    Add capture notation handling for pawns

--- backend/gameplay_api/utils/algebraic_notation_parser.py
+++ backend/gameplay_api/utils/algebraic_notation_parser.py
@@ -1,6 +1,8 @@
 from typing import TypedDict
 from general import *
+from move_type_detection import is_capture
 from enums import PieceType
+from fen_parser import parse_board_placement
 
 files_list = ["a", "b", "c", "d", "e", "f", "g", "h"]
 piece_notation_mapping = {
@@ -19,39 +21,44 @@ class MoveInfo(TypedDict):
     piece_color: str
 
 
-def handle_pawn_move(move_info: MoveInfo):
+def handle_pawn_move(board_placement: dict, move_info: MoveInfo):
     starting_square = move_info["starting_square"]
     destination_square = move_info["destination_square"]
 
-    start_file = get_file(int(starting_square))
-    end_rank = get_row(int(destination_square))
+    notated_square = convert_to_algebraic_notation(destination_square)
+    starting_file = get_file(starting_square)
+    file_letter = files_list[int(starting_file)]
 
-    file_letter = files_list[start_file]
-
-    return f"{file_letter}{end_rank}"
-
+    if is_capture(board_placement, move_info):
+        return f"{file_letter}x{notated_square}"
+    else:
+        return notated_square
 
 def handle_piece_move(move_info: MoveInfo):
     destination_square = move_info["destination_square"]
     piece_type = move_info["piece_type"]
-    piece_notation = piece_notation_mapping[piece_type.lower()]
+    piece_notation = piece_notation_mapping[piece_type.lower()].upper()
 
-    return f"{piece_notation.upper()}{convert_to_algebraic_notation(destination_square)}"
+    notated_square = convert_to_algebraic_notation(destination_square)
+
+    return f"{piece_notation}{notated_square}"
 
 
-def parse_algebraic_notation(board_placement: dict, move_info: MoveInfo) -> str:
+def get_algebraic_notation(board_placement: dict, move_info: MoveInfo) -> str:
     piece_type = move_info["piece_type"].lower()
 
     if piece_type == PieceType.PAWN.value:
-        return handle_pawn_move(move_info)
+        return handle_pawn_move(board_placement, move_info)
 
     else:
         return handle_piece_move(move_info)
-
-
-parsed_rook_notation = parse_algebraic_notation({}, {
-    "starting_square": 12,
-    "destination_square": 44,
-    "piece_type": "Queen",
+    
+parsed_board_placement = parse_board_placement("rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR")
+move_info = {
+    "starting_square": 28,
+    "destination_square": 35,
+    "piece_type": "pawn",
     "piece_color": "white"
-})
+}
+
+print(get_algebraic_notation(parsed_board_placement, move_info))
---
+++ frontend/src/globalComponents/MoveListPanel.tsx
@@ -0,0 +1,13 @@
+type MoveListPanelProps = {
+	moveList: Array<Array<string>>
+}
+
+function MoveListPanel({ moveList }: MoveListPanelProps) {
+	return (
+		<div className="move-list-panel">
+
+		</div>
+	)
+}
+
+export default MoveListPanel;
