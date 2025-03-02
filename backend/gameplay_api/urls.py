ca82d26b930d431d45f0e57c8c808fd240901555
= <=>
2025-03-02 19:36:49 +0800

    Add endpoint for fetching position list

--- backend/gameplay_api/urls.py
+++ backend/gameplay_api/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
 	path("get-gameplay-settings/", GetGameplaySettingsView.as_view(), name="get_gameplay_settings"),
 	path("update-gameplay-settings/", UpdateSettingsView.as_view(), name="update_gameplay_settings"),
 	path("get-current-position/", GetCurrentPositionView.as_view(), name="get_current_position"),
-	path("get-player-timer/", GetPlayerTimerView.as_view(), name="get_player_timer")
+	path("get-player-timer/", GetPlayerTimerView.as_view(), name="get_player_timer"),
+	path("get-position-list/", GetPositionListView.as_view(), name="get_position_list"),
 ]
