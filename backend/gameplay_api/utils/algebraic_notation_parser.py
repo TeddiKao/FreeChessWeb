8ed2bed9e052581861c2b61a3f743904403ef5f5
= <=>
2025-03-02 13:14:40 +0800

    Rename all imports

--- backend/gameplay_api/utils/algebraic_notation_parser.py
+++ backend/gameplay_api/utils/algebraic_notation_parser.py
@@ -1,8 +1,8 @@
 from typing import TypedDict
 from general import *
-from move_type_detection import is_capture
-from enums import PieceType
-from fen_parser import parse_board_placement
+from move_validation_api.utils.get_move_type import get_is_check, get_is_capture
+from .enums import PieceType
+from .fen_parser import parse_board_placement
 
 files_list = ["a", "b", "c", "d", "e", "f", "g", "h"]
 piece_notation_mapping = {
@@ -29,19 +29,20 @@ def handle_pawn_move(board_placement: dict, move_info: MoveInfo):
     starting_file = get_file(starting_square)
     file_letter = files_list[int(starting_file)]
 
-    if is_capture(board_placement, move_info):
-        return f"{file_letter}x{notated_square}"
-    else:
-        return notated_square
+    capture_notation = "x" if is_capture(board_placement, move_info) else ""
 
-def handle_piece_move(move_info: MoveInfo):
+    return f"{file_letter}{capture_notation}{notated_square}"
+
+def handle_piece_move(board_placement: dict, move_info: MoveInfo):
     destination_square = move_info["destination_square"]
     piece_type = move_info["piece_type"]
     piece_notation = piece_notation_mapping[piece_type.lower()].upper()
 
     notated_square = convert_to_algebraic_notation(destination_square)
+    capture_notation = "x" if get_is_capture(board_placement, move_info) else ""
+    check_notation = "+" if get_is_check(board_placement, move_info) else ""
 
-    return f"{piece_notation}{notated_square}"
+    return f"{piece_notation}{capture_notation}{notated_square}{check_notation}"
 
 
 def get_algebraic_notation(board_placement: dict, move_info: MoveInfo) -> str:
@@ -51,13 +52,13 @@ def get_algebraic_notation(board_placement: dict, move_info: MoveInfo) -> str:
         return handle_pawn_move(board_placement, move_info)
 
     else:
-        return handle_piece_move(move_info)
+        return handle_piece_move(board_placement, move_info)
     
-parsed_board_placement = parse_board_placement("rnbqkbnr/ppp1pppp/8/3p4/4P3/8/PPPP1PPP/RNBQKBNR")
+parsed_board_placement = parse_board_placement("8/4p3/3p4/4b2p/p1Q2N2/1P2R3/6p1/6K1")
 move_info = {
-    "starting_square": 28,
-    "destination_square": 35,
-    "piece_type": "pawn",
+    "starting_square": 29,
+    "destination_square": 14,
+    "piece_type": "knight",
     "piece_color": "white"
 }
 
