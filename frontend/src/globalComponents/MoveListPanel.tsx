76b99860e1088dbfa7197639b4b69725d9565a1f
= <=>
2025-03-03 20:32:55 +0800

    Enhance styling for move panel

--- frontend/src/globalComponents/MoveListPanel.tsx
+++ frontend/src/globalComponents/MoveListPanel.tsx
@@ -1,29 +1,30 @@
-import "../styles/gameplay/move-list-panel.css"
+import "../styles/gameplay/move-list-panel.css";
 
 type MoveListPanelProps = {
-	moveList: Array<Array<string>>
-}
+    moveList: Array<Array<string>>;
+};
 
 function MoveListPanel({ moveList }: MoveListPanelProps) {
-	function generateMovePairMoves(movePair: Array<string>) {
-		return movePair.map((notatedMove: string, _) => {
-			return (
-				<p className="notated-move">{notatedMove}</p>
-			)
-		})
-	}
+    function generateMovePairMoves(movePair: Array<string>) {
+        return movePair.map((notatedMove: string, _) => {
+            return <p className="notated-move">{notatedMove}</p>;
+        });
+    }
 
-	return (
-		<div className="move-list-panel-container">
-			{moveList.map((movePair: Array<string>, moveIndex: number) => {
-				return (
-					<div key={moveIndex} className="move-pair-container">
-						{generateMovePairMoves(movePair)}
-					</div>
-				)
-			})}
-		</div>
-	)
+    return (
+        <div className="move-list-panel-container">
+            {moveList.map((movePair: Array<string>, moveIndex: number) => {
+                return (
+                    <div className="move-info-container">
+						<p className="move-number">{moveIndex + 1}.</p>
+                        <div key={moveIndex} className="move-pair-container">
+                            {generateMovePairMoves(movePair)}
+                        </div>
+                    </div>
+                );
+            })}
+        </div>
+    );
 }
 
 export default MoveListPanel;
