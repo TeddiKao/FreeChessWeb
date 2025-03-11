6f3be8e04dd7301629366ef41f43437876702ab8
= <=>
2025-03-11 12:10:54 +0800

    Add component for confirmation popup and add event handling when clicking resignation button

--- frontend/src/globalComponents/gameplaySidePanel/GameplayActionButtons.tsx
+++ frontend/src/globalComponents/gameplaySidePanel/GameplayActionButtons.tsx
@@ -1,11 +1,19 @@
+import { useState } from "react";
+
 import "../../styles/features/gameplay/gameplay-action-buttons.scss";
 
 function GameplayActionButtons() {
+	const [confirmationPopupVisible, setConfirmationPopupVisible] = useState(false);
+
+	function handleResignation() {
+		setConfirmationPopupVisible(true);
+	}
+
 	return (
 		<div className="gameplay-action-buttons-container">
 			<h4 className="gameplay-action-buttons-header">Gameplay actions</h4>
 			<div className="gameplay-action-buttons">
-				<div className="resignation-icon">
+				<div onClick={handleResignation} className="resignation-icon">
 					<img className="resign-icon" src="/resignButton.svg" />
 					<p className="helper-text">Resign</p>
 				</div>
