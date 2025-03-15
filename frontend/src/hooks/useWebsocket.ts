ed50db65f12de169d7d3b9c5a1b3a2d577e3e736
= <=>
2025-03-15 12:56:15 +0800

    Fix bug in WebSocket hook which caused only a single WebSocket to be active at a time

--- frontend/src/hooks/useWebsocket.ts
+++ frontend/src/hooks/useWebsocket.ts
@@ -1,13 +1,5 @@
-let websocket: any = null;
-
 function useWebSocket(url: string, onMessage?: any, onError?: any) {
-    if (websocket) {
-        if (websocket.readyState !== WebSocket.CLOSED) {
-            return websocket;
-        }
-    }
-
-    websocket = new WebSocket(url);
+    const websocket = new WebSocket(url);
 
     if (onMessage) {
         websocket.onmessage = onMessage;
