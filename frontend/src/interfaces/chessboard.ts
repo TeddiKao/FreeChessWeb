c416db08b79da36476261047f6b7c6bd2cf0ee50
Teddi <teddikao@gmail.com>
2025-04-20 10:54:01 +0800

    Fixes boolean conversion for DEBUG env variable
    Ensures the DEBUG environment variable is correctly interpreted as a boolean value.
    It converts the string value to a boolean value when it's being read from the environment.

--- frontend/src/interfaces/chessboard.ts
+++ frontend/src/interfaces/chessboard.ts
@@ -2,7 +2,6 @@ import React from "react";
 import {
     ChessboardSquareIndex,
     OptionalValue,
-    RefObject,
     StateSetterFunction,
 } from "../types/general.ts";
 import { ParsedFENString, PieceColor, PieceType } from "../types/gameLogic.ts";
