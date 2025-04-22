0ac0e1822afd0e1625a51c6935fb089768c925e9
Teddi <teddikao@gmail.com>
2025-04-22 12:45:06 +0800

    Wraps loading screen in modal
    Wraps the authentication loading screen within a modal.
    
    This ensures that the loading screen is properly layered and
    visually distinct from the rest of the application. It also adds
    the ability to control the visibility of the loading screen.

--- frontend/src/globalComponents/modals/AuthLoadingScreen.tsx
+++ frontend/src/globalComponents/modals/AuthLoadingScreen.tsx
@@ -1,17 +1,26 @@
 import LoadingSpinner from "../LoadingSpinner";
+import "../../styles/modals/auth-loading-screen.scss";
+import ModalWrapper from "../wrappers/ModalWrapper";
 
 type AuthLoadingScreenProps = {
-    authMethod: "login" | "signup"
-}
+	visible: boolean;
+	authMethod: "login" | "signup";
+};
 
-function AuthLoadingScreen({ authMethod }: AuthLoadingScreenProps) {
-    return (
-        <div className="auth-loading-screen-container">
-            <h4 className="auth-method">{authMethod}</h4>
-            <LoadingSpinner />
-            <p className="please-wait-text">Please wait</p>
-        </div>
-    )
+function AuthLoadingScreen({ visible, authMethod }: AuthLoadingScreenProps) {
+	if (!visible) {
+		return null;
+	}
+
+	return (
+		<ModalWrapper visible={visible}>
+			<div className="auth-loading-screen-container">
+				<h4 className="auth-method">{authMethod}</h4>
+				<LoadingSpinner />
+				<p className="please-wait-text">Please wait</p>
+			</div>
+		</ModalWrapper>
+	);
 }
 
 export default AuthLoadingScreen;
