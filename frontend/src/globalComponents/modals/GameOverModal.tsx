f82e78b507e16b737514573fa395257827e5edd6
Teddi <teddikao@gmail.com>
2025-04-23 21:01:09 +0800

    Enables matchmaking after game over
    Initiates a websocket connection to the matchmaking server
    upon clicking the "New Game" button in the game over modal.
    
    This allows the user to automatically search for a new game
    with the same time control settings.
    
    Closes the websocket connection when the component unmounts.

--- frontend/src/globalComponents/modals/GameOverModal.tsx
+++ frontend/src/globalComponents/modals/GameOverModal.tsx
@@ -1,57 +1,86 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "../../styles/modals/game-over-modal.scss";
-import { capitaliseFirstLetter, parseWebsocketUrl } from "../../utils/generalUtils";
+import {
+	capitaliseFirstLetter,
+	parseWebsocketUrl,
+} from "../../utils/generalUtils";
 import useWebSocket from "../../hooks/useWebsocket";
 import { TimeControl } from "../../types/gameSetup";
 import { useNavigate } from "react-router-dom";
 
 type GameOverModalProps = {
-    visible: boolean,
-    gameEndCause: string,
-    gameWinner: string | null,
-    timeControlInfo: TimeControl
-}
+	visible: boolean;
+	gameEndCause: string;
+	gameWinner: string | null;
+	timeControlInfo: TimeControl;
+};
 
-function GameOverModal({ visible, gameEndCause, gameWinner, timeControlInfo }: GameOverModalProps) {
-    const [matchmakingWebsocketEnabled, setMatchmakingWebsocketEnabled] = useState(false);
-    
-    const navigate = useNavigate();
+function GameOverModal({
+	visible,
+	gameEndCause,
+	gameWinner,
+	timeControlInfo,
+}: GameOverModalProps) {
+	const [matchmakingWebsocketEnabled, setMatchmakingWebsocketEnabled] =
+		useState(false);
+    const matchmakingWebsocketRef = useRef<WebSocket | null>(null);
 
-    const websocketUrl = parseWebsocketUrl("matchmaking-server", {
-        baseTime: timeControlInfo.baseTime,
-        increment: timeControlInfo.increment,
-    })
+	const navigate = useNavigate();
 
-    const matchmakingWebsocket = useWebSocket(websocketUrl, handleOnMessage, undefined, matchmakingWebsocketEnabled);
-    const matchmakingWebsocketRef = useRef(matchmakingWebsocket);
+	useEffect(() => {
+		const websocketUrl = parseWebsocketUrl("matchmaking-server", {
+			baseTime: timeControlInfo.baseTime,
+			increment: timeControlInfo.increment,
+		});
 
-    function handleOnMessage() {
+		const matchmakingWebsocket = useWebSocket(
+			websocketUrl,
+			handleOnMessage,
+			undefined,
+			matchmakingWebsocketEnabled
+		);
 
-    }
-    
-    if (!visible) {
-        return null;
-    }
+        matchmakingWebsocketRef.current = matchmakingWebsocket;
 
-    const gameResultText = !gameWinner
-        ? "Draw"
-        : `${capitaliseFirstLetter(gameWinner)} won`;
-    const gameEndCauseText = capitaliseFirstLetter(gameEndCause);
+        return () => {
+            if (matchmakingWebsocketRef.current?.readyState === WebSocket.OPEN) {
+                matchmakingWebsocketRef.current.close();
+            }
+        }
+	});
 
-    function handleNewGameCreation() {
-        setMatchmakingWebsocketEnabled(true);
-    }
+	function handleOnMessage(eventData: any) {
+		console.log(JSON.parse(eventData));
+	}
 
-    return (
-        <div className="game-over-modal-container">
-            <h1 className="game-result">{gameResultText}</h1>
-            <p className="game-end-cause">by {gameEndCauseText}</p>
-            <div className="buttons-container">
-                <button onClick={handleNewGameCreation} className="new-game-button">New game</button>
-                <button className="rematch-button">Rematch</button>
-            </div>
-        </div>
-    );
+	if (!visible) {
+		return null;
+	}
+
+	const gameResultText = !gameWinner
+		? "Draw"
+		: `${capitaliseFirstLetter(gameWinner)} won`;
+	const gameEndCauseText = capitaliseFirstLetter(gameEndCause);
+
+	function handleNewGameCreation() {
+		setMatchmakingWebsocketEnabled(true);
+	}
+
+	return (
+		<div className="game-over-modal-container">
+			<h1 className="game-result">{gameResultText}</h1>
+			<p className="game-end-cause">by {gameEndCauseText}</p>
+			<div className="buttons-container">
+				<button
+					onClick={handleNewGameCreation}
+					className="new-game-button"
+				>
+					New game
+				</button>
+				<button className="rematch-button">Rematch</button>
+			</div>
+		</div>
+	);
 }
 
 export default GameOverModal;
