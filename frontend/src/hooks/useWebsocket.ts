99cb0e889ff53086da65c8c5399772c32d93609b
Teddi <teddikao@gmail.com>
2025-04-23 20:56:04 +0800

    Refactors WebSocket handling into a hook
    Moves WebSocket creation and event handling into a React hook for better component management and reusability.
    
    Introduces an `enabled` prop to conditionally initialize the WebSocket connection, preventing unnecessary connections.
    
    Adds a cleanup function to close the WebSocket connection when the component unmounts or the URL changes, preventing memory leaks.

--- frontend/src/hooks/useWebsocket.ts
+++ frontend/src/hooks/useWebsocket.ts
@@ -1,19 +1,37 @@
-function useWebSocket(url: string, onMessage?: any, onError?: any) {
-    const websocket = new WebSocket(url);
+import { useEffect, useState } from "react";
 
-    if (onMessage) {
-        websocket.onmessage = onMessage;
-    }
+function useWebSocket(
+	url: string,
+	onMessage?: any,
+	onError?: any,
+	enabled = true
+) {
+	const [socketState, setSocketState] = useState<WebSocket | null>(null);
+    
+    useEffect(() => {
+		if (!enabled) {
+			return;
+		}
 
-    if (onError) {
-        websocket.onerror = onError;
-    }
+		const websocket = new WebSocket(url);
+        setSocketState(websocket);
 
-    websocket.onclose = (event: CloseEvent) => {
-        
-    }
+		if (onMessage) {
+			websocket.onmessage = onMessage;
+		}
 
-    return websocket;
+		if (onError) {
+			websocket.onerror = onError;
+		}
+
+        return () => {
+            if (websocket.readyState === WebSocket.OPEN) {
+                websocket.close();
+            }
+        }
+	}, [url, enabled]);
+
+	return socketState;
 }
 
 export default useWebSocket;
