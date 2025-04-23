7b4cf84433f2ecbec71ccfcbd1727cb2b398b252
Teddi <teddikao@gmail.com>
2025-04-23 20:57:56 +0800

    Enables new game creation from game over modal
    Allows users to initiate a new game directly from the game over modal.
    
    It initializes matchmaking websocket upon clicking "New Game" which
    navigates the user to the matchmaking queue.

--- frontend/src/globalComponents/modals/GameOverModal.tsx
+++ frontend/src/globalComponents/modals/GameOverModal.tsx
@@ -1,13 +1,34 @@
+import { useRef, useState } from "react";
 import "../../styles/modals/game-over-modal.scss";
-import { capitaliseFirstLetter } from "../../utils/generalUtils";
+import { capitaliseFirstLetter, parseWebsocketUrl } from "../../utils/generalUtils";
+import useWebSocket from "../../hooks/useWebsocket";
+import { TimeControl } from "../../types/gameSetup";
+import { useNavigate } from "react-router-dom";
 
 type GameOverModalProps = {
     visible: boolean,
     gameEndCause: string,
     gameWinner: string | null,
+    timeControlInfo: TimeControl
 }
 
-function GameOverModal({ visible, gameEndCause, gameWinner }: GameOverModalProps) {
+function GameOverModal({ visible, gameEndCause, gameWinner, timeControlInfo }: GameOverModalProps) {
+    const [matchmakingWebsocketEnabled, setMatchmakingWebsocketEnabled] = useState(false);
+    
+    const navigate = useNavigate();
+
+    const websocketUrl = parseWebsocketUrl("matchmaking-server", {
+        baseTime: timeControlInfo.baseTime,
+        increment: timeControlInfo.increment,
+    })
+
+    const matchmakingWebsocket = useWebSocket(websocketUrl, handleOnMessage, undefined, matchmakingWebsocketEnabled);
+    const matchmakingWebsocketRef = useRef(matchmakingWebsocket);
+
+    function handleOnMessage() {
+
+    }
+    
     if (!visible) {
         return null;
     }
@@ -17,12 +38,16 @@ function GameOverModal({ visible, gameEndCause, gameWinner }: GameOverModalProps
         : `${capitaliseFirstLetter(gameWinner)} won`;
     const gameEndCauseText = capitaliseFirstLetter(gameEndCause);
 
+    function handleNewGameCreation() {
+        setMatchmakingWebsocketEnabled(true);
+    }
+
     return (
         <div className="game-over-modal-container">
             <h1 className="game-result">{gameResultText}</h1>
             <p className="game-end-cause">by {gameEndCauseText}</p>
             <div className="buttons-container">
-                <button className="new-game-button">New game</button>
+                <button onClick={handleNewGameCreation} className="new-game-button">New game</button>
                 <button className="rematch-button">Rematch</button>
             </div>
         </div>
