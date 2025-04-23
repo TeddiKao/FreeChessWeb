f1ef8dc1fe54f1b1d0e7424fb69a3f4c62a0ba5f
Teddi <teddikao@gmail.com>
2025-04-23 20:42:44 +0800

    Adds websocket URL parsing functionality
    Introduces a utility function to construct websocket URLs
    with authentication token and extra parameters. This enhances
    websocket connection management by centralizing URL construction logic.

--- frontend/src/utils/generalUtils.ts
+++ frontend/src/utils/generalUtils.ts
@@ -1,4 +1,6 @@
 import { isEqual } from "lodash";
+import { websocketBaseURL } from "../constants/urls";
+import { getAccessToken } from "./tokenUtils";
 
 function capitaliseFirstLetter(string: string): string {
 	const capitalisedFirstLetter = string.charAt(0).toUpperCase();
@@ -25,6 +27,22 @@ function padZero(value: number): string | number {
 
 function isTouchDevice() {
 	return ("ontouchstart" in window) || navigator.maxTouchPoints > 0;
+}
+
+function parseWebsocketUrl(websocketRoute: string, extraParams: Record<string, any>) {
+	const accessToken = getAccessToken();
+	const urlWithTokenOnly = `${websocketBaseURL}ws/${websocketRoute}/?token=${accessToken}`
+
+	let websocketUrl = urlWithTokenOnly;
+
+	for (const paramName of Object.keys(extraParams)) {
+		const paramValue = extraParams[paramName];
+		const paramUrl = `&${paramName}=${paramValue}`
+
+		websocketUrl += paramUrl;
+	}
+
+	return websocketUrl;
 }
 
 export {
