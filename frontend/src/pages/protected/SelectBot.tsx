e316415ef7087d0d84d3e4e21465b5ee24a83ec4
Teddi <teddikao@gmail.com>
2025-04-26 21:09:11 +0800

    Adds bot selection interface
    Implements the bot selection interface by rendering bot categories based on the configuration.
    This allows users to choose from different bot categories to play against.

--- frontend/src/pages/protected/SelectBot.tsx
+++ frontend/src/pages/protected/SelectBot.tsx
@@ -1,9 +1,14 @@
+import { bots as botsTable } from "../../constants/botsConfig";
+import BotCategory from "../../pageComponents/playBot/BotCategory";
+
 function SelectBot() {
-    return (
-        <div className="select-bot-interface-container">
-            
-        </div>
-    )
+	return (
+		<div className="select-bot-interface-container">
+			{botsTable.map((categoryInfo, _) => (
+				<BotCategory categoryInfo={categoryInfo} />
+			))}
+		</div>
+	);
 }
 
 export default SelectBot;
