93fcbc9b66f847555ef53e2aa45533eb68920e43
Teddi <teddikao@gmail.com>
2025-04-26 21:07:36 +0800

    Creates bot category and info components
    Introduces reusable components for displaying bot categories
    and individual bot information. This allows for dynamic
    rendering of bots within a specific category.

---
+++ frontend/src/pageComponents/playBot/BotCategory.tsx
@@ -0,0 +1,22 @@
+import { BotCategoryInfo } from "../../constants/botsConfig";
+import BotCard from "./BotInfo";
+
+type BotCategoryProps = {
+	categoryInfo: BotCategoryInfo;
+};
+
+function BotCategory({ categoryInfo }: BotCategoryProps) {
+	const categoryName = categoryInfo["categoryName"];
+	const bots = categoryInfo["bots"];
+
+	return (
+		<div className="bot-category-container">
+			<h3 className="bot-category-name">{categoryName}</h3>
+			{bots.map((botInfo, _) => (
+				<BotCard botInfo={botInfo} />
+			))}
+		</div>
+	);
+}
+
+export default BotCategory;
