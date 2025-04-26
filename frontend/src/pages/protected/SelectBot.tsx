c992d189ad9df30ee37ed5e6e7cda05fe63e9fe8
Teddi <teddikao@gmail.com>
2025-04-26 21:18:43 +0800

    Enhances the bot selection interface
    Improves the bot selection page by adding a navigation bar and a heading.
    It also wraps the bot categories in a container.
    This provides better structure and visual appeal to the user interface.

--- frontend/src/pages/protected/SelectBot.tsx
+++ frontend/src/pages/protected/SelectBot.tsx
@@ -1,12 +1,19 @@
 import { bots as botsTable } from "../../constants/botsConfig";
+import DashboardNavbar from "../../pageComponents/dashboard/DashboardNavbar";
 import BotCategory from "../../pageComponents/playBot/BotCategory";
+
+import "../../styles/pages/select-bot.scss";
 
 function SelectBot() {
 	return (
 		<div className="select-bot-interface-container">
-			{botsTable.map((categoryInfo, _) => (
-				<BotCategory categoryInfo={categoryInfo} />
-			))}
+            <DashboardNavbar />
+            <h2 className="select-bot-page-heading">Play vs bot</h2>
+			<div className="bots-container">
+				{botsTable.map((categoryInfo, _) => (
+					<BotCategory categoryInfo={categoryInfo} />
+				))}
+			</div>
 		</div>
 	);
 }
