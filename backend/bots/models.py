50c26c30e3d74fab8d31d3ab1a6fa33c156502ae
Teddi <teddikao@gmail.com>
2025-04-27 16:27:16 +0800

    Creates BotGame model and related migrations
    Initializes the BotGame model with fields to represent a chess game
    state, including board placement, castling rights, move history, and
    player clocks. Also includes the related migrations to create the table
    in the database. Sets default values for JSON fields to ensure proper
    initialization, using the fen_parser utility.

--- backend/bots/models.py
+++ backend/bots/models.py
@@ -1,8 +1,34 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
+from gameplay.utils.fen_parser import get_default_position_list, get_starting_position_structured_fen, get_starting_structured_board_placement, get_starting_structured_castling_rights
+
+from core.constants import EMPTY_LIST, EMPTY_DICT
+
 class BotGame(models.Model):
     human_player = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     bot = models.CharField(max_length=150, null=False, blank=False)
+    white_player = models.CharField(max_length=5, null=False, blank=False) # Human or bot
+    black_player = models.CharField(max_length=5, null=False, blank=False) # Human or bot
 
-    structured_board_placement = models.JSONField(null=False, blank=False)
+    structured_board_placement = models.JSONField(null=False, blank=False, default=get_starting_structured_board_placement)
+    structured_castling_rights = models.JSONField(null=False, blank=False, default=get_starting_position_structured_fen)
+    en_passant_target_square = models.IntegerField(null=True, blank=True)
+    current_move_number = models.IntegerField(null=False, blank=False, default=1)
+    current_player_turn = models.CharField(max_length=5, null=False, blank=False, default="white")
+    halfmove_clock = models.IntegerField(null=False, blank=False, default=0)
+
+    game_winner = models.CharField(max_length=5, null=True, blank=True) # Human or bot
+    game_ended = models.BooleanField(null=False, blank=False, default=False)
+    game_ended_cause = models.CharField(max_length=50, null=True, blank=True)
+
+    white_player_clock = models.DecimalField(max_digits=7, decimal_places=1, null=True, blank=True)
+    black_player_clock = models.DecimalField(max_digits=7, decimal_places=1, null=True, blank=True)
+    white_player_increment = models.IntegerField(null=False, blank=False, default=0)
+    black_player_increment = models.IntegerField(null=False, blank=False, default=0)
+
+    position_list = models.JSONField(null=False, blank=False, default=get_default_position_list)
+    move_list = models.JSONField(null=False, blank=False, default=EMPTY_LIST)
+
+    captured_white_material = models.JSONField(null=False, blank=False, default=EMPTY_DICT)
+    captured_black_material = models.JSONField(null=False, blank=False, default=EMPTY_DICT)
