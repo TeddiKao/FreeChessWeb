bdc3938e1e0dd89ceba1431e09be03be41a867a4
Teddi <teddikao@gmail.com>
2025-04-27 15:03:33 +0800

    Adds PlayBot page
    Creates the PlayBot page with a chessboard component.
    
    Fetches and displays the initial board state using FEN notation.

---
+++ frontend/src/pages/protected/PlayBot.tsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import BotChessboard from "../../globalComponents/chessboards/BotChessboard";
+import DashboardNavbar from "../../pageComponents/dashboard/DashboardNavbar";
+import { fetchFen } from "../../utils/apiUtils";
+import { ParsedFENString } from "../../types/gameLogic";
+
+function PlayBot() {
+    const startingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
+    const [parsedFEN, setParsedFEN] = useState<ParsedFENString | null>(null);
+
+    useEffect(() => {
+        updateParsedFEN();
+    }, []);
+
+    async function updateParsedFEN() {
+        const parsedFEN = await fetchFen(startingFEN);
+
+        return parsedFEN;
+    }
+
+    if (!parsedFEN) {
+        return null;
+    }
+
+	return (
+		<>
+            <DashboardNavbar />
+			<div className="play-bot-interface-container">
+                <BotChessboard parsed_fen_string={parsedFEN} orientation="white" />
+            </div>
+		</>
+	);
+}
