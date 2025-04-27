3844786bf4f0f55e94514cf5c4e4468666f4afef
Teddi <teddikao@gmail.com>
2025-04-28 07:34:35 +0800

    Adds board orientation and settings modal
    Implements functionality for flipping the chessboard orientation.
    
    Introduces a settings modal for gameplay customization.

--- frontend/src/pages/protected/PlayBot.tsx
+++ frontend/src/pages/protected/PlayBot.tsx
@@ -2,35 +2,83 @@ import { useEffect, useState } from "react";
 import BotChessboard from "../../globalComponents/chessboards/BotChessboard";
 import DashboardNavbar from "../../pageComponents/dashboard/DashboardNavbar";
 import { fetchFen } from "../../utils/apiUtils";
-import { ParsedFENString } from "../../types/gameLogic";
+import { ParsedFENString, PieceColor } from "../../types/gameLogic";
 
 import "../../styles/pages/play-bot.scss";
+import useGameplaySettings from "../../hooks/useGameplaySettings";
+import GameplaySettings from "../../globalComponents/modals/GameplaySettings";
+import ModalWrapper from "../../globalComponents/wrappers/ModalWrapper";
 
 function PlayBot() {
-    const startingFEN = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1"
-    const [parsedFEN, setParsedFEN] = useState<ParsedFENString | null>(null);
+	const startingFEN =
+		"rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+	const [parsedFEN, setParsedFEN] = useState<ParsedFENString | null>(null);
+	const [boardOrientation, setBoardOrientation] = useState<string>("White");
 
-    useEffect(() => {
-        updateParsedFEN();
-    }, []);
+	const initialGameplaySettings = useGameplaySettings();
+	const [gameplaySettings, setGameplaySettings] = useState<boolean>(false);
+	const [gameplaySettingsVisible, setGameplaySettingsVisible] =
+		useState<boolean>(false);
 
-    async function updateParsedFEN() {
-        const parsedFEN = await fetchFen(startingFEN);
-        setParsedFEN(parsedFEN)
-    }
+	useEffect(() => {
+		updateParsedFEN();
+	}, []);
 
-    if (!parsedFEN) {
-        return null;
-    }
+	async function updateParsedFEN() {
+		const parsedFEN = await fetchFen(startingFEN);
+		setParsedFEN(parsedFEN);
+	}
+
+	function toggleBoardOrientation() {
+		const newOrientation =
+			boardOrientation.toLowerCase() === "white" ? "Black" : "White";
+		setBoardOrientation(newOrientation);
+	}
+
+	function handleSettingsDisplay() {
+		setGameplaySettingsVisible(true);
+	}
+
+	function handleSettingsModalClose() {
+		setGameplaySettingsVisible(false);
+	}
+
+	if (!parsedFEN) {
+		return null;
+	}
 
 	return (
 		<>
-            <DashboardNavbar />
+			<DashboardNavbar />
 			<div className="play-bot-interface-container">
-                <div className="bot-chessboard-wrapper">
-                    <BotChessboard squareSize={58} parsed_fen_string={parsedFEN} orientation="White" />
-                </div>
-            </div>
+				<div className="bot-chessboard-wrapper">
+					<BotChessboard
+						squareSize={58}
+						parsed_fen_string={parsedFEN}
+						orientation={boardOrientation}
+					/>
+				</div>
+
+				<div className="board-actions">
+					<img
+						onClick={toggleBoardOrientation}
+						className="flip-board-icon"
+						src="/flip-board-icon.png"
+					/>
+					<img
+						className="settings-icon"
+						src="/settings.svg"
+						onClick={handleSettingsDisplay}
+					/>
+				</div>
+			</div>
+
+			<ModalWrapper visible={gameplaySettingsVisible}>
+				<GameplaySettings
+					onClose={handleSettingsModalClose}
+					setGameplaySettings={setGameplaySettings}
+				/>
+			</ModalWrapper>
 		</>
 	);
 }
