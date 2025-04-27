2f0153aaaf35a87481daba7264625cf0fa3771d7
Teddi <teddikao@gmail.com>
2025-04-27 14:55:57 +0800

    Adds BotChessboard component Creates a new React component responsible for rendering the chessboard for the bot's perspective.
    It dynamically generates the chessboard based on the provided FEN string and orientation prop, displaying the pieces in their correct positions with appropriate styling.

---
+++ frontend/src/globalComponents/chessboards/BotChessboard.tsx
@@ -0,0 +1,65 @@
+import { BotChessboardProps } from "../../interfaces/chessboard";
+
+function BotChessboard({ parsed_fen_string, orientation }: BotChessboardProps) {
+	function generateChessboard() {
+		const squareElements = [];
+
+		const startingRow = orientation === "White" ? 8 : 1;
+		const endingRow = orientation === "White" ? 1 : 8;
+
+		for (
+			let row = startingRow;
+			orientation === "White" ? row >= endingRow : row <= endingRow;
+			orientation === "White" ? row-- : row++
+		) {
+			const startingIndex = (row - 1) * 8 + 1;
+			const endingIndex = row * 8;
+
+			for (let square = startingIndex; square <= endingIndex; square++) {
+				const boardPlacement = parsed_fen_string["board_placement"];
+				const squaresArray = Object.keys(boardPlacement);
+
+				const file = square - startingIndex + 1;
+				const squareIsLight = (file + row) % 2 !== 0;
+
+				const squareColor = squareIsLight ? "light" : "dark";
+
+				if (squaresArray.includes(`${square - 1}`)) {
+					const pieceType =
+						boardPlacement[`${square - 1}`]["piece_type"];
+					const pieceColor =
+						boardPlacement[`${square - 1}`][
+							"piece_color"
+						].toLowerCase();
+
+					squareElements.push(
+						<div
+							key={square}
+							id={`${square}`}
+							className={`chessboard-square ${squareColor}`}
+						>
+							<img
+								src={`/${pieceColor}${pieceType}.svg`}
+								className="piece-image"
+							/>
+						</div>
+					);
+				} else {
+					squareElements.push(
+						<div
+							key={square}
+							id={`${square}`}
+							className={`chessboard-square ${squareColor}`}
+						></div>
+					);
+				}
+			}
+		}
+
+		return squareElements;
+	}
+
+	return <div className="chessboard-container">{generateChessboard()}</div>;
+}
+
+export default BotChessboard;
