616a550536525f76ee82415f2897e09715cd7658
Teddi <teddikao@gmail.com>
2025-04-28 17:02:26 +0800

    Adds endpoint for making a move
    Adds a new endpoint that allows the client to
    submit a move for a bot game.

--- backend/bots/urls.py
+++ backend/bots/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CreateBotGameView
+from .views import CreateBotGameView, MakeMoveView
 
 urlpatterns = [
-    path("create-bot-game/", CreateBotGameView.as_view(), name="create_bot_game")
+    path("create-bot-game/", CreateBotGameView.as_view(), name="create_bot_game"),
+    path("make-move/", MakeMoveView.as_view(), name="make_move")
 ]
