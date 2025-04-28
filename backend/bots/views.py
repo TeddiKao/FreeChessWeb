6f59fe202ed534e84590bd8f6604c41e11a1162c
Teddi <teddikao@gmail.com>
2025-04-28 17:01:44 +0800

    Adds endpoint to make moves in bot games
    Implements an API endpoint that handles move requests
    for bot games. It validates the move and updates the
    game state accordingly.

--- backend/bots/views.py
+++ backend/bots/views.py
@@ -1,3 +1,5 @@
+import copy
+
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework import status
@@ -5,6 +7,8 @@ from rest_framework.response import Response
 
 from .models import BotGame
 from core.utils import decide_bot_game_color
+from move_validation.utils.move_validation import validate_move
+from gameplay.utils.position_update import update_structured_fen
 
 class CreateBotGameView(APIView):
     def post(self, request):
@@ -22,5 +26,31 @@ class CreateBotGameView(APIView):
             return Response({
                 "game_id": bot_game_id,
             }, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
+
+class MakeMoveView(APIView):
+    def post(self, request):
+        try:
+            move_info = request.data.get("move_info")
+            game_id = request.data.get("game_id")
+            
+            bot_game: BotGame = BotGame.objects.first(id=game_id)
+            current_structured_fen = bot_game.get_full_structured_fen()
+
+            is_move_valid = validate_move(current_structured_fen, move_info)
+            if not is_move_valid:
+                return Response({
+                    "is_valid": False
+                }, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+            else:
+                new_structured_fen = update_structured_fen(current_structured_fen, move_info)
+                bot_game.update_full_structured_fen(new_structured_fen)
+
+                return Response({
+                    "is_valid": True,
+                    "new_structured_fen": bot_game.get_full_structured_fen()
+                })
+            
         except Exception as e:
             print(e)
