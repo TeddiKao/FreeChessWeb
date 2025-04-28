3337139e9d39d13b9b90546874ec197dc94c0b60
Teddi <teddikao@gmail.com>
2025-04-28 08:46:04 +0800

    Adds endpoint for creating bot games
    Implements a new API endpoint that allows users to
    create new chess games against a bot. The endpoint
    handles the creation of the game record including
    determining the colors for each player.

--- backend/bots/views.py
+++ backend/bots/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
 
-# Create your views here.
+from gameplay.models import ChessGame
+from core.utils import decide_bot_game_color
+
+class CreateBotGameView(APIView):
+    def post(self, request):
+        bot_to_play_against = request.data.get("bot")
+        colors = decide_bot_game_color()
+
+        bot_game_id = ChessGame.objects.create(
+            human_player=request.user,
+            bot=bot_to_play_against,
+            white_player=colors["white"],
+            black_player=colors["black"],
+        )
+
+        return {
+            "game_id": bot_game_id,
+        }
