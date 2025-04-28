275764874c37206fac2e9d36bf7e07c75f16cd71
Teddi <teddikao@gmail.com>
2025-04-28 21:26:32 +0800

    Adds position list update functionality
    Implements the function to update the position list based on new move information and the resulting position.
    
    Includes logic to determine the move type and record starting/destination squares.

--- backend/gameplay/utils/game_state_history_update.py
+++ backend/gameplay/utils/game_state_history_update.py
@@ -1,6 +1,7 @@
 import copy
 
 from gameplay.utils.algebraic_notation_parser import get_algebraic_notation
+from move_validation.utils.get_move_type import get_move_type
 
 def update_move_list(current_structured_fen, current_move_list, new_move_info):
     updated_move_list: list[list[str]] = copy.deepcopy(current_move_list)
@@ -15,6 +16,26 @@ def update_move_list(current_structured_fen, current_move_list, new_move_info):
     else:
         updated_move_list[-1].append(algebraic_notation)
 
+    return updated_move_list
 
-def update_position_list(current_position_list, new_move, new_position):
-    pass
+
+def update_position_list(current_position_list, new_move_info, new_position):
+    updated_position_list: list = copy.deepcopy(current_position_list)
+    current_position = current_position_list[-1]["position"]
+
+    current_structured_board_placement = current_position["board_placement"]
+    current_en_passant_target_square = current_position["en_passant_target_square"]
+
+    move_type = get_move_type(current_structured_board_placement, current_en_passant_target_square, new_move_info)
+    starting_square = new_move_info["starting_square"]
+    destination_square = new_move_info["destination_square"]
+    
+    updated_position_list.append({
+        "position": new_position,
+        "last_dragged_square": starting_square,
+        "last_dropped_square": destination_square,
+        "move_type": move_type
+    })
+
+    return updated_position_list
+
