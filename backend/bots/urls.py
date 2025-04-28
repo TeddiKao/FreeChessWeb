d7771576512d40ab6094a3838e1e9710ccbb2a91
Teddi <teddikao@gmail.com>
2025-04-28 12:33:06 +0800

    Fixes URL patterns definition
    Corrects the URL patterns definition by changing `urls` to `urlpatterns`.
    
    This ensures that the URL patterns are correctly registered and accessible within the Django application.

--- backend/bots/urls.py
+++ backend/bots/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import CreateBotGameView
 
-urls = [
-    path("/create-bot-game/", CreateBotGameView.as_view(), name="create_bot_game")
+urlpatterns = [
+    path("create-bot-game/", CreateBotGameView.as_view(), name="create_bot_game")
 ]
