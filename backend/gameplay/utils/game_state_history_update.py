83d6e4e7a88fe335a4dba55686a2e86d272bbfcc
Teddi <teddikao@gmail.com>
2025-04-28 21:18:35 +0800

    Updates move list with algebraic notation
    Calculates and appends the algebraic notation of a move
    to the move list, incorporating board placement and
    en passant information. This provides a human-readable
    representation of each move.

--- backend/gameplay/utils/game_state_history_update.py
+++ backend/gameplay/utils/game_state_history_update.py
@@ -1,5 +1,20 @@
-def update_move_list(current_move_list, new_move):
-    pass
+import copy
+
+from gameplay.utils.algebraic_notation_parser import get_algebraic_notation
+
+def update_move_list(current_structured_fen, current_move_list, new_move_info):
+    updated_move_list: list[list[str]] = copy.deepcopy(current_move_list)
+    
+    structured_board_placement = current_structured_fen["board_placement"]
+    en_passant_target_square = current_structured_fen["en_passant_target_square"]
+
+    algebraic_notation = get_algebraic_notation(structured_board_placement, en_passant_target_square, new_move_info)
+    
+    if len(updated_move_list[-1]) == 2:
+        updated_move_list.append([algebraic_notation])
+    else:
+        updated_move_list[-1].append(algebraic_notation)
+
 
 def update_position_list(current_position_list, new_move, new_position):
     pass
