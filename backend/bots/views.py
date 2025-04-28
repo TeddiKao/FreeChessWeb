abe9763772701db105d3d236b91baa3a323a6db3
Teddi <teddikao@gmail.com>
2025-04-28 13:02:03 +0800

    Creates bot game via API endpoint
    Creates a bot game instance using the provided data.
    
    Improves the API endpoint by adding try/except to catch errors, returns a response with the game ID and uses appropriate HTTP status codes.
    
    Uses BotGame model instead of ChessGame model to create the game instance.

--- backend/bots/views.py
+++ backend/bots/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework import status
+from rest_framework.response import Response
 
-from gameplay.models import ChessGame
+from .models import BotGame
 from core.utils import decide_bot_game_color
 
 class CreateBotGameView(APIView):
     def post(self, request):
-        bot_to_play_against = request.data.get("bot")
-        colors = decide_bot_game_color()
+        try:
+            bot_to_play_against = request.data.get("bot")
+            colors = decide_bot_game_color()
 
-        bot_game_id = ChessGame.objects.create(
-            human_player=request.user,
-            bot=bot_to_play_against,
-            white_player=colors["white"],
-            black_player=colors["black"],
-        )
+            bot_game_id = BotGame.objects.create(
+                human_player=request.user,
+                bot=bot_to_play_against,
+                white_player=colors["white"],
+                black_player=colors["black"],
+            )
 
-        return {
-            "game_id": bot_game_id,
-        }
+            print("Successfuly created game!")
+
+            return Response({
+                "game_id": bot_game_id,
+            }, status=status.HTTP_200_OK)
+        except Exception as e:
+            print(e)
