841a09cf111c8ef0b53ba3b3c8718491e2f82e9f
Teddi <teddikao@gmail.com>
2025-05-01 13:08:51 +0800

    Retrieves game ID from query string
    Retrieves the game ID from the query string during the websocket connection.
    
    This allows the consumer to identify the specific game associated with the websocket connection.

--- backend/bots/consumers.py
+++ backend/bots/consumers.py
@@ -2,4 +2,9 @@ from channels.generic.websocket import AsyncWebsocketConsumer
 
 class BotGameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        return await super().connect()
+        query_string: bytes = self.scope.get("query_string", b"")
+        decoded_query_string = query_string.decode()
+
+        game_id = decoded_query_string.get("gameId")[0]
+
+        await self.accept()
