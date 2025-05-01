e89224d3b47a279561a481b449fed967d6dcb12f
Teddi <teddikao@gmail.com>
2025-05-01 13:21:12 +0800

    Rename WebSocket URL for bot game server

--- backend/bots/routing.py
+++ backend/bots/routing.py
@@ -2,5 +2,5 @@ from django.urls import re_path
 from .consumers import BotGameConsumer
 
 websocket_urlpatterns = [
-    re_path("/bot-game-server/", BotGameConsumer.as_asgi())
+    re_path("ws/bot-game-server/", BotGameConsumer.as_asgi())
 ]
