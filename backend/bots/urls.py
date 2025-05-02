4dbe4f98a0e77c2ec1f8512abdb6541eefec5ea4
Teddi <teddikao@gmail.com>
2025-05-02 11:01:36 +0800

    Delete unused views in bots app and update URLs accordingly

--- backend/bots/urls.py
+++ backend/bots/urls.py
@@ -1,9 +1,8 @@
 from django.urls import path
-from .views import CreateBotGameView, MakeMoveView, GetBotGamePositionListView, GetBotGameMoveListView
+from .views import CreateBotGameView, GetBotGamePositionListView, GetBotGameMoveListView
 
 urlpatterns = [
     path("create-bot-game/", CreateBotGameView.as_view(), name="create_bot_game"),
-    path("make-move/", MakeMoveView.as_view(), name="make_move"),
     path("get-position-list/", GetBotGamePositionListView.as_view(), name="get_bot_game_position_list"),
     path("get-move-list/", GetBotGameMoveListView.as_view(), name="get_bot_game_move_list")
 ]
