02682b99675f02bdd41382a26040a8f3f555b569
Teddi <teddikao@gmail.com>
2025-05-02 11:18:58 +0800

    Fix typo in enum members for bot game

--- frontend/src/enums/gameLogic.ts
+++ frontend/src/enums/gameLogic.ts
@@ -20,8 +20,8 @@ enum GameplayWebSocketEventTypes {
 enum BotGameWebSocketEventTypes {
 	MOVE_REGISTERED = "move_registered",
 	CHECKMATE_OCCURRED = "checkmate_occurred",
-	STALEMATE_OCCURED = "stalemate_occurred",
-	THREEFOLD_REPETITION_OCCURED = "threefold_repetition_occurred",
+	STALEMATE_OCCURRED = "stalemate_occurred",
+	THREEFOLD_REPETITION_OCCURRED = "threefold_repetition_occurred",
 	FIFTY_MOVE_RULE_REACHED = "50_move_rule_reached"
 }
 
