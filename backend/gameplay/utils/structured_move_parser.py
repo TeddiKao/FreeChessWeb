b4653de6b489dfe0ee8215c33d730fdc8ec124b5
Teddi <teddikao@gmail.com>
2025-05-03 07:42:52 +0800

    Complete function to convert universal notation move into a dictionary of move info

--- backend/gameplay/utils/structured_move_parser.py
+++ backend/gameplay/utils/structured_move_parser.py
@@ -1,11 +1,19 @@
 from move_validation.utils.general import convert_to_square_index
 
-def parse_structured_move(structured_fen, universal_notation):
-    board_placement = structured_fen["board_placement"]
-    en_passant_target_square = structured_fen["en_passant_target_square"]
-    
+def parse_structured_move(board_placement, universal_notation):
     starting_square_universal = universal_notation[:2]
     destination_square_universal = universal_notation[2:]
+    
     starting_square_index = convert_to_square_index(starting_square_universal)
     destination_square_index = convert_to_square_index(destination_square_universal)
+    
+    piece_moved_info = board_placement.get(str(starting_square_index))
+    piece_moved_type = piece_moved_info["piece_type"]
+    piece_moved_color = piece_moved_info["piece_color"]
 
+    return {
+        "starting_square": starting_square_index,
+        "destination_square": destination_square_index,
+        "piece_type": piece_moved_type,
+        "piece_color": piece_moved_color
+    }
