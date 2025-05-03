28f2f11f51ea5d6d6a4d1a95ad3a41e9267ebfbf
Teddi <teddikao@gmail.com>
2025-05-03 09:33:00 +0800

    Add enum member for when bot makes a move

--- frontend/src/enums/gameLogic.ts
+++ frontend/src/enums/gameLogic.ts
@@ -22,7 +22,8 @@ enum BotGameWebSocketEventTypes {
 	CHECKMATE_OCCURRED = "checkmate_occurred",
 	STALEMATE_OCCURRED = "stalemate_occurred",
 	THREEFOLD_REPETITION_OCCURRED = "threefold_repetition_occurred",
-	FIFTY_MOVE_RULE_REACHED = "50_move_rule_reached"
+	FIFTY_MOVE_RULE_REACHED = "50_move_rule_reached",
+	BOT_MOVE_MADE = "bot_move_made",
 }
 
 enum ActionWebSocketEventTypes {
