ebd48fb71b637d0a601c6e3d9a1fa0c475e484e9
Teddi <teddikao@gmail.com>
2025-05-03 20:34:18 +0800

    Optimise bot playing logic by only creating stockfish engine once

--- backend/bots/consumers.py
+++ backend/bots/consumers.py
@@ -1,4 +1,5 @@
 import json
+import time
 
 from urllib.parse import parse_qs
 from channels.generic.websocket import AsyncWebsocketConsumer
@@ -102,6 +103,8 @@ class BotGameConsumer(AsyncWebsocketConsumer):
 		await self.accept()
 
 		self.game_id = game_id
+		self.stockfish_engine = initialise_stockfish_instance()
+
 		bot_game_model: BotGame = await BotGame.async_get_bot_game_from_id(self.game_id)
 		current_player_turn = await bot_game_model.async_get_game_attr("current_player_turn")
 		
@@ -110,29 +113,46 @@ class BotGameConsumer(AsyncWebsocketConsumer):
 			await self.make_bot_move()
 
 	async def make_bot_move(self):
-		
+		bot_move_process_start = time.perf_counter()
 
 		bot_game_model: BotGame = await BotGame.async_get_bot_game_from_id(self.game_id)
-		stockfish_engine = initialise_stockfish_instance()
-
 		
+		stockfish_init_start = time.perf_counter()
+		stockfish_init_end = time.perf_counter()
+
+		print(f"Stockfish engine initialisation took {(stockfish_init_end - stockfish_init_start):.6f} seconds")
 
 		current_structured_fen = await bot_game_model.async_get_full_structured_fen()
 		current_board_placement = current_structured_fen["board_placement"]
 		current_en_passant_target_square = current_structured_fen["en_passant_target_square"]
 		
+		raw_fen_parsing_tart = time.perf_counter()
 		raw_fen = parse_raw_fen(current_structured_fen)
-		stockfish_engine.set_fen_position(raw_fen)
+		raw_fen_parsing_end = time.perf_counter()
 
-		best_move = stockfish_engine.get_best_move()
+		print(f"Raw fen parsing took {(raw_fen_parsing_end - raw_fen_parsing_tart):.6f} seconds")
+
+		self.stockfish_engine.set_fen_position(raw_fen)
+
+		best_move = self.stockfish_engine.get_best_move()
+
 		structured_move_info = parse_structured_move(current_board_placement, best_move)
 
 		if not validate_move(current_structured_fen, structured_move_info):
 			return
 
+		game_state_update_start = time.perf_counter()
 		updated_structured_fen, updated_move_list, updated_position_list = await self.get_updated_game_state(bot_game_model, structured_move_info)
+		game_state_update_end = time.perf_counter()
 
+		print(f"Game state update took {(game_state_update_end - game_state_update_start):.6f} seconds")
+
+		result_check_start = time.perf_counter()
 		await self.check_for_results(bot_game_model, structured_move_info["piece_color"])
+		result_check_end = time.perf_counter()
+
+		print(f"Result check took {(result_check_end - result_check_start):.6f} seconds")
+		
 		await self.send(json.dumps({
 			"type": "bot_move_made",
 			"new_structured_fen": updated_structured_fen,
@@ -140,6 +160,9 @@ class BotGameConsumer(AsyncWebsocketConsumer):
 			"new_move_list": updated_move_list,
 			"move_type": get_move_type(current_board_placement, current_en_passant_target_square, structured_move_info)
 		}))
+
+		bot_move_process_end = time.perf_counter()
+		print(f"Bot move process took {(bot_move_process_end - bot_move_process_start):.6f} seconds")
 
 	async def receive(self, text_data=None, bytes_data=None):
 		parsed_text_data = json.loads(text_data)
