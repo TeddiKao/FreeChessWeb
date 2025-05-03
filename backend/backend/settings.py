5b11f99f56f0a0dc819d60dfb45f25e741e676b2
Teddi <teddikao@gmail.com>
2025-05-03 15:30:56 +0800

    Add print statement to check Stockfish path

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -167,6 +167,8 @@ if platform.system() == "Windows":
     STOCKFISH_PATH = str(Path(PROJECT_ROOT_DIR) / "engines" / "stockfish-windows.exe")
 else:
     STOCKFISH_PATH = str(Path(PROJECT_ROOT_DIR) / "engines" / "stockfish-linux")
+
+print(f"Stockfish path: {STOCKFISH_PATH}")
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
