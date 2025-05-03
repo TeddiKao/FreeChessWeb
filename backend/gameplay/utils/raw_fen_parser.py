5c9e2224e4bda29bf35b3c38ccc87654e015f94c
Teddi <teddikao@gmail.com>
2025-05-03 09:55:55 +0800

    Make piece abbrivieations the keys of the dictionary

--- backend/gameplay/utils/raw_fen_parser.py
+++ backend/gameplay/utils/raw_fen_parser.py
@@ -1,12 +1,12 @@
 from move_validation.utils.general import convert_to_algebraic_notation
 
 piece_letter_mapping = {
-    "pawn": "p",
-    "knight": "n",
-    "bishop": "b",
-    "rook": "r",
-    "queen": "q",
-    "king": "k"
+    "p": "pawn",
+    "n": "knight",
+    "b": "bishop",
+    "r": "rook",
+    "q": "queen",
+    "k": "king"
 }
 
 castling_mapping = {
