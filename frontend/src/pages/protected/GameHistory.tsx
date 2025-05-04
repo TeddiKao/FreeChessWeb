cf23d64a6933c7a9b9301f34270eafdb65131b17
Teddi <teddikao@gmail.com>
2025-05-04 21:06:46 +0800

    Add basic styling for GameHistory page

--- frontend/src/pages/protected/GameHistory.tsx
+++ frontend/src/pages/protected/GameHistory.tsx
@@ -1,10 +1,17 @@
 import DashboardNavbar from "../../components/page/dashboard/DashboardNavbar";
 
+import "../../styles/pages/game-history.scss";
+
 function GameHistory() {
 	return (
 		<>
             <DashboardNavbar />
-			<div className="game-history-interface-container"></div>
+			<div className="game-history-interface-container">
+                <h2 className="game-history-header">Game History</h2>
+                <div className="completed-games-container">
+                    
+                </div>
+            </div>
 		</>
 	);
 }
