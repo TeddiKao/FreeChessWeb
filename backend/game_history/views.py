f8d507291924eeabf635aed1423390fbc5755d91
Teddi <teddikao@gmail.com>
2025-05-05 09:20:03 +0800

    Complete view for fetching completed games

--- backend/game_history/views.py
+++ backend/game_history/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.db.models import Q
+
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+from gameplay.models import ChessGame
+from core.utils import serialize_models_including_fields
 
 # Create your views here.
 class FetchCompletedGamesView(APIView):
-    pass
+    def post(self, request):
+        user = request.user
+        fields_to_include = ["white_player", "black_player", "game_winner"]
+
+        games_played = ChessGame.objects.filter(Q(white_player=user) | Q(black_player=user))
+        completed_games = games_played.filter(game_status="Ended")
+        serialized_completed_games = serialize_models_including_fields(completed_games, fields_to_include)
+
+        return Response(serialized_completed_games, status=status.HTTP_200_OK)
+        
