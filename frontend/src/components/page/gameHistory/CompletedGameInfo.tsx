a76e804c4486ceb1dfb9a5128ff247f353363451
Teddi <teddikao@gmail.com>
2025-05-05 12:27:11 +0800

    Add classes to white and black player info to use for styling in the future

--- frontend/src/components/page/gameHistory/CompletedGameInfo.tsx
+++ frontend/src/components/page/gameHistory/CompletedGameInfo.tsx
@@ -9,8 +9,8 @@ function CompletedGameInfo({ gameInfo }: CompletedGameInfoProps) {
 	return (
 		<div className="completed-game-info-container">
 			<div className="players-container">
-				<p>White player: {gameInfo.white_player}</p>
-				<p>Black player: {gameInfo.black_player}</p>
+				<p className="white-player-name">White player: {gameInfo.white_player}</p>
+				<p className="black-player-name">Black player: {gameInfo.black_player}</p>
 			</div>
 
 			<p>Result: {gameInfo.game_winner}</p>
