00b063a7314b940b80a9e5c735032e1091769cf3
Teddi <teddikao@gmail.com>
2025-05-05 09:22:34 +0800

    Rename URL to better reflect purpose

--- backend/game_history/urls.py
+++ backend/game_history/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from .views import FetchCompletedGamesView
 
 urlpatterns = [
-    path("get-game-history/", FetchCompletedGamesView.as_view(), name="fetch_completed_games")
+    path("get-completed-games/", FetchCompletedGamesView.as_view(), name="fetch_completed_games")
 ]
