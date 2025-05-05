b8164f95ec76787c79b852c8917a9f59161507d6
Teddi <teddikao@gmail.com>
2025-05-05 12:17:32 +0800

    Add basic styling for CompletedGameInfo component to define layout

--- frontend/src/components/page/gameHistory/CompletedGameInfo.tsx
+++ frontend/src/components/page/gameHistory/CompletedGameInfo.tsx
@@ -1,16 +1,21 @@
+import "../../../styles/features/gameHistory/completed-game-info.scss";
+
 type CompletedGameInfoProps = {
-    gameInfo: any,
-    username: string,
-}
+	gameInfo: any;
+	username: string;
+};
 
 function CompletedGameInfo({ gameInfo }: CompletedGameInfoProps) {
-    return (
-        <div className="completed-game-info-container">
-            <p>White player: {gameInfo.white_player}</p>
-            <p>Black player: {gameInfo.black_player}</p>
-            <p>Result: {gameInfo.game_winner}</p>
-        </div>
-    )
+	return (
+		<div className="completed-game-info-container">
+			<div className="players-container">
+				<p>White player: {gameInfo.white_player}</p>
+				<p>Black player: {gameInfo.black_player}</p>
+			</div>
+
+			<p>Result: {gameInfo.game_winner}</p>
+		</div>
+	);
 }
 
 export default CompletedGameInfo;
