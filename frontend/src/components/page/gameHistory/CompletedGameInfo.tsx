e057776a256fc8aaa4eb9388806a28b46c8d4a74
Teddi <teddikao@gmail.com>
2025-05-05 10:00:33 +0800

    Add basic markup for testing purposes

--- frontend/src/components/page/gameHistory/CompletedGameInfo.tsx
+++ frontend/src/components/page/gameHistory/CompletedGameInfo.tsx
@@ -1,10 +1,15 @@
 type CompletedGameInfoProps = {
-    gameInfo: any
+    gameInfo: any,
+    username: string,
 }
 
 function CompletedGameInfo({ gameInfo }: CompletedGameInfoProps) {
     return (
-        <div className="completed-game-info-container"></div>
+        <div className="completed-game-info-container">
+            <p>White player: {gameInfo.white_player}</p>
+            <p>Black player: {gameInfo.black_player}</p>
+            <p>Result: {gameInfo.game_winner}</p>
+        </div>
     )
 }
 
