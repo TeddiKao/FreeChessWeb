e012a3cca0f98d51aff4cdd9536ace2da9ae72f8
Teddi <teddikao@gmail.com>
2025-05-05 09:19:45 +0800

    Add functions to serialize models by fields to include

--- backend/core/utils.py
+++ backend/core/utils.py
@@ -3,7 +3,7 @@ from deepdiff import DeepDiff
 
 import random
 
-def serialize_model(model_instance: Model, exclude_fields: list[str] = None):
+def serialize_excluding_fields(model_instance: Model, exclude_fields: list[str] = None):
 	exclude_fields = exclude_fields or []
 
 	model_fields: list[Field] = model_instance._meta.fields
@@ -19,6 +19,40 @@ def serialize_model(model_instance: Model, exclude_fields: list[str] = None):
 		model_data[field_name] = field_value
 
 	return model_data
+
+def serialize_including_fields(model_instance: Model, include_fields: list[str]):
+	model_fields: list[Field] = model_instance._meta.fields
+	model_data = {}
+
+	for field in model_fields:
+		if field.name not in include_fields:
+			continue
+
+		field_name = field.name
+		field_value = getattr(model_instance, field_name)
+
+		model_data[field_name] = field_value
+
+	return model_data
+
+
+def serialize_models_excluding_fields(model_list: list[Model], exclude_fields: list[str] = None):
+	serialized_model_list = []	
+	
+	for model in model_list:
+		serialized_model = serialize_excluding_fields(model, exclude_fields)
+		serialized_model_list.append(serialized_model)
+
+	return serialized_model_list
+
+def serialize_models_including_fields(model_list: list[Model], include_fields: list[str]):
+	serialized_model_list = []	
+	
+	for model in model_list:
+		serialized_model = serialize_including_fields(model, include_fields)
+		serialized_model_list.append(serialized_model)
+
+	return serialized_model_list
 
 def compare_dictionaries(dict1: dict, dict2: dict):
 	return len(DeepDiff(dict1, dict2)) == 0
