e6ca0ae8543c142e30664aa25d7d8548b4cb501e
Teddi <teddikao@gmail.com>
2025-05-05 09:40:37 +0800

    Add logic to use completedGames and username when displaying user's game history

--- frontend/src/pages/protected/GameHistory.tsx
+++ frontend/src/pages/protected/GameHistory.tsx
@@ -1,15 +1,37 @@
+import { useEffect, useState } from "react";
 import DashboardNavbar from "../../components/page/dashboard/DashboardNavbar";
+import useCompletedGames from "../../hooks/useCompletedGames";
 
 import "../../styles/pages/game-history.scss";
+import CompletedGameInfo from "../../components/page/gameHistory/CompletedGameInfo";
+import useUsername from "../../hooks/useUsername";
+import { isNullOrUndefined } from "../../utils/generalUtils";
 
 function GameHistory() {
+    const initialCompletedGames = useCompletedGames();
+    const username = useUsername();
+
+    const [completedGames, setCompletedGames] = useState(initialCompletedGames);
+
+    useEffect(() => {
+        setCompletedGames(initialCompletedGames);
+    }, [initialCompletedGames]);
+
+    if (isNullOrUndefined(username) || isNullOrUndefined(completedGames)) {
+        return null;
+    }
+
 	return (
 		<>
             <DashboardNavbar />
 			<div className="game-history-interface-container">
                 <h2 className="game-history-header">Game History</h2>
                 <div className="completed-games-container">
-                    
+                    {completedGames.map((gameInfo, index) => {
+                        return (
+                            <CompletedGameInfo gameInfo={gameInfo} />
+                        )
+                    })}
                 </div>
             </div>
 		</>
