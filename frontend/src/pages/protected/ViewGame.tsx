ecc0c6d852e8a0e0883bad0ca49cff7c9064af1b
Teddi <teddikao@gmail.com>
2025-05-05 16:19:40 +0800

    Add additional content onto ViewGame page

--- frontend/src/pages/protected/ViewGame.tsx
+++ frontend/src/pages/protected/ViewGame.tsx
@@ -2,36 +2,66 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchMoveList, fetchPositionList } from "../../utils/apiUtils";
 import { ParsedFENString } from "../../types/gameLogic";
+import GameReplayChessboard from "../../components/global/chessboards/GameReplayChessboard";
 
 function ViewGame() {
-    const { gameId } = useParams();
+	const { gameId } = useParams();
 
-    const [positionList, setPositionList] = useState([]);
-    const [positionIndex, setPositionIndex] = useState(0);
-    const [moveList, setMoveList] = useState([]);
-    const parsedFEN: ParsedFENString = positionList[positionIndex]?.["position"];
+	const [boardOrientation, setBoardOrientation] = useState("White");
 
-    useEffect(() => {
-        updatePositionList();
-        updateMoveList();
-    }, []);
+	const [positionList, setPositionList] = useState([]);
+	const [positionIndex, setPositionIndex] = useState(0);
+	const [moveList, setMoveList] = useState([]);
 
-    async function updatePositionList() {
-        const fetchedPositionList = await fetchPositionList(Number(gameId));
-        setPositionList(fetchedPositionList);
-    }
+	const parsedFEN: ParsedFENString =
+		positionList[positionIndex]?.["position"];
+	const lastDraggedSquare =
+		positionList[positionIndex]?.["last_dragged_square"];
+	const lastDroppedSquare =
+		positionList[positionIndex]?.["last_dropped_square"];
 
-    async function updateMoveList() {
-        const fetchedMoveList = await fetchMoveList(Number(gameId));
-        setMoveList(fetchedMoveList);
-    }
+	useEffect(() => {
+		updatePositionList();
+		updateMoveList();
+	}, []);
 
+	async function updatePositionList() {
+		const fetchedPositionList = await fetchPositionList(Number(gameId));
+		setPositionList(fetchedPositionList);
+	}
 
-    return (
-        <div className="view-game-interface-container">
-            
-        </div>
-    )
+	async function updateMoveList() {
+		const fetchedMoveList = await fetchMoveList(Number(gameId));
+		setMoveList(fetchedMoveList);
+	}
+
+	function toggleBoardOrientation() {
+		const isWhite = boardOrientation.toLowerCase() === "white";
+		const newOrientation = isWhite ? "Black" : "White";
+
+		setBoardOrientation(newOrientation);
+	}
+
+	return (
+		<div className="view-game-interface-container">
+			<div className="view-game-chessboard-wrapper">
+				<GameReplayChessboard
+					orientation={boardOrientation}
+					parsed_fen_string={parsedFEN}
+					lastDraggedSquare={lastDraggedSquare}
+					lastDroppedSquare={lastDroppedSquare}
+				/>
+			</div>
+
+			<div className="board-actions">
+				<img
+					onClick={toggleBoardOrientation}
+					className="flip-board-icon"
+					src="/flip-board-icon.png"
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default ViewGame;
