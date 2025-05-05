81c59ba77c929309178dcfd0ee2c7132c1298b40
Teddi <teddikao@gmail.com>
2025-05-05 16:07:20 +0800

    Add basic logic for chessboard generation and move/position list fetching

--- frontend/src/pages/protected/ViewGame.tsx
+++ frontend/src/pages/protected/ViewGame.tsx
@@ -1,6 +1,36 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { fetchMoveList, fetchPositionList } from "../../utils/apiUtils";
+import { ParsedFENString } from "../../types/gameLogic";
+
 function ViewGame() {
+    const { gameId } = useParams();
+
+    const [positionList, setPositionList] = useState([]);
+    const [positionIndex, setPositionIndex] = useState(0);
+    const [moveList, setMoveList] = useState([]);
+    const parsedFEN: ParsedFENString = positionList[positionIndex]?.["position"];
+
+    useEffect(() => {
+        updatePositionList();
+        updateMoveList();
+    }, []);
+
+    async function updatePositionList() {
+        const fetchedPositionList = await fetchPositionList(Number(gameId));
+        setPositionList(fetchedPositionList);
+    }
+
+    async function updateMoveList() {
+        const fetchedMoveList = await fetchMoveList(Number(gameId));
+        setMoveList(fetchedMoveList);
+    }
+
+
     return (
-        <div className="view-game-interface-container"></div>
+        <div className="view-game-interface-container">
+            
+        </div>
     )
 }
 
