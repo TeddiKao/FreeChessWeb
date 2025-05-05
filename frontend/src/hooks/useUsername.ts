4f23b1d27365638216739c3c67dcfb42462f2fe4
Teddi <teddikao@gmail.com>
2025-05-05 09:35:52 +0800

    Create custom hook to fetch username of current user

---
+++ frontend/src/hooks/useUsername.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { getUsername } from "../utils/apiUtils";
+
+function useUsername() {
+    const [username, setUsername] = useState<string | null>(null);
+
+    async function updateUsername() {
+        const fetchedUsername = await getUsername();
+
+        setUsername(fetchedUsername);
+    }
+
+    return username;
+}
+
+export default useUsername;
