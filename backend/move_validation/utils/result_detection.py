1bca10b22c1d1c81466f98a67a642ba4f76570d9
Teddi <teddikao@gmail.com>
2025-05-07 07:24:51 +0800

    Add function to check if checkmated or stalemated in one loop

--- backend/move_validation/utils/result_detection.py
+++ backend/move_validation/utils/result_detection.py
@@ -79,6 +79,40 @@ def get_is_checkmated(current_fen: dict, king_color: str) -> bool:
 
     return True
 
+def is_checkmated_or_stalemated(current_fen: dict, king_color: str):
+    board_placement = current_fen["board_placement"]
+    castling_rights: dict = current_fen["castling_rights"]
+    en_passant_target_square: str = current_fen["en_passant_target_square"]
+
+    all_legal_moves: list = []
+
+    king_in_check: bool = is_king_in_check(
+        board_placement, king_color)
+    
+    for square in board_placement.keys():
+        piece_color: str = board_placement[square]["piece_color"]
+        piece_type: str = board_placement[square]["piece_type"]
+
+        if piece_color.lower() != king_color.lower():
+            continue
+
+        move_info: dict = {
+            "piece_color": piece_color,
+            "piece_type": piece_type,
+            "starting_square": square
+        }
+
+        all_legal_moves += get_legal_moves(
+            move_info, board_placement, en_passant_target_square, castling_rights)
+
+        if len(all_legal_moves) > 0:
+            return False, False
+
+    if king_in_check:
+        return True, False
+    else:
+        return False, True
+
 def get_position_occurences(position_list: list, position: dict):
     occurences = 0
 
