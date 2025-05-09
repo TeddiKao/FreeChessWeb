60b9c5c77857d501239fc8e0c8326b8d7f91d646
Teddi <teddikao@gmail.com>
2025-05-09 21:27:18 +0800

    Add UserGreeting component to display personalized greeting to the user

---
+++ frontend/src/components/page/dashboard/UserGreeting.tsx
@@ -0,0 +1,23 @@
+import useUsername from "../../../hooks/useUsername";
+import { getLocalTimeHours } from "../../../utils/timeUtils";
+
+function UserGreeting() {
+    const currentTime = getLocalTimeHours();
+    const username = useUsername();
+
+    function getGreeting() {
+        if (currentTime < 12) {
+            return "Good Morning";
+        } else if (currentTime < 18) {
+            return "Good Afternoon";
+        } else {
+            return "Good Evening";
+        }
+    }
+
+    return (
+        <h2 className="user-greeting">{getGreeting()} {username}!</h2>
+    )
+}
+
+export default UserGreeting;
