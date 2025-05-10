c18a19d06339c06b7f9a72a0c2fc60005da3bdae
Teddi <teddikao@gmail.com>
2025-05-10 20:44:08 +0800

    Add UserGreeting component back to Dashboard page

--- frontend/src/pages/protected/Dashboard.tsx
+++ frontend/src/pages/protected/Dashboard.tsx
@@ -1,10 +1,15 @@
 import DashboardNavbar from "../../components/page/dashboard/DashboardNavbar.tsx";
 import UserGreeting from "../../components/page/dashboard/UserGreeting.tsx";
 
+import "../../styles/pages/dashboard.scss";
+
 function Dashboard() {
 	return (
 		<>
 			<DashboardNavbar />
+			<div className="dashboard-interface-container">
+				<UserGreeting />
+			</div>
 		</>
 	);
 }
