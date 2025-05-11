aeb1a3c760c44c9007dd007bc8409b5612775b26
Teddi <teddikao@gmail.com>
2025-05-11 19:33:00 +0800

    Implement custom hook to further encapsulate logic

---
+++ frontend/src/hooks/usePieceAnimation.ts
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import { ChessboardSquareIndex, OptionalValue } from "../types/general";
+import { calculateXYTransform } from "../utils/boardUtils";
+
+function usePieceAnimation() {
+	const [animatingPieceSquare, setAnimatingPieceSquare] =
+		useState<OptionalValue<ChessboardSquareIndex>>(null);
+	const [animatingPieceStyles, setAnimatingPieceStyles] = useState({});
+
+	function animatePiece(
+		startingSquare: ChessboardSquareIndex,
+		destinationSquare: ChessboardSquareIndex,
+        squareSize?: number
+	) {
+		const [xTransform, yTransform] = calculateXYTransform(
+			startingSquare,
+			destinationSquare,
+			squareSize
+		);
+
+		setAnimatingPieceSquare(Number(startingSquare));
+		setAnimatingPieceStyles({
+			transform: `translate(${xTransform}px, ${yTransform}px)`,
+			pointerEvents: "none",
+			zIndex: "100",
+		});
+	}
+
+    return [animatingPieceSquare, animatingPieceStyles, animatePiece];
+}
+
+export default usePieceAnimation;
