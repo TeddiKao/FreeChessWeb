fc49d4c4c738f3dc3e2895c881502f1699897efd
Teddi <teddikao@gmail.com>
2025-05-11 12:14:00 +0800

    Add DashboardSuggestions component to dashboard page

--- frontend/src/pages/protected/Dashboard.tsx
+++ frontend/src/pages/protected/Dashboard.tsx
@@ -1,4 +1,5 @@
 import DashboardNavbar from "../../components/page/dashboard/DashboardNavbar.tsx";
+import DashboardSuggestions from "../../components/page/dashboard/DashboardSuggestions.tsx";
 import UserGreeting from "../../components/page/dashboard/UserGreeting.tsx";
 
 import "../../styles/pages/dashboard.scss";
@@ -9,7 +10,11 @@ function Dashboard() {
 			<DashboardNavbar />
 			<div className="dashboard-interface-container">
 				<UserGreeting />
-				<p className="dashboard-suggestion-text">Here are some suggestions for you</p>
+				<p className="dashboard-suggestion-text">
+					Here are some suggestions for you
+				</p>
+
+				<DashboardSuggestions />
 			</div>
 		</>
 	);
