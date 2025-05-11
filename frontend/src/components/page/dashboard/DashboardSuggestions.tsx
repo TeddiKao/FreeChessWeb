a19d41b100c87e3c87a10f6241f0abffcdc5e1d2
Teddi <teddikao@gmail.com>
2025-05-11 10:04:07 +0800

    Add basic markup for DashboardSuggestions component

--- frontend/src/components/page/dashboard/DashboardSuggestions.tsx
+++ frontend/src/components/page/dashboard/DashboardSuggestions.tsx
@@ -1,9 +1,19 @@
+import "../../../styles/components/dashboard/dashboard-suggestions.scss";
+
 function DashboardSuggestions() {
-    return (
-        <div className="dashboard-suggestions-container">
-            
-        </div>
-    )
+	return (
+		<div className="dashboard-suggestions-container">
+			<div className="dashboard-suggestion-container">
+				<img
+					src="/pass-and-play-dashboard-suggestion.svg"
+					className="dashboard-suggestion-icon"
+				/>
+
+                <h4 className="dashboard-suggestion-name">Pass and Play</h4>
+                <p className="dashboard-suggestion-description">Play a game of chess on a single device</p>
+			</div>
+		</div>
+	);
 }
 
 export default DashboardSuggestions;
