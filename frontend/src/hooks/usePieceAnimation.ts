6161af8d8bcedd857ea151071259f455e6d5115b
Teddi <teddikao@gmail.com>
2025-05-12 18:42:46 +0800

    Add move replay animation function to usePieceAnimation hook

--- frontend/src/hooks/usePieceAnimation.ts
+++ frontend/src/hooks/usePieceAnimation.ts
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { ChessboardSquareIndex, OptionalValue } from "../types/general";
-import { calculateXYTransform } from "../utils/boardUtils";
+import { calculateReplayXYTransform, calculateXYTransform } from "../utils/boardUtils";
 import { convertToMilliseconds } from "../utils/timeUtils";
 import { pieceAnimationTime } from "../constants/pieceAnimation";
 import { PieceColor } from "../types/gameLogic";
@@ -14,7 +14,7 @@ function usePieceAnimation() {
 		startingSquare: ChessboardSquareIndex,
 		destinationSquare: ChessboardSquareIndex,
 		orientation: PieceColor,
-        squareSize?: number,
+		squareSize?: number
 	) {
 		const [xTransform, yTransform] = calculateXYTransform(
 			startingSquare,
@@ -33,10 +33,36 @@ function usePieceAnimation() {
 		setTimeout(() => {
 			setAnimatingPieceSquare(null);
 			setAnimatingPieceStyles({});
-		}, convertToMilliseconds(pieceAnimationTime))
+		}, convertToMilliseconds(pieceAnimationTime));
 	}
 
-    return [animatingPieceSquare, animatingPieceStyles, animatePiece];
+	function animateMoveReplay(
+		startingSquare: ChessboardSquareIndex,
+		destinationSquare: ChessboardSquareIndex,
+		orientation: PieceColor,
+		squareSize?: number
+	) {
+		const [xTransform, yTransform] = calculateReplayXYTransform(
+			startingSquare,
+			destinationSquare,
+			orientation,
+			squareSize
+		);
+
+		setAnimatingPieceSquare(Number(destinationSquare));
+		setAnimatingPieceStyles({
+			transform: `translate(${xTransform}px, ${yTransform}px)`,
+			pointerEvents: "none",
+			zIndex: "100",
+		});
+
+		setTimeout(() => {
+			setAnimatingPieceSquare(null);
+			setAnimatingPieceStyles({});
+		}, convertToMilliseconds(pieceAnimationTime));
+	}
+
+	return [animatingPieceSquare, animatingPieceStyles, animatePiece, animateMoveReplay];
 }
 
 export default usePieceAnimation;
