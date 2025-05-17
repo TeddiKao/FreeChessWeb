e31b2328e03df21872a6f8a2a15e1fa6a9f46870
Teddi <teddikao@gmail.com>
2025-05-17 17:44:41 +0800

    Add placeholder component for page navigation

---
+++ frontend/src/components/page/gameHistory/PageNavigation.tsx
@@ -0,0 +1,11 @@
+import { StateSetterFunction } from "../../../types/general";
+
+type PageNavigationProps = {
+    setPage: StateSetterFunction<number>;
+}
+
+function PageNavigation({ setPage }: PageNavigationProps) {
+
+}
+
+export default PageNavigation;
