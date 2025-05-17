393c120f7a12ffaacef933fd962791d958187073
Teddi <teddikao@gmail.com>
2025-05-17 17:58:41 +0800

    Add basic display and functions for navigating to previous and next pages

--- frontend/src/components/page/gameHistory/PageNavigation.tsx
+++ frontend/src/components/page/gameHistory/PageNavigation.tsx
@@ -2,10 +2,25 @@ import { StateSetterFunction } from "../../../types/general";
 
 type PageNavigationProps = {
     setPage: StateSetterFunction<number>;
+    currentPage: number;
+    totalPages: number;
 }
 
-function PageNavigation({ setPage }: PageNavigationProps) {
+function PageNavigation({ setPage, currentPage, totalPages }: PageNavigationProps) {
+    function nextPage() {
+        setPage((prevPage) => prevPage < totalPages ? prevPage ++ : prevPage);
+    }
 
+
+    function previousPage() {
+        setPage((prevPage) => prevPage > 1 ? prevPage -- : prevPage)
+    }
+    
+    return (
+        <div className="page-navigation-container">
+            <p className="page-indicator">{currentPage}/{totalPages}</p>
+        </div>
+    )
 }
 
 export default PageNavigation;
