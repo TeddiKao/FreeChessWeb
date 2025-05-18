8afca748e73d10a09e28119c2631b794678cd399
Teddi <teddikao@gmail.com>
2025-05-18 13:53:19 +0800

    Add functionality to change CSS classes depending on current page, used to show and hide navigation buttons

--- frontend/src/components/page/gameHistory/PageNavigation.tsx
+++ frontend/src/components/page/gameHistory/PageNavigation.tsx
@@ -2,32 +2,69 @@ import { StateSetterFunction } from "../../../types/general";
 import "../../../styles/features/gameHistory/page-navigation.scss";
 
 type PageNavigationProps = {
-    setPage: StateSetterFunction<number>;
-    currentPage: number;
-    totalPages: number;
-}
+	setPage: StateSetterFunction<number>;
+	currentPage: number;
+	totalPages: number;
+};
 
-function PageNavigation({ setPage, currentPage, totalPages }: PageNavigationProps) {
-    function nextPage() {
-        setPage((prevPage) => prevPage < totalPages ? prevPage ++ : prevPage);
-    }
+function PageNavigation({
+	setPage,
+	currentPage,
+	totalPages,
+}: PageNavigationProps) {
+	function nextPage() {
+		setPage((prevPage) => (prevPage < totalPages ? prevPage++ : prevPage));
+	}
 
+	function previousPage() {
+		setPage((prevPage) => (prevPage > 1 ? prevPage-- : prevPage));
+	}
 
-    function previousPage() {
-        setPage((prevPage) => prevPage > 1 ? prevPage -- : prevPage)
-    }
-    
-    return (
-        <div className="page-navigation-container">
-            <div role="button" className="prev-page-button-container">
-                <img src="/left-arrow.svg" className="prev-page-icon" alt="Icon for navigating to previous page" />
-            </div>
-            <p className="page-indicator">{currentPage}/{totalPages}</p>
-            <div role="button" className="next-page-button-container">
-                <img src="/right-arrow.svg" className="next-page-icon" alt="Icon for navigating to next page" />
-            </div>
-        </div>
-    )
+	function isPrevPageButtonVisible() {
+		return currentPage > 1;
+	}
+
+	function isNextPageButtonVisible() {
+		return currentPage < totalPages;
+	}
+
+	function getPrePageButtonIsHiddenClass() {
+		return !isPrevPageButtonVisible() ? "hidden" : "";
+	}
+
+	function getNextPageButtonIsHiddenClass() {
+		return !isNextPageButtonVisible() ? "hidden" : "";
+	}
+
+	return (
+		<div className="page-navigation-container">
+			<div
+				role="button"
+				className={`prev-page-button-container ${getPrePageButtonIsHiddenClass()}`}
+			>
+				<img
+					src="/left-arrow.svg"
+					className="prev-page-icon"
+					alt="Icon for navigating to previous page"
+				/>
+			</div>
+            
+			<p className="page-indicator">
+				{currentPage}/{totalPages}
+			</p>
+
+			<div
+				role="button"
+				className={`next-page-button-container ${getNextPageButtonIsHiddenClass()}`}
+			>
+				<img
+					src="/right-arrow.svg"
+					className="next-page-icon"
+					alt="Icon for navigating to next page"
+				/>
+			</div>
+		</div>
+	);
 }
 
 export default PageNavigation;
