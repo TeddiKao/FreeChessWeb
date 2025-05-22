9fa972953b3b27cb45b240ab716dbd3f217a2ae4
Teddi <teddikao@gmail.com>
2025-05-23 07:45:07 +0800

    Remove unused React immport

--- frontend/src/components/global/PromotionPopup.tsx
+++ frontend/src/components/global/PromotionPopup.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import { useEffect, useRef } from "react";
 
 import "../../styles/components/chessboard/promotion-popup.scss";
 import { PieceColor, PieceType } from "../../types/gameLogic";
