0d51b8068f8ea01ef0a3e4587924264ff82506fb
Teddi <teddikao@gmail.com>
2025-05-24 13:44:18 +0800

    Add basic markup for account links container

--- frontend/src/components/page/dashboard/DashboardNavbar.tsx
+++ frontend/src/components/page/dashboard/DashboardNavbar.tsx
@@ -23,16 +23,27 @@ function DashboardNavbar() {
 			onClick={expandDashboardNavbar}
 			className="dashboard-navbar-container"
 		>
-			{dashboardNavLinks.map(({ name, icon, subLinks, path }, index) => (
-				<ParentLink
-					key={index}
-					name={name}
-					icon={icon}
-					subLinks={subLinks}
-					path={path}
-					dashboardNavbarExpanded={dashboardNavbarExpanded}
-				/>
-			))}
+			<div className="dashboard-navbar-main-links-container">
+				{dashboardNavLinks.map(
+					({ name, icon, subLinks, path }, index) => (
+						<ParentLink
+							key={index}
+							name={name}
+							icon={icon}
+							subLinks={subLinks}
+							path={path}
+							dashboardNavbarExpanded={dashboardNavbarExpanded}
+						/>
+					)
+				)}
+			</div>
+
+			<div className="dashboard-navbar-account-links-container">
+				<div className="dashboard-navbar-account-link">
+					<img className="dashboard-navbar-account-icon" src="/account-icon.svg" />
+					<div className="dashboard-navbar-account-options"></div>
+				</div>
+			</div>
 		</nav>
 	);
 }
