0a5c5935011ba6d7c6ed0e1cba7b44f95e30cfbf
Teddi <teddikao@gmail.com>
2025-05-30 18:42:55 +0800

    Add constants to define plural and singular forms for chess pieces, increasing robustness

---
+++ frontend/src/constants/pieceMappings.ts
@@ -0,0 +1,17 @@
+const pluralToSingularPieceMap = {
+    pawns: "pawn",
+    knights: "knight",
+    bishops: "bishop",
+    rooks: "rook",
+    queens: "queen"
+}
+
+const singularToPluralPieceMap = {
+    pawn: "pawns",
+    knight: "knights",
+    bishop: "bishops",
+    rook: "rooks",
+    queen: "queens"
+}
+
+export { pluralToSingularPieceMap, singularToPluralPieceMap }
