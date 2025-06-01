7cb155b9a119e07c8523d196eb2f4050a7726aa0
Teddi <teddikao@gmail.com>
2025-06-01 17:00:33 +0800

    Add method to check user a player has been matched with

--- backend/matchmaking/models.py
+++ backend/matchmaking/models.py
@@ -14,6 +14,9 @@ class WaitingPlayer(models.Model):
 	assigned_game_id = models.IntegerField(null=True, blank=True)
 
 	@database_sync_to_async
+	def get_matched_user(self):
+		return self.matched_user
+	@database_sync_to_async
 	def has_player_been_matched(self):
 		return self.has_been_matched
 
