5bcd0a287bec7b50114f325ca04c1c16b45d9761
Teddi <teddikao@gmail.com>
2025-06-10 17:33:29 +0800

    Add Websocket lifecycle handling for Challenge Websocket provider

--- frontend/src/components/global/wrappers/ChallengeWebsocketProvider.tsx
+++ frontend/src/components/global/wrappers/ChallengeWebsocketProvider.tsx
@@ -1,11 +1,47 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
+import { parseWebsocketUrl } from "../../../utils/generalUtils";
+import useWebSocket from "../../../hooks/useWebsocket";
+import useWebsocketLifecycle from "../../../hooks/useWebsocketLifecycle";
 
 type ChallengeWebsocketProviderProps = {
-    children: ReactNode;
-}
+	children: ReactNode;
+};
 
-function ChallengeWebsocketProvider({ children }: ChallengeWebsocketProviderProps) {
-    return children;
+function ChallengeWebsocketProvider({
+	children,
+}: ChallengeWebsocketProviderProps) {
+	const websocketURL = parseWebsocketUrl("challenge-server");
+	const [challengeWebsocketEnabled, setChallengeWebsocketEnabled] =
+		useState(false);
+
+	const challengeWebsocketRef = useRef<WebSocket | null>(null);
+	const challengeWebsocketExistsRef = useRef<boolean>(false);
+
+	const socket = useWebSocket(
+		websocketURL,
+		handleOnMessage,
+		undefined,
+		challengeWebsocketEnabled
+	);
+
+	useWebsocketLifecycle({
+		websocket: socket,
+		websocketRef: challengeWebsocketRef,
+		websocketExistsRef: challengeWebsocketExistsRef,
+		setWebsocketEnabled: setChallengeWebsocketEnabled,
+		handleWindowUnload: handleWindowUnload,
+	});
+
+	function handleOnMessage() {}
+
+	function handleWindowUnload() {
+		if (challengeWebsocketRef.current?.readyState == WebSocket.OPEN) {
+			challengeWebsocketRef.current.close();
+			challengeWebsocketExistsRef.current = false;
+		}
+	}
+
+	return children;
 }
 
 export default ChallengeWebsocketProvider;
