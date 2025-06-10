7b547cfdc84c4b15bd4e4774efab9d0daff4b240
Teddi <teddikao@gmail.com>
2025-06-10 12:15:02 +0800

    Add basic markup for ChallengeNotification component

--- frontend/src/components/global/modals/ChallengeNotification.tsx
+++ frontend/src/components/global/modals/ChallengeNotification.tsx
@@ -1,4 +1,5 @@
 import { TimeControl } from "../../../types/gameSetup";
+import { displayTimeControl } from "../../../utils/timeUtils";
 import ModalWrapper from "../wrappers/ModalWrapper";
 
 type ChallengeNotificationProps = {
@@ -11,12 +12,37 @@ type ChallengeNotificationProps = {
 function ChallengeNotification({
 	visible,
 	challengerUsername,
-    challengerRelationship,
+	challengerRelationship,
 	timeControl,
 }: ChallengeNotificationProps) {
 	return (
 		<ModalWrapper visible={visible}>
-			<div className="challenge-notification-modal-container"></div>
+			<div className="challenge-notification-modal-container">
+				<h2 className="challenge-notifcation-modal-title">
+					Challenge received
+				</h2>
+				<div className="challenger-info-container">
+					<img className="challenger-profile-picture" />
+					<div className="main-challenge-info">
+						<p className="challenger-relationship">
+							{challengerRelationship}
+						</p>
+
+						<p className="challenger-username">
+							{challengerUsername}
+						</p>
+
+                        <p className="challenge-time-control">
+                            {displayTimeControl(timeControl)}
+                        </p>
+					</div>
+				</div>
+
+                <div className="challenge-handling-buttons-container">
+                    <button className="decline-challenge-button">Decline</button>
+                    <button className="accept-challenge-button">Accept</button>
+                </div>
+			</div>
 		</ModalWrapper>
 	);
 }
