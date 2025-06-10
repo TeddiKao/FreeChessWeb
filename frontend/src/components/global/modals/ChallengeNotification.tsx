9fd81e7a2ba4322b4f71677d5385a4e81440e328
Teddi <teddikao@gmail.com>
2025-06-10 12:05:26 +0800

    Add boilerplate code for ChallengeNotification modal

---
+++ frontend/src/components/global/modals/ChallengeNotification.tsx
@@ -0,0 +1,13 @@
+import ModalWrapper from "../wrappers/ModalWrapper";
+
+function ChallengeNotification() {
+	return (
+		<ModalWrapper>
+			<div className="challenge-notification-modal-container">
+                
+            </div>
+		</ModalWrapper>
+	);
+}
+
+export default ChallengeNotification;
