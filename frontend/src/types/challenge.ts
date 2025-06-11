52a608248b4839a3cfb603341e75beecd78af8aa
Teddi <teddikao@gmail.com>
2025-06-11 10:59:45 +0800

    Add type to represent data sent in challenge websocket

--- frontend/src/types/challenge.ts
+++ frontend/src/types/challenge.ts
@@ -1,3 +1,12 @@
+import { TimeControl } from "./gameSetup";
+
 type ChallengeRelationships = "Recent opponent" | "Unknown";
 
-export type { ChallengeRelationships }
+type ChallengeWebsocketEventData = {
+    type: string;
+    challenge_recepient: string;
+    relationship: ChallengeRelationships;
+    challenge_time_control: TimeControl;
+}
+
+export type { ChallengeRelationships, ChallengeWebsocketEventData }
