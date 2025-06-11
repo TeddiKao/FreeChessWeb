eb9a2fefaf29a5f91b9ab389cbe3e8b9fb86d090
Teddi <teddikao@gmail.com>
2025-06-11 12:42:16 +0800

    Add new interface to represent data when challenge is accepted

--- frontend/src/interfaces/challenge.ts
+++ frontend/src/interfaces/challenge.ts
@@ -1,11 +1,19 @@
 import { ChallengeRelationships } from "../types/challenge";
+import { PieceColor } from "../types/gameLogic";
 import { TimeControl } from "../types/gameSetup";
 import { BasicWebSocketEventData } from "./general";
 
-interface ChallengeWebsocketEventData extends BasicWebSocketEventData {
+interface ChallengeSentWebsocketEventData extends BasicWebSocketEventData {
     challenge_sender: string;
     relationship: ChallengeRelationships;
     challenge_time_control: TimeControl;
 }
 
-export type { ChallengeWebsocketEventData }
+interface ChallengeAcceptedWebsocketEventData extends BasicWebSocketEventData {
+    game_id: number;
+    base_time: number;
+    increment: number;
+    assigned_color: PieceColor;
+}
+
+export type { ChallengeSentWebsocketEventData, ChallengeAcceptedWebsocketEventData }
