53a4d24de6e94e80f5b094f3df16948095e9ae14
Teddi <teddikao@gmail.com>
2025-06-12 11:59:53 +0800

    Add placeholder component for Challenge response waiting screen

---
+++ frontend/src/components/global/modals/ChallengeResponseWaitScreen.tsx
@@ -0,0 +1,11 @@
+import ModalWrapper from "../wrappers/ModalWrapper";
+
+function ChallengeResponseWaitScreen() {
+    return (
+        <ModalWrapper>
+            
+        </ModalWrapper>
+    )
+}
+
+export default ChallengeResponseWaitScreen;
