e0215c523156accce35c9329d77af44acb8306e8
Teddi <teddikao@gmail.com>
2025-06-12 12:03:21 +0800

    Add markup for new ChallengeResponseWaitScreen

--- frontend/src/components/global/modals/ChallengeResponseWaitScreen.tsx
+++ frontend/src/components/global/modals/ChallengeResponseWaitScreen.tsx
@@ -1,9 +1,21 @@
+import { TimeControl } from "../../../types/gameSetup";
+import { displayTimeControl } from "../../../utils/timeUtils";
+import LoadingSpinner from "../LoadingSpinner";
 import ModalWrapper from "../wrappers/ModalWrapper";
 
-function ChallengeResponseWaitScreen() {
+type ChallengeResponseWaitScreenProps = {
+    visible: boolean;
+    timeControlInfo: TimeControl
+}
+
+function ChallengeResponseWaitScreen({ visible, timeControlInfo }: ChallengeResponseWaitScreenProps) {
     return (
-        <ModalWrapper>
-            
+        <ModalWrapper visible={visible}>
+            <div className="challenge-response-wait-screen-container">
+                <h3 className="challenge-response-status">Waiting for response</h3>
+                <LoadingSpinner />
+                <p className="challenge-time-control">{displayTimeControl(timeControlInfo)}</p>
+            </div>
         </ModalWrapper>
     )
 }
