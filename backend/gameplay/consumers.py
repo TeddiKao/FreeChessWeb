d4ec09a0c07eace579096b1f10be79af70c134cb
Teddi <teddikao@gmail.com>
2025-06-12 12:07:44 +0800

    Add functionality to consumer to return message to client once challenge has been sent successfully

--- backend/gameplay/consumers.py
+++ backend/gameplay/consumers.py
@@ -1059,6 +1059,13 @@ class GameChallengeConsumer(AsyncWebsocketConsumer):
 		)
 
 		await self.channel_layer.group_send(
+			self.room_group_name,
+			{
+				"type": "challenge_successfully_sent"
+			}
+		)
+
+		await self.channel_layer.group_send(
 			f"challenge_room_{recepient_user_id}",
 			{
 				"type": "challenge_received",
@@ -1155,4 +1162,9 @@ class GameChallengeConsumer(AsyncWebsocketConsumer):
 
 			"white_player_username": event["white_player_username"],
 			"black_player_username": event["black_player_username"]
+		}))
+
+	async def challenge_successfully_sent(self, event):
+		await self.send(json.dumps({
+			"type": "challenge_successfully_sent"
 		}))
