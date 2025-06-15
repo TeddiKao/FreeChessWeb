a4879d693b92540c696d952cf4931fc5f5558ebe
Teddi <teddikao@gmail.com>
2025-06-15 16:10:49 +0800

    Move navbar for guest home page into separate folder

---
+++ frontend/src/pages/GuestHomePage/GuestHomePage.tsx
@@ -0,0 +1,11 @@
+import UnauthenticatedNavbar from "../components/global/UnauthenticatedNavbar.js";
+
+function GuestHomePage() {
+	return (
+		<>
+			<UnauthenticatedNavbar />
+		</>
+	);
+}
+
+export default GuestHomePage;
