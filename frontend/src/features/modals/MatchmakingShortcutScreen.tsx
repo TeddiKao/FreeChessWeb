0f204c244c38beb127a15b00a663b903d747e33a
Teddi <teddikao@gmail.com>
2025-06-15 18:05:42 +0800

    FIx imports for MatchmakingShortcutScreen component

--- frontend/src/features/modals/MatchmakingShortcutScreen.tsx
+++ frontend/src/features/modals/MatchmakingShortcutScreen.tsx
@@ -3,7 +3,7 @@ import { displayTimeControl } from "../../utils/timeUtils";
 import LoadingSpinner from "../../components/common/LoadingSpinner";
 import ModalWrapper from "../../components/wrappers/ModalWrapper";
 
-import "../../../styles/modals/matchmaking-shortcut-screen.scss";
+import "../../styles/modals/matchmaking-shortcut-screen.scss";
 
 type MatchmakingShortcutScreenProps = {
 	visible: boolean;
