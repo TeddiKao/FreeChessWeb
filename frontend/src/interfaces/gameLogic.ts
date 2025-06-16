c1b62bc3809742d10c0a29f3885ddd5d87b79983
Teddi <teddikao@gmail.com>
2025-06-16 21:03:56 +0800

    Add type to define structure of Position list

--- frontend/src/interfaces/gameLogic.ts
+++ frontend/src/interfaces/gameLogic.ts
@@ -1,50 +1,72 @@
 import { MoveInfo, ParsedFENString } from "../types/gameLogic";
 import { BasicWebSocketEventData } from "./general.ts";
-import { CapturedPiecesList, PromotedPiecesList } from "./materialCalculation.ts";
+import {
+	CapturedPiecesList,
+	PromotedPiecesList,
+} from "./materialCalculation.ts";
 
 interface MoveMadeEventData extends BasicWebSocketEventData {
-    move_data: MoveInfo;
-    new_parsed_fen: ParsedFENString;
-    move_made_by: string,
-    move_type: string;
-    new_position_index: number;
+	move_data: MoveInfo;
+	new_parsed_fen: ParsedFENString;
+	move_made_by: string;
+	move_type: string;
+	new_position_index: number;
 }
 
 interface TimerChangedEventData extends BasicWebSocketEventData {
-    white_player_clock: number;
-    black_player_clock: number;
+	white_player_clock: number;
+	black_player_clock: number;
 }
 
 interface PositionListUpdateEventData extends BasicWebSocketEventData {
-    new_position_list: Array<{
-        position: ParsedFENString;
-        last_dragged_square: string;
-        last_dropped_square: string;
-        move_type: string;
-        move_info: MoveInfo;
-        captured_material: {
-            white: CapturedPiecesList;
-            black: CapturedPiecesList;
-        };
-        promoted_pieces: {
-            white: PromotedPiecesList;
-            black: PromotedPiecesList;
-        }
-    }>;
+	new_position_list: Array<{
+		position: ParsedFENString;
+		last_dragged_square: string;
+		last_dropped_square: string;
+		move_type: string;
+		move_info: MoveInfo;
+		captured_material: {
+			white: CapturedPiecesList;
+			black: CapturedPiecesList;
+		};
+		promoted_pieces: {
+			white: PromotedPiecesList;
+			black: PromotedPiecesList;
+		};
+	}>;
 }
 
 interface CheckmateEventData extends BasicWebSocketEventData {
-    winning_color: string;
+	winning_color: string;
 }
 
 interface MoveListUpdateEventData extends BasicWebSocketEventData {
-	new_move_list: Array<Array<string>>
+	new_move_list: Array<Array<string>>;
 }
 
+interface PositionInfo {
+	position: ParsedFENString;
+	last_dragged_square: string;
+	last_dropped_square: string;
+	move_type: string;
+	move_info: MoveInfo;
+	captured_material: {
+		white: CapturedPiecesList;
+		black: CapturedPiecesList;
+	};
+	promoted_pieces: {
+		white: PromotedPiecesList;
+		black: PromotedPiecesList;
+	};
+}
+
+type PositionList = Array<PositionInfo>;
+
 export type {
-    MoveMadeEventData,
-    TimerChangedEventData,
-    PositionListUpdateEventData,
+	MoveMadeEventData,
+	TimerChangedEventData,
+	PositionListUpdateEventData,
 	CheckmateEventData,
-	MoveListUpdateEventData
+	MoveListUpdateEventData,
+    PositionList
 };
