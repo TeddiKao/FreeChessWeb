89441b25a13deff53ad0d75c27b6539f643544a1
Teddi <teddikao@gmail.com>
2025-06-16 21:07:22 +0800

    Add basic game state fetching logic in new multiplayer gameplay hook

--- frontend/src/hooks/useMultiplayerGameplayLogic.ts
+++ frontend/src/hooks/useMultiplayerGameplayLogic.ts
@@ -1,8 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ParsedFENString } from "../types/gameLogic";
+import { PositionList } from "../interfaces/gameLogic";
+import { fetchMoveList, fetchPositionList } from "../utils/apiUtils";
 
-function useMultiplayerGameplayLogic() {
-    const [positionList, setPositionList] = useState()
+function useMultiplayerGameplayLogic(gameId: number) {
+    const [positionList, setPositionList] = useState<PositionList>([]);
+    const [positionIndex, setPositionIndex] = useState(0);
+
+    const [moveList, setMoveList] = useState<Array<Array<string>>>([]);
+
+    useEffect(() => {
+        updatePositionList();
+        updateMoveList();
+    }, []);
+
+    async function updatePositionList() {
+        const positionList = await fetchPositionList(gameId);
+
+        setPositionList(positionList);
+    }
+
+    async function updateMoveList() {
+        const moveList = await fetchMoveList(gameId);
+
+        setMoveList(moveList);
+    }
 }
 
 export default useMultiplayerGameplayLogic;
