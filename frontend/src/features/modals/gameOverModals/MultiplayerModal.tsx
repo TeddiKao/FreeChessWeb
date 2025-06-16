7368eedd3435944d980a248045a63e299e2c4c5c
Teddi <teddikao@gmail.com>
2025-06-16 14:08:18 +0800

    Modify multiplayer modal to navigate to temp route before actual /play route

--- frontend/src/features/modals/gameOverModals/MultiplayerModal.tsx
+++ frontend/src/features/modals/gameOverModals/MultiplayerModal.tsx
@@ -48,6 +48,7 @@ function GameOverModal({
 		increment: timeControlInfo.increment,
 		cancelSuccess: () => {},
 		enabled: matchmakingWebsocketEnabled,
+		navigateToTemp: true,
 	})
 
 	if (!visible) {
