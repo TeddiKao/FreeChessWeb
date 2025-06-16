4041d27d84e87fbe658c37ee0efea1ac947ae267
Teddi <teddikao@gmail.com>
2025-06-16 21:00:04 +0800

    Add boilerplate code for new multiplayer gameplay logic hook

---
+++ frontend/src/hooks/useMultiplayerGameplayLogic.ts
@@ -0,0 +1,8 @@
+import { useState } from "react";
+import { ParsedFENString } from "../types/gameLogic";
+
+function useMultiplayerGameplayLogic() {
+    const [positionList, setPositionList] = useState()
+}
+
+export default useMultiplayerGameplayLogic;
