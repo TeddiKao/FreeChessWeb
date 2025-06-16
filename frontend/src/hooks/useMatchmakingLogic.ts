1a131d531a509fc2d6464f86f96ad479cea035d0
Teddi <teddikao@gmail.com>
2025-06-16 13:19:32 +0800

    Complete match found logic in useMatchmakingLogic hook

--- frontend/src/hooks/useMatchmakingLogic.ts
+++ frontend/src/hooks/useMatchmakingLogic.ts
@@ -1,45 +1,93 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { websocketBaseURL } from "../constants/urls";
 import { getAccessToken } from "../utils/tokenUtils";
 import useWebsocketWithLifecycle from "./useWebsocketWithLifecycle";
 import { MatchmakingEvents } from "../enums/gameSetup";
+import { useNavigate } from "react-router-dom";
+import { getAssignedColor } from "../utils/matchmakingUtils";
 
 interface MatchmakingLogicHookProps {
-    enabled: boolean;
-    baseTime: number;
-    increment: number;
+	enabled: boolean;
+	baseTime: number;
+	increment: number;
 }
 
-function useMatchmakingLogic({ enabled, baseTime, increment }: MatchmakingLogicHookProps) {
+function useMatchmakingLogic({
+	enabled,
+	baseTime,
+	increment,
+}: MatchmakingLogicHookProps) {
+	const [matchmakingStatus, setMatchmakingStatus] =
+		useState<string>("Finding match");
+	const [matchFound, setMatchFound] = useState<boolean>(false);
+
 	const matchFoundRef = useRef<boolean | null>(null);
 	const gameIdRef = useRef<string | number | null>(null);
 	const whitePlayerRef = useRef<string | null>(null);
 	const blackPlayerRef = useRef<string | null>(null);
 
-    const matchmakingWebsocketUrl = `${websocketBaseURL}/ws/matchmaking-server/?token=${getAccessToken()}&baseTime=${baseTime}&increment=${increment}`;
-    const { socketRef: matchmakingWebsocketRef } = useWebsocketWithLifecycle({
-        url: matchmakingWebsocketUrl,
-        enabled: enabled,
-        onMessage: handleOnMessage
-    })
+	const matchmakingWebsocketUrl = `${websocketBaseURL}/ws/matchmaking-server/?token=${getAccessToken()}&baseTime=${baseTime}&increment=${increment}`;
+	const { socketRef: matchmakingWebsocketRef } = useWebsocketWithLifecycle({
+		url: matchmakingWebsocketUrl,
+		enabled: enabled,
+		onMessage: handleOnMessage,
+	});
 
-    function handleOnMessage(event: MessageEvent): void {
+	const navigate = useNavigate();
+
+	useEffect(() => {
+        if (!matchFound) return;
+        if (!whitePlayerRef.current) return;
+        if (!blackPlayerRef.current) return;
+		const handleRedirection = async () => {
+			const gameSetupInfo = {
+				baseTime,
+				increment,
+				gameId: gameIdRef.current,
+
+				assignedColor: await getAssignedColor(whitePlayerRef.current!, blackPlayerRef.current!),
+
+                whitePlayerUsername: whitePlayerRef.current,
+                blackPlayerUsername: blackPlayerRef.current,
+			};
+
+            navigate("/play", { state: gameSetupInfo })
+		};
+
+        handleRedirection();
+	}, [matchFound, setMatchFound, navigate]);
+
+	function handleOnMessage(event: MessageEvent): void {
 		const parsedEventData = JSON.parse(event.data);
-        const eventType = parsedEventData["type"];
+		const eventType = parsedEventData["type"];
 
-        switch (eventType) {
-            case MatchmakingEvents.MATCH_FOUND:
-                if (parsedEventData["match_found"]) {
-                    
-                }
-                
-                break;
+		switch (eventType) {
+			case MatchmakingEvents.MATCH_FOUND:
+				handleMatchFound(parsedEventData);
 
-            case MatchmakingEvents.CANCELLED_SUCCESSFULLY:
-                break;
-        }
+				break;
+
+			case MatchmakingEvents.CANCELLED_SUCCESSFULLY:
+				break;
+		}
 	}
 
+	function handleMatchFound(parsedEventData: any) {
+		matchmakingWebsocketRef.current?.close();
+
+		setMatchmakingStatus("Match found");
+
+		matchFoundRef.current = true;
+		gameIdRef.current = parsedEventData["game_id"];
+
+		const whitePlayer = parsedEventData["white_player"];
+		const blackPlayer = parsedEventData["black_player"];
+
+		whitePlayerRef.current = whitePlayer;
+		blackPlayerRef.current = blackPlayer;
+
+		setMatchFound(true);
+	}
 }
 
 export default useMatchmakingLogic;
