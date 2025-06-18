83596f373f8c3d30aed99f5a28148975506f8de0
Teddi <teddikao@gmail.com>
2025-06-18 12:59:13 +0800

    Ensure that setGameWinner is typed correctly

--- frontend/src/features/gameplay/gameplaySidePanel/GameplayActionButtons.tsx
+++ frontend/src/features/gameplay/gameplaySidePanel/GameplayActionButtons.tsx
@@ -9,13 +9,14 @@ import { ActionWebSocketEventTypes } from "../../../enums/gameLogic";
 import ConfirmationPopup from "../../../components/common/ConfirmationPopup";
 import { getAccessToken } from "../../../utils/tokenUtils";
 import useWebsocketWithLifecycle from "../../../hooks/useWebsocketWithLifecycle";
+import { PieceColor } from "../../../types/gameLogic";
 
 type GameplayActionButtonsProps = {
 	gameId: string | number;
 	parentActionWebsocket: RefObject<WebSocket | null>;
 	setGameEnded: StateSetterFunction<boolean>;
 	setGameEndedCause: StateSetterFunction<string>;
-	setGameWinner: StateSetterFunction<string>;
+	setGameWinner: StateSetterFunction<PieceColor | "">;
 	setMessagePopupVisible: StateSetterFunction<boolean>;
 	setMessageToDisplay: StateSetterFunction<string>;
 	setDrawOfferReceived: StateSetterFunction<boolean>;
