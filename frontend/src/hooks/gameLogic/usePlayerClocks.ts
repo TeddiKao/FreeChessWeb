5080bc50bd159ce34bc08329b2c2125a35e213d5
Teddi <teddikao@gmail.com>
2025-06-20 16:31:10 +0800

    Add player clock states to new clock logic hook

--- frontend/src/hooks/gameLogic/usePlayerClocks.ts
+++ frontend/src/hooks/gameLogic/usePlayerClocks.ts
@@ -1,5 +1,8 @@
-function usePlayerClocks() {
+import { useState } from "react";
 
+function usePlayerClocks(baseTime: number) {
+	const [whitePlayerClock, setWhitePlayerClock] = useState<number>(baseTime);
+	const [blackPlayerClock, setBlackPlayerClock] = useState<number>(baseTime);
 }
 
 export default usePlayerClocks;
