d5655618df8cc6346f90763743c859ed48f02fb7
Teddi <teddikao@gmail.com>
2025-06-20 18:11:13 +0800

    Complete logic in useClickedSquaresState hook

--- frontend/src/hooks/gameLogic/useClickedSquaresState.ts
+++ frontend/src/hooks/gameLogic/useClickedSquaresState.ts
@@ -1,13 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { ChessboardSquareIndex } from "../../types/general";
 
-function useClickedSquaresState() {
-    const [prevClickedSquare, setPrevClickedSquare] =
+function useClickedSquaresState(onClickCallback: () => void) {
+	const [prevClickedSquare, setPrevClickedSquare] =
 		useState<ChessboardSquareIndex | null>(null);
 	const [clickedSquare, setClickedSquare] =
 		useState<ChessboardSquareIndex | null>(null);
 
-    
+	useEffect(() => {
+		onClickCallback();
+	}, [prevClickedSquare, clickedSquare]);
+
+	return {
+		prevClickedSquare,
+		setPrevClickedSquare,
+		clickedSquare,
+		setClickedSquare,
+	};
 }
 
 export default useClickedSquaresState;
