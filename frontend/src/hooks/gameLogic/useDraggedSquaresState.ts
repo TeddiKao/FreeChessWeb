56edb8f834ea59a793844c736b5c3e97379c090b
Teddi <teddikao@gmail.com>
2025-06-20 18:35:14 +0800

    Complete logic for drag squares state management hook

--- frontend/src/hooks/gameLogic/useDraggedSquaresState.ts
+++ frontend/src/hooks/gameLogic/useDraggedSquaresState.ts
@@ -1,5 +1,22 @@
-function useDraggedSquaresState() {
+import { useEffect, useState } from "react";
+import { ChessboardSquareIndex } from "../../types/general";
 
+function useDraggedSquaresState(onDragDropCallback: () => void) {
+    const [draggedSquare, setDraggedSquare] =
+		useState<ChessboardSquareIndex | null>(null);
+	const [droppedSquare, setDroppedSquare] =
+		useState<ChessboardSquareIndex | null>(null);
+
+    useEffect(() => {
+        onDragDropCallback();
+    }, [draggedSquare, droppedSquare, onDragDropCallback]);
+
+    return {
+        draggedSquare,
+        setDraggedSquare,
+        droppedSquare,
+        setDroppedSquare,
+    };
 }
 
 export default useDraggedSquaresState;
