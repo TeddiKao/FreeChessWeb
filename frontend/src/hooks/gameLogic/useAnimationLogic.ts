6610feb66892cd1a5c4df8d6f11c29f4877a5852
Teddi <teddikao@gmail.com>
2025-06-20 12:05:16 +0800

    Move animation state manipulation functions to new animation logic hook

--- frontend/src/hooks/gameLogic/useAnimationLogic.ts
+++ frontend/src/hooks/gameLogic/useAnimationLogic.ts
@@ -14,6 +14,44 @@ function useAnimationLogic() {
 
 	const [animationSquare, setAnimationSquare] =
 		useState<ChessboardSquareIndex | null>(null);
+
+    function performPostAnimationCleanup() {
+		clearPostAnimationCallback();
+		clearAnimationStartingSquare();
+		clearAnimationDestinationSquare();
+		clearAnimationRef();
+
+		setAnimationSquare(null);
+	}
+
+
+    function updateAnimationStartingSquare(square: ChessboardSquareIndex) {
+		animationStartingSquareRef.current = square;
+	}
+
+	function updateAnimationDestinationSquare(square: ChessboardSquareIndex) {
+		animationDestinationSquareRef.current = square;
+	}
+
+	function updatePostAnimationCallback(callbackFn: () => void) {
+		postAnimationActionRef.current = callbackFn;
+	}
+
+	function clearPostAnimationCallback() {
+		postAnimationActionRef.current = null;
+	}
+
+	function clearAnimationStartingSquare() {
+		animationStartingSquareRef.current = null;
+	}
+
+	function clearAnimationDestinationSquare() {
+		animationDestinationSquareRef.current = null;
+	}
+
+	function clearAnimationRef() {
+		animationRef.current = null;
+	}
 }
 
 export default useAnimationLogic;
