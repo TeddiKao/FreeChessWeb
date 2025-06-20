76d5f6e46687fb309d5a9e6fbb4b10dd26bec91d
Teddi <teddikao@gmail.com>
2025-06-20 18:54:54 +0800

    Move game end updating functions to useGameEndState hook

--- frontend/src/hooks/gameLogic/useGameEndState.ts
+++ frontend/src/hooks/gameLogic/useGameEndState.ts
@@ -1,10 +1,37 @@
 import { useState } from "react";
 import { PieceColor } from "../../types/gameLogic";
+import { CheckmateEventData } from "../../interfaces/gameLogic";
 
 function useGameEndState() {
     const [hasGameEnded, setHasGameEnded] = useState<boolean>(false);
 	const [gameEndedCause, setGameEndedCause] = useState<string>("");
 	const [gameWinner, setGameWinner] = useState<PieceColor | "">("");
+
+    function handleStalemate() {
+		setHasGameEnded(true);
+		setGameEndedCause("Stalemate");
+	}
+
+	function handleThreefoldRepetition() {
+		setHasGameEnded(true);
+		setGameEndedCause("Repetition");
+	}
+
+	function handle50MoveRule() {
+		setHasGameEnded(true);
+		setGameEndedCause("50-move-rule");
+	}
+
+	function handleInsufficientMaterial() {
+		setHasGameEnded(true);
+		setGameEndedCause("Insufficient material");
+	}
+
+	function handleCheckmate(eventData: CheckmateEventData) {
+		setHasGameEnded(true);
+		setGameEndedCause("Checkmate");
+		setGameWinner(eventData["winning_color"] as PieceColor);
+	}
 }
 
 export default useGameEndState;
