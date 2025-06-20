22421f524dafb5f342a376dd92629d11f522e555
Teddi <teddikao@gmail.com>
2025-06-20 12:03:19 +0800

    Move all ref and state variables related to animation to new animation logic hook

--- frontend/src/hooks/gameLogic/useAnimationLogic.ts
+++ frontend/src/hooks/gameLogic/useAnimationLogic.ts
@@ -1,5 +1,19 @@
-function useAnimationLogic() {
+import { useRef, useState } from "react";
+import { ChessboardSquareIndex } from "../../types/general";
 
+function useAnimationLogic() {
+    const animationRef = useRef<HTMLDivElement | null>(null);
+	const postAnimationActionRef = useRef<(() => void) | null>(null);
+
+	const animationStartingSquareRef = useRef<ChessboardSquareIndex | null>(
+		null
+	);
+	const animationDestinationSquareRef = useRef<ChessboardSquareIndex | null>(
+		null
+	);
+
+	const [animationSquare, setAnimationSquare] =
+		useState<ChessboardSquareIndex | null>(null);
 }
 
 export default useAnimationLogic;
