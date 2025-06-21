3989d433f451a0f2e4b154e284a97aa5d676b0f5
Teddi <teddikao@gmail.com>
2025-06-21 18:45:31 +0800

    Add promotion state and ref variables to usePromotionLogic hook

--- frontend/src/hooks/gameLogic/usePromotionLogic.ts
+++ frontend/src/hooks/gameLogic/usePromotionLogic.ts
@@ -1,5 +1,16 @@
-function usePromotionLogic() {
+import { useRef, useState } from "react";
+import { BoardPlacement, ParsedFENString } from "../../types/gameLogic";
+import { ChessboardSquareIndex } from "../../types/general";
 
+function usePromotionLogic() {
+    const boardStateBeforePromotion = useRef<BoardPlacement | null>(null);
+	const prePromotionBoardState = useRef<ParsedFENString | null>(null);
+
+	const promotionSquareRef = useRef<ChessboardSquareIndex | null>(null);
+	const originalPawnSquareRef = useRef<ChessboardSquareIndex | null>(null);
+
+	const [shouldShowPromotionPopup, setShouldShowPromotionPopup] =
+		useState(false);
 }
 
 export default usePromotionLogic;
