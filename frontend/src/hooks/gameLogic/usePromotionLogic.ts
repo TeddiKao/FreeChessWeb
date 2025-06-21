bd2d0dc974fbd5b50a81ce50503c31905e7568a7
Teddi <teddikao@gmail.com>
2025-06-21 18:49:23 +0800

    Add promotion state manipulation functions to promotion logic hook

--- frontend/src/hooks/gameLogic/usePromotionLogic.ts
+++ frontend/src/hooks/gameLogic/usePromotionLogic.ts
@@ -1,8 +1,10 @@
 import { useRef, useState } from "react";
-import { BoardPlacement, ParsedFENString } from "../../types/gameLogic";
+import { BoardPlacement, ParsedFENString, PieceColor } from "../../types/gameLogic";
 import { ChessboardSquareIndex } from "../../types/general";
+import { getRank } from "../../utils/boardUtils";
+import { isPawnPromotion } from "../../utils/moveUtils";
 
-function usePromotionLogic() {
+function usePromotionLogic(parsedFEN: ParsedFENString) {
     const boardStateBeforePromotion = useRef<BoardPlacement | null>(null);
 	const prePromotionBoardState = useRef<ParsedFENString | null>(null);
 
@@ -11,6 +13,91 @@ function usePromotionLogic() {
 
 	const [shouldShowPromotionPopup, setShouldShowPromotionPopup] =
 		useState(false);
+
+    function storeBoardStateBeforePromotion(
+		color: PieceColor,
+		destinationSquare: ChessboardSquareIndex
+	) {
+		if (!parsedFEN) return;
+
+		const isPromotion = isPawnPromotion(color, getRank(destinationSquare));
+
+		if (!isPromotion) return;
+
+		boardStateBeforePromotion.current = parsedFEN["board_placement"];
+	}
+
+    function cancelPromotion() {
+		setShouldShowPromotionPopup(false);
+
+		clearPrePromotionBoardState();
+		clearBoardStateBeforePromotion();
+		clearPromotionSquare();
+		clearOriginalPawnSquare();
+	}
+
+	function performPostPromotionCleanup() {
+		clearBoardStateBeforePromotion();
+		clearPrePromotionBoardState();
+		clearPromotionSquare();
+		clearOriginalPawnSquare();
+
+		setShouldShowPromotionPopup(false);
+	}
+
+	function preparePromotion(
+		startingSquare: ChessboardSquareIndex,
+		destinationSquare: ChessboardSquareIndex
+	) {
+		updatePrePromotionBoardState(startingSquare, destinationSquare);
+		updatePromotionSquare(destinationSquare);
+		updateOriginalPawnSquare(startingSquare);
+	}
+
+    function updatePrePromotionBoardState(
+		startingSquare: ChessboardSquareIndex,
+		destinationSquare: ChessboardSquareIndex
+	) {
+		if (!parsedFEN) return;
+
+		const prePromotionParsedFEN = structuredClone(parsedFEN);
+		const currentBoardPlacement = prePromotionParsedFEN["board_placement"];
+		const pawnInfo = currentBoardPlacement[startingSquare.toString()];
+
+		const prePromotionBoardPlacement = structuredClone(
+			currentBoardPlacement
+		);
+
+		delete prePromotionBoardPlacement[startingSquare.toString()];
+		prePromotionBoardPlacement[destinationSquare.toString()] = pawnInfo;
+
+		prePromotionParsedFEN["board_placement"] = prePromotionBoardPlacement;
+		prePromotionBoardState.current = prePromotionParsedFEN;
+	}
+
+    function updatePromotionSquare(square: ChessboardSquareIndex) {
+		promotionSquareRef.current = square;
+	}
+
+	function updateOriginalPawnSquare(square: ChessboardSquareIndex) {
+		originalPawnSquareRef.current = square;
+	}
+
+	function clearBoardStateBeforePromotion() {
+		boardStateBeforePromotion.current = null;
+	}
+
+	function clearPrePromotionBoardState() {
+		prePromotionBoardState.current = null;
+	}
+
+	function clearOriginalPawnSquare() {
+		originalPawnSquareRef.current = null;
+	}
+
+	function clearPromotionSquare() {
+		promotionSquareRef.current = null;
+	}
 }
 
 export default usePromotionLogic;
