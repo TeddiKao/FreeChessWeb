c77c6774bce6900d6b3105f3e98b2ad057e7a790
Teddi <teddikao@gmail.com>
2025-06-21 09:51:17 +0800

    Add basic websocket and lifecycle management logic to multiplayer websocket hook

--- frontend/src/hooks/useMultiplayerGameplayWebsocket.ts
+++ frontend/src/hooks/useMultiplayerGameplayWebsocket.ts
@@ -1,5 +1,14 @@
-function useMultiplayerGameplayWebsocket() {
+import { websocketBaseURL } from "../constants/urls";
+import { getAccessToken } from "../utils/tokenUtils";
+import useWebsocketWithLifecycle from "./useWebsocketWithLifecycle";
 
+function useMultiplayerGameplayWebsocket() {
+    const gameWebsocketUrl = `${websocketBaseURL}/ws/game-server/?token=${getAccessToken()}&gameId=${gameId}`;
+	const { socketRef: gameWebsocketRef } = useWebsocketWithLifecycle({
+		url: gameWebsocketUrl,
+		onMessage: handleOnMessage,
+		enabled: true,
+	});
 }
 
 export default useMultiplayerGameplayWebsocket;
