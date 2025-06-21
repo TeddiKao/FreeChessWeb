eff4936d547445c38d877b2fc9432f64467965a4
Teddi <teddikao@gmail.com>
2025-06-21 09:55:23 +0800

    Add message handler to gameplay websocket hook

--- frontend/src/hooks/useMultiplayerGameplayWebsocket.ts
+++ frontend/src/hooks/useMultiplayerGameplayWebsocket.ts
@@ -1,11 +1,12 @@
 import { websocketBaseURL } from "../constants/urls";
+import { GameplayWebSocketEventTypes } from "../enums/gameLogic";
 import { getAccessToken } from "../utils/tokenUtils";
 import useWebsocketWithLifecycle from "./useWebsocketWithLifecycle";
 
 interface MultiplayerGameplayWebsocketHookProps {
 	handleMoveMade: () => void;
-	handleMoveListUpdate: () => void;
-	handlePositionListUpdate: () => void;
+	handleMoveListUpdated: () => void;
+	handlePositionListUpdated: () => void;
 	handleCheckmate: () => void;
 	handleDraw: () => void;
 	handlePlayerTimeout: () => void;
@@ -14,12 +15,12 @@ interface MultiplayerGameplayWebsocketHookProps {
 
 function useMultiplayerGameplayWebsocket({
 	handleMoveMade,
-    handlePositionListUpdate,
-    handleMoveListUpdate,
-    handleCheckmate,
-    handlePlayerTimeout,
-    handleTimerChanged,
-    handleDraw
+	handlePositionListUpdated,
+	handleMoveListUpdated,
+	handleCheckmate,
+	handlePlayerTimeout,
+	handleTimerChanged,
+	handleDraw,
 }: MultiplayerGameplayWebsocketHookProps) {
 	const gameWebsocketUrl = `${websocketBaseURL}/ws/game-server/?token=${getAccessToken()}&gameId=${gameId}`;
 	const { socketRef: gameWebsocketRef } = useWebsocketWithLifecycle({
@@ -27,6 +28,57 @@ function useMultiplayerGameplayWebsocket({
 		onMessage: handleOnMessage,
 		enabled: true,
 	});
+
+	function handleOnMessage(event: MessageEvent) {
+		const eventData = JSON.parse(event.data);
+		const eventType = eventData["type"];
+
+		switch (eventType) {
+			case GameplayWebSocketEventTypes.MOVE_MADE:
+				handleMoveMade(eventData);
+				break;
+
+			case GameplayWebSocketEventTypes.TIMER_DECREMENTED:
+			case GameplayWebSocketEventTypes.TIMER_INCREMENTED:
+				handleTimerChanged(eventData);
+				break;
+
+			case GameplayWebSocketEventTypes.POSITION_LIST_UPDATED:
+				handlePositionListUpdated(eventData);
+				break;
+
+			case GameplayWebSocketEventTypes.MOVE_LIST_UPDATED:
+				handleMoveListUpdated(eventData);
+				break;
+
+			case GameplayWebSocketEventTypes.PLAYER_CHECKMATED:
+				handleCheckmate(eventData);
+				break;
+
+			case GameplayWebSocketEventTypes.PLAYER_STALEMATED:
+				handleDraw("Stalemate");
+				break;
+
+			case GameplayWebSocketEventTypes.THREEFOLD_REPETITION_DETECTED:
+				handleDraw("Repetition");
+				break;
+
+			case GameplayWebSocketEventTypes.FIFTY_MOVE_RULE_DETECTED:
+				handleDraw("50-move-rule");
+				break;
+
+			case GameplayWebSocketEventTypes.INSUFFICIENT_MATERIAL:
+				handleDraw("Insufficient material");
+				break;
+
+			case GameplayWebSocketEventTypes.PLAYER_TIMEOUT:
+				handlePlayerTimeout(eventData);
+				break;
+
+			default:
+				break;
+		}
+	}
 }
 
 export default useMultiplayerGameplayWebsocket;
