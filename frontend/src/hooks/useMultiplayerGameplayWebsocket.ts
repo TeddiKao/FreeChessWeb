3f770997bfa08c85f9be1e9c839ac26180104eee
Teddi <teddikao@gmail.com>
2025-06-21 09:54:00 +0800

    Add required callbacks as props to multiplayer gameplay websocket

--- frontend/src/hooks/useMultiplayerGameplayWebsocket.ts
+++ frontend/src/hooks/useMultiplayerGameplayWebsocket.ts
@@ -2,8 +2,26 @@ import { websocketBaseURL } from "../constants/urls";
 import { getAccessToken } from "../utils/tokenUtils";
 import useWebsocketWithLifecycle from "./useWebsocketWithLifecycle";
 
-function useMultiplayerGameplayWebsocket() {
-    const gameWebsocketUrl = `${websocketBaseURL}/ws/game-server/?token=${getAccessToken()}&gameId=${gameId}`;
+interface MultiplayerGameplayWebsocketHookProps {
+	handleMoveMade: () => void;
+	handleMoveListUpdate: () => void;
+	handlePositionListUpdate: () => void;
+	handleCheckmate: () => void;
+	handleDraw: () => void;
+	handlePlayerTimeout: () => void;
+	handleTimerChanged: () => void;
+}
+
+function useMultiplayerGameplayWebsocket({
+	handleMoveMade,
+    handlePositionListUpdate,
+    handleMoveListUpdate,
+    handleCheckmate,
+    handlePlayerTimeout,
+    handleTimerChanged,
+    handleDraw
+}: MultiplayerGameplayWebsocketHookProps) {
+	const gameWebsocketUrl = `${websocketBaseURL}/ws/game-server/?token=${getAccessToken()}&gameId=${gameId}`;
 	const { socketRef: gameWebsocketRef } = useWebsocketWithLifecycle({
 		url: gameWebsocketUrl,
 		onMessage: handleOnMessage,
