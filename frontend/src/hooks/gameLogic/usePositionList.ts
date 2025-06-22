d0c8b10d87dc1256c4cffcda632a3164387170b9
Teddi <teddikao@gmail.com>
2025-06-22 10:07:44 +0800

    Add basic position list fetching functionality upon hook mount

--- frontend/src/hooks/gameLogic/usePositionList.ts
+++ frontend/src/hooks/gameLogic/usePositionList.ts
@@ -1,5 +1,19 @@
+import { useEffect, useState } from "react";
+import { PositionList } from "../../interfaces/gameLogic";
+import { fetchPositionList } from "../../utils/apiUtils";
+
 function usePositionList(gameId: number) {
-    
+    const [positionList, setPositionList] = useState<PositionList>([]);
+
+    useEffect(() => {
+        updatePositionList();
+    }, [gameId]);
+
+    async function updatePositionList() {
+		const positionList = await fetchPositionList(gameId);
+
+		setPositionList(positionList);
+	}
 }
 
 export default usePositionList;
