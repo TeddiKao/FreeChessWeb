6c3afd7104a89afdd19bee0e31b64ce270236eee
Teddi <teddikao@gmail.com>
2025-06-24 12:03:37 +0800

    Move login and signup pages to new "auth" feature folder

---
+++ frontend/src/features/auth/pages/Login.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "../../forms/AuthForm";
+
+function Login() {
+	return <AuthForm method="Login" />;
+}
+
+export default Login;
---
+++ frontend/src/features/auth/pages/Signup.tsx
@@ -0,0 +1,7 @@
+import AuthForm from "../../forms/AuthForm";
+
+function Signup() {
+	return <AuthForm method="Signup" />;
+}
+
+export default Signup;
