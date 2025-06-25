9401898f05675aa5161b39b6bb0bc15f5be052a0
Teddi <teddikao@gmail.com>
2025-06-25 12:21:15 +0800

    Move pass-and-play move logic utility functions into pass and play feature folder

---
+++ frontend/src/features/gameplay/passAndPlay/utils/general.ts
@@ -0,0 +1,13 @@
+import { PieceColor } from "../../common/types/pieces.types";
+
+function getOppositeColor(color: PieceColor): PieceColor {
+	color = color.toLowerCase() as PieceColor;
+
+	if (color === "white") {
+		return "black";
+	} else {
+		return "white";
+	}
+}
+
+export { getOppositeColor };
