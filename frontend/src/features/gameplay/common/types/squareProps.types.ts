4747059b8a0329d3506c139165ca48dabda43903
Teddi <teddikao@gmail.com>
2025-06-25 11:09:08 +0800

    Create props interface for empty and filled square props

---
+++ frontend/src/features/gameplay/common/types/squareProps.types.ts
@@ -0,0 +1,40 @@
+import { OptionalValue, RefObject, StateSetterFunction } from "../../../../types/general";
+import { ChessboardSquareIndex } from "./board.types";
+import { PieceColor, PieceType } from "./pieces.types";
+
+interface EmptySquareProps {
+	squareNumber: ChessboardSquareIndex;
+	squareColor: string;
+	orientation: string;
+	displayPromotionPopup: boolean;
+
+	prevClickedSquare: OptionalValue<ChessboardSquareIndex>;
+	clickedSquare: OptionalValue<ChessboardSquareIndex>;
+
+	setDraggedSquare: StateSetterFunction<OptionalValue<ChessboardSquareIndex>>;
+	setDroppedSquare: StateSetterFunction<OptionalValue<ChessboardSquareIndex>>;
+	setPrevClickedSquare: StateSetterFunction<
+		OptionalValue<ChessboardSquareIndex>
+	>;
+	setClickedSquare: StateSetterFunction<OptionalValue<ChessboardSquareIndex>>;
+
+	handlePromotionCancel: (color: PieceColor) => void;
+	handlePawnPromotion: (
+		color: PieceColor,
+		promotedPiece: PieceType
+	) => Promise<void> | void;
+	previousDraggedSquare: OptionalValue<ChessboardSquareIndex>;
+	previousDroppedSquare: OptionalValue<ChessboardSquareIndex>;
+
+	squareSize?: number;
+	animatingPieceStyle?: Record<string, unknown>;
+	animatingPieceSquare: OptionalValue<ChessboardSquareIndex>;
+	animationRef: RefObject<HTMLDivElement | null>;
+}
+
+interface FilledSquareProps extends EmptySquareProps {
+	pieceColor?: PieceColor;
+	pieceType?: PieceType;
+}
+
+export type { EmptySquareProps, FilledSquareProps }
