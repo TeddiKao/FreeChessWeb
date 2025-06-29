e41e4c0008f15f2ade147a746d0fd8f3100a114b
Teddi <teddikao@gmail.com>
2025-06-29 13:58:17 +0800

    Fix all imports in GameHistory page

--- frontend/src/features/gameHistory/pages/GameHistory.tsx
+++ frontend/src/features/gameHistory/pages/GameHistory.tsx
@@ -1,11 +1,11 @@
 import { useEffect, useRef, useState } from "react";
-import DashboardNavbar from "../../components/common/DashboardNavbar/DashboardNavbar";
-import CompletedGameInfo from "./GameHistory/components/CompletedGameInfo";
-import { isNullOrUndefined } from "../../utils/generalUtils";
-import useCompletedGames from "../../hooks/useCompletedGames";
-import { getTotalPages } from "../../utils/pageNavigationUtils";
-import useUsername from "../../hooks/useUsername";
-import PageNavigation from "./GameHistory/components/PageNavigation";
+import { isNullOrUndefined } from "../../../utils/generalUtils";
+import DashboardNavbar from "../../../components/common/DashboardNavbar/DashboardNavbar";
+import useCompletedGames from "../../../hooks/useCompletedGames";
+import useUsername from "../../../hooks/useUsername";
+import { getTotalPages } from "../../../utils/pageNavigationUtils";
+import CompletedGameInfo from "../components/CompletedGameInfo";
+import PageNavigation from "../components/PageNavigation";
 
 function GameHistory() {
 	const initialUsername = useUsername();
