d2db2fa32825cd9ec17b0560d0369f5d24266ec7
Teddi <teddikao@gmail.com>
2025-07-03 20:39:55 +0800

    Update access token fetching import in API file

--- frontend/src/app/api.ts
+++ frontend/src/app/api.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { getAccessToken } from "./features/auth/utils.ts";
+import { getAccessToken } from "../features/auth/utils";
 
 const api = axios.create({
 	baseURL: import.meta.env.VITE_API_URL,
