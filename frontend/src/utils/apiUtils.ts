1dfb7db83591d2c8cb41080c4457e4a096b73fdc
Teddi <teddikao@gmail.com>
2025-07-04 21:47:07 +0800

    Move functions from general apiUtils file to gameHistory api file

--- frontend/src/utils/apiUtils.ts
+++ frontend/src/utils/apiUtils.ts
@@ -12,39 +12,6 @@ async function getUsername() {
 	return username;
 }
 
-
-async function fetchCompletedGames(currentPage: number) {
-	let completedGames = null;
-	try {
-		const response = await api.post("/game-history/get-completed-games/", {
-			current_page: currentPage,
-		});
-
-		completedGames = response.data;
-	} catch (error) {
-		console.error(error);
-	}
-
-	return completedGames;
-}
-
-async function fetchTotalCompletedGames() {
-	let totalCompletedGames = null;
-	try {
-		const response = await api.post(
-			"/game-history/get-total-completed-games/"
-		);
-
-		totalCompletedGames = response.data;
-	} catch (error) {
-		console.error(error);
-	}
-
-	return totalCompletedGames;
-}
-
 export {
 	getUsername,
-	fetchCompletedGames,
-	fetchTotalCompletedGames,
 };
