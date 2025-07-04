be25968983d7d82967aaf5089d7f094a353cf38e
Teddi <teddikao@gmail.com>
2025-07-04 09:32:43 +0800

    Move multiplayer-related enums to multiplayer feature folder

--- frontend/src/enums/gameLogic.ts
+++ frontend/src/enums/gameLogic.ts
@@ -3,22 +3,6 @@ enum MoveMethods {
 	"DRAG" = "drag",
 }
 
-enum GameplayWebSocketEventTypes {
-	MOVE_MADE = "move_made",
-	TIMER_DECREMENTED = "timer_decremented",
-	TIMER_INCREMENTED = "timer_incremented",
-	POSITION_LIST_UPDATED = "position_list_updated",
-	MOVE_LIST_UPDATED = "move_list_updated",
-	CAPTURED_MATERIAL_LIST_UPDATED = "captured_material_list_updated",
-	PROMOTED_PIECES_LIST_UPDATED = "promoted_pieces_list_updated",
-	PLAYER_STALEMATED = "player_stalemated",
-	PLAYER_CHECKMATED = "player_checkmated",
-	THREEFOLD_REPETITION_DETECTED = "threefold_repetition_detected",
-	FIFTY_MOVE_RULE_DETECTED = "50_move_rule_detected",
-	INSUFFICIENT_MATERIAL = "insufficient_material",
-	PLAYER_TIMEOUT = "player_timeout",
-}
-
 enum BotGameWebSocketEventTypes {
 	MOVE_REGISTERED = "move_registered",
 	CHECKMATE_OCCURRED = "checkmate_occurred",
@@ -28,16 +12,7 @@ enum BotGameWebSocketEventTypes {
 	BOT_MOVE_MADE = "bot_move_made",
 }
 
-enum ActionWebSocketEventTypes {
-	PLAYER_RESIGNED = "player_resigned",
-	DRAW_OFFERED = "draw_offered",
-	DRAW_OFFER_ACCPETED = "draw_accepted",
-	DRAW_OFFER_DECLINED = "draw_declined",
-}
-
 export {
 	MoveMethods,
-	GameplayWebSocketEventTypes,
-	ActionWebSocketEventTypes,
 	BotGameWebSocketEventTypes,
 };
