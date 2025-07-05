9c9130f73b93748e75e6b885abfc43cfe0cd1fed
Teddi <teddikao@gmail.com>
2025-07-05 20:23:43 +0800

    Move bot chessboard props interface to dedicated types folder in bot feature

---
+++ frontend/src/features/gameplay/bot/types/botChessboardProps.types.ts
@@ -0,0 +1,31 @@
+import { DisplayChessboardProps } from "../../../../interfaces/chessboard";
+import { ChessboardSquareIndex } from "../../../../shared/types/chessTypes/board.types";
+import { ParsedFEN, MoveInfo } from "../../../../shared/types/chessTypes/gameState.types";
+import { StateSetterFunction, OptionalValue } from "../../../../shared/types/utility.types";
+
+interface BotChessboardProps extends DisplayChessboardProps {
+	gameplaySettings: any;
+	gameId: number;
+	botId: string;
+	setMoveList: StateSetterFunction<Array<Array<string>>>;
+	setPositionList: StateSetterFunction<
+		Array<{
+			position: ParsedFEN;
+			move_type: string;
+			last_dragged_square: ChessboardSquareIndex;
+			last_dropped_square: ChessboardSquareIndex;
+			move_info: MoveInfo;
+		}>
+	>;
+	lastDraggedSquare: ChessboardSquareIndex;
+	lastDroppedSquare: ChessboardSquareIndex;
+
+	setGameEnded: StateSetterFunction<boolean>;
+	setGameEndedCause: StateSetterFunction<string>;
+	setGameWinner: StateSetterFunction<string>;
+
+	parentAnimationSquare: OptionalValue<ChessboardSquareIndex>;
+	parentAnimationStyles: Record<string, unknown>;
+}
+
+export type { BotChessboardProps }
