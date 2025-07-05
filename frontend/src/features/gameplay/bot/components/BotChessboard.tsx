6ca4076ed128b8e51fdb49a7f046412a57fd5311
Teddi <teddikao@gmail.com>
2025-07-05 12:09:34 +0800

    Remove double semicolon in react import

--- frontend/src/features/gameplay/bot/components/BotChessboard.tsx
+++ frontend/src/features/gameplay/bot/components/BotChessboard.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from "react";;
+import { useState, useEffect, useRef } from "react";
 
 import { clearSquaresStyling, getRank } from "../../../../utils/boardUtils.ts";
 
