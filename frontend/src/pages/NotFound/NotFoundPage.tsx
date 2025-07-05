c03d964cbf6b44f9086baeb49d51b30180464b9c
Teddi <teddikao@gmail.com>
2025-07-05 19:49:53 +0800

    Move NotFound page to NotFound page folder and rename page

---
+++ frontend/src/pages/NotFound/NotFoundPage.tsx
@@ -0,0 +1,15 @@
+import "../styles/pages/not-found.scss";
+
+function NotFoundPage() {
+	return (
+		<div className="not-found-page-container">
+			<h1 className="not-found-heading">404 not found</h1>
+			<p>The page you were looking for was not found</p>
+			<a className="home-page-link" href="/">
+				Return to home page
+			</a>
+		</div>
+	);
+}
+
+export default NotFoundPage;
