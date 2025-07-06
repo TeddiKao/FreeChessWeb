ba5cab2c0f382b1ec34757315a9618a9daa095d0
Teddi <teddikao@gmail.com>
2025-07-06 11:26:47 +0800

    Fix all other imports accordingly

--- frontend/src/pages/helpers/TempRoute.tsx
+++ frontend/src/pages/helpers/TempRoute.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
-import { isNullOrUndefined } from "../shared/utils/generalUtils";
+import { isNullOrUndefined } from "../../shared/utils/generalUtils";
 
 type LocationState = {
 	route: string;
