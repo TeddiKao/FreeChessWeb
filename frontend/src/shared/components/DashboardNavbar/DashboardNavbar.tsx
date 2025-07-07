bd085db9f08bdf01a8b3b322b2de280fc53950a7
Teddi <teddikao@gmail.com>
2025-07-07 15:08:38 +0800

    Add state variable to track expand/collapse state of dashboard navbar

--- frontend/src/shared/components/DashboardNavbar/DashboardNavbar.tsx
+++ frontend/src/shared/components/DashboardNavbar/DashboardNavbar.tsx
@@ -1,8 +1,15 @@
 import AccountInfo from "./components/AccountInfo";
 import "../../styles/DashboardNavbar/dashboard-navbar.scss";
 import SiteLinks from "./components/SiteLinks/SiteLinks";
+import { useState } from "react";
 
 function DashboardNavbar() {
+	const [dashboardNavbarExpanded, setDashboardNavbarExpanded] = useState(false);
+
+	function toggleDashboardNavbar() {
+		setDashboardNavbarExpanded((prevExpanded) => !prevExpanded);
+	}
+
 	return (
 		<nav className="dashboard-navbar-container">
 			<AccountInfo />
