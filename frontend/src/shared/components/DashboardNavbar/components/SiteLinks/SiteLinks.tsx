cf44940c083a855f176d27e3e434d18326cd2a9c
Teddi <teddikao@gmail.com>
2025-07-07 13:31:16 +0800

    Add logic for rendering main links in SiteLinks component

--- frontend/src/shared/components/DashboardNavbar/components/SiteLinks/SiteLinks.tsx
+++ frontend/src/shared/components/DashboardNavbar/components/SiteLinks/SiteLinks.tsx
@@ -1,9 +1,21 @@
+import { dashboardNavLinks } from "../../../../constants/navLinksConfig";
+import MainLink from "./components/MainLink";
+
 function SiteLinks() {
-    return (
-        <div className="site-links-container">
-            
-        </div>
-    )
+	return (
+		<div className="site-links-container">
+			{dashboardNavLinks.map(({ name, icon, path }, index) => {
+				return (
+					<MainLink
+						key={index}
+						linkName={name}
+						linkPath={path}
+						linkIcon={icon}
+					/>
+				);
+			})}
+		</div>
+	);
 }
 
 export default SiteLinks;
