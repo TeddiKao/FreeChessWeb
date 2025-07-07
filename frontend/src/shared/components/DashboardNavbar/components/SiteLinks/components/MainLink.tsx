ca84246e32b4c220af229989d5ac407eb56ed8b6
Teddi <teddikao@gmail.com>
2025-07-07 13:32:53 +0800

    Add logic for rendering main link

--- frontend/src/shared/components/DashboardNavbar/components/SiteLinks/components/MainLink.tsx
+++ frontend/src/shared/components/DashboardNavbar/components/SiteLinks/components/MainLink.tsx
@@ -1,6 +1,6 @@
 interface MainLinkProps {
     linkName: string;
-    linkPath: string;
+    linkPath?: string;
     linkIcon: string;
     subLinks?: Array<{ name: string; path: string; icon: string }>;
 }
@@ -8,8 +8,10 @@ interface MainLinkProps {
 function MainLink({ linkName, linkPath, linkIcon, subLinks }: MainLinkProps) {
     return (
         <div className="main-link-container">
-        
+            <img className="main-link-icon" alt="Link icon" src={linkIcon} />
+            <p className="main-link-name">{linkName}</p>
         </div>
     );
 }
 
+export default MainLink;
