361134d60b5aebef1964148b1f4d622e051b8588
Teddi <teddikao@gmail.com>
2025-07-07 13:23:46 +0800

    Modify icon path

--- frontend/src/shared/constants/navLinksConfig.ts
+++ frontend/src/shared/constants/navLinksConfig.ts
@@ -8,7 +8,7 @@ type DashboardNavLinks = Array<{
 const dashboardNavLinks: DashboardNavLinks = [
     {
         name: "Home",
-        icon: "/icons/dashboard/home.svg",
+        icon: "/icons/dashboar/navbar/siteLinks/home.svg",
         path: "/home"
     }
 ]
