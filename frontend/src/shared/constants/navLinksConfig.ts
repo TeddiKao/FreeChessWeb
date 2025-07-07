7cc2af1a03c99d1721075a6bfc1af7635eac968f
Teddi <teddikao@gmail.com>
2025-07-07 13:16:07 +0800

    Remove existing dashboard link configurations to start from scratch

--- frontend/src/shared/constants/navLinksConfig.ts
+++ frontend/src/shared/constants/navLinksConfig.ts
@@ -8,38 +8,8 @@ type DashboardNavLinks = Array<{
 const dashboardNavLinks: DashboardNavLinks = [
     {
         name: "Home",
-        icon: "/icons/dashboard/navbar/mainLinks/home-page-icon.svg",
+        icon: "/icons/dashboard/home.svg",
         path: "/home"
-    },
-
-    {
-        name: "Play",
-        icon: "/icons/dashboard/navbar/mainLinks/play-links-icon.svg",
-        subLinks: [
-            {
-                name: "Pass and Play",
-                icon: "/icons/dashboard/navbar/mainLinks/pass-and-play-icon.svg",
-                path: "/pass-and-play"
-            },
-
-            {
-                name: "Play vs Bot",
-                icon: "/icons/dashboard/navbar/mainLinks/play-vs-bot-icon.svg",
-                path: "/select-bot"
-            },
-
-            {
-                name: "Play vs Human",
-                icon: "/icons/dashboard/navbar/mainLinks/play-vs-human-icon.svg",
-                path: "/game-setup"
-            },
-
-            {
-                name: "Game History",
-                icon: "/icons/dashboard/navbar/mainLinks/game-history-icon.svg",
-                path: "/game-history"
-            }
-        ]
     }
 ]
 
