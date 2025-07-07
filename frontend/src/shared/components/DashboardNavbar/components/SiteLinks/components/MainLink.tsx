0d464069953f9326cb51e90875a5c181a2148266
Teddi <teddikao@gmail.com>
2025-07-07 15:32:03 +0800

    Add logic to display link name if dashboard navbar is expanded

--- frontend/src/shared/components/DashboardNavbar/components/SiteLinks/components/MainLink.tsx
+++ frontend/src/shared/components/DashboardNavbar/components/SiteLinks/components/MainLink.tsx
@@ -2,26 +2,37 @@ import { useNavigate } from "react-router-dom";
 import "../../../../../styles/DashboardNavbar/links/main-link.scss";
 
 interface MainLinkProps {
-    linkName: string;
-    linkPath?: string;
-    linkIcon: string;
-    subLinks?: Array<{ name: string; path: string; icon: string }>;
+	linkName: string;
+	linkPath?: string;
+	linkIcon: string;
+	subLinks?: Array<{ name: string; path: string; icon: string }>;
+
+	dashboardNavbarExpanded: boolean;
 }
 
-function MainLink({ linkName, linkPath, linkIcon, subLinks }: MainLinkProps) {
-    const navigate = useNavigate();
+function MainLink({
+	linkName,
+	linkPath,
+	linkIcon,
+	subLinks,
+	dashboardNavbarExpanded,
+}: MainLinkProps) {
+	const navigate = useNavigate();
 
-    function handleRedirect() {
-        if (!linkPath) return;
+	function handleRedirect() {
+		if (!linkPath) return;
 
-        navigate(linkPath);
-    }
+		navigate(linkPath);
+	}
 
-    return (
-        <div onClick={handleRedirect} className="main-link-container">
-            <img className="main-link-icon" alt="Link icon" src={linkIcon} />
-        </div>
-    );
+	return (
+		<div onClick={handleRedirect} className="main-link-container">
+			<img className="main-link-icon" alt="Link icon" src={linkIcon} />
+            {dashboardNavbarExpanded && (
+                <p className="main-link-name">{linkName}</p>
+            )}
+		</div>
+	);
 }
 
 export default MainLink;
