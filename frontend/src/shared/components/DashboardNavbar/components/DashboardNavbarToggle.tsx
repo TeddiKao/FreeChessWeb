4e9e68f53d6ce559a01d0a36a8fc18a9b254531c
Teddi <teddikao@gmail.com>
2025-07-08 07:31:01 +0800

    Add logic to get icon based on navbar's expanded/collapsed state

--- frontend/src/shared/components/DashboardNavbar/components/DashboardNavbarToggle.tsx
+++ frontend/src/shared/components/DashboardNavbar/components/DashboardNavbarToggle.tsx
@@ -9,6 +9,14 @@ function DashboardNavbarToggle({
 	dashboardNavbarExpanded,
     toggle
 }: DashboardNavbarToggleProps) {
+	function getToggleIcon() {
+		if (dashboardNavbarExpanded) {
+			return "/icons/dashboard/navbar/dashboard-navbar-expanded-toggle.svg";
+		} else {
+			return "/icons/dashboard/navbar/dashboard-navbar-collapsed-toggle.svg";
+		}
+	}
+
 	return (
 		<div onClick={toggle} className="dashboard-navbar-toggle-container">
 			<img className="dashboard-navbar-toggle-icon" alt="Toggle icon" src="/icons/dashboard/navbar/menu.svg"/>
