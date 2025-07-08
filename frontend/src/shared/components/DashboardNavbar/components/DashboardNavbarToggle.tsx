1deceac713c2f59d47f75c899977e6bbd4f4b09b
Teddi <teddikao@gmail.com>
2025-07-08 12:23:01 +0800

    Add basic markup for dashboard navbar toggle

---
+++ frontend/src/shared/components/DashboardNavbar/components/DashboardNavbarToggle.tsx
@@ -0,0 +1,11 @@
+
+
+function DashboardNavbarToggle() {
+    return (
+        <div className="dashboard-navbar-toggle-container">
+            <img className="dashboard-navbar-toggle-icon" />
+        </div>
+    )
+}
+
+export default DashboardNavbarToggle;
