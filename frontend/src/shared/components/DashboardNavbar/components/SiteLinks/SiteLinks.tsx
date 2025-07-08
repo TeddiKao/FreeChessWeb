f450bfc20cbfe1d305fac3c39fee0ec877d31d08
Teddi <teddikao@gmail.com>
2025-07-08 19:44:26 +0800

    Add state variable to check whether link is expanded

--- frontend/src/shared/components/DashboardNavbar/components/SiteLinks/SiteLinks.tsx
+++ frontend/src/shared/components/DashboardNavbar/components/SiteLinks/SiteLinks.tsx
@@ -2,12 +2,15 @@ import { dashboardNavLinks } from "../../../../constants/navLinksConfig";
 import MainLink from "./components/MainLink";
 
 import "../../../../styles/DashboardNavbar/site-links.scss";
+import { useState } from "react";
 
 interface SiteLinkProps {
 	dashboardNavbarExpanded: boolean;
 }
 
 function SiteLinks({ dashboardNavbarExpanded }: SiteLinkProps) {
+	const [expandedLink, setExpandedLink] = useState<string | null>(null);
+
 	return (
 		<div className="site-links-container">
 			{dashboardNavLinks.map(({ name, icon, path }, index) => {
@@ -18,6 +21,7 @@ function SiteLinks({ dashboardNavbarExpanded }: SiteLinkProps) {
 						linkPath={path}
 						linkIcon={icon}
 						dashboardNavbarExpanded={dashboardNavbarExpanded}
+						linkExpanded={expandedLink === name}
 					/>
 				);
 			})}
