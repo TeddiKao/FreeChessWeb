c0149aa3ff64c4df4cdef539437b6ec573a43241
Teddi <teddikao@gmail.com>
2025-07-09 16:54:40 +0800

    Wraps account options in FlyoutMenu
    Wraps the AccountOptions component in a FlyoutMenu component.
    This allows the account options to be displayed in a flyout menu.

--- frontend/src/shared/components/DashboardNavbar/components/AccountOptions.tsx
+++ frontend/src/shared/components/DashboardNavbar/components/AccountOptions.tsx
@@ -1,12 +1,16 @@
+import FlyoutMenu from "../../layout/FlyoutMenu";
+
 function AccountOptions() {
-    return (
-        <div className="account-options-container">
-            <div className="logout-container">
-                <img className="logout-icon" alt="Logout icon" />
-                <p className="logout-text">Logout</p>
-            </div>
-        </div>
-    )
+	return (
+		<FlyoutMenu>
+			<div className="account-options-container">
+				<div className="logout-container">
+					<img className="logout-icon" alt="Logout icon" />
+					<p className="logout-text">Logout</p>
+				</div>
+			</div>
+		</FlyoutMenu>
+	);
 }
 
 export default AccountOptions;
