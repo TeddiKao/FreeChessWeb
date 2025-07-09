09b345719513d4eb8ca9bd75612f11c2fe43311d
Teddi <teddikao@gmail.com>
2025-07-09 12:54:29 +0800

    Adds logout option to account menu
    Adds a logout option with an icon and text to the account options menu.
    This allows users to easily sign out of their account from the dashboard.

--- frontend/src/shared/components/DashboardNavbar/components/AccountOptions.tsx
+++ frontend/src/shared/components/DashboardNavbar/components/AccountOptions.tsx
@@ -1,6 +1,11 @@
 function AccountOptions() {
     return (
-        <div className="account-options-container"></div>
+        <div className="account-options-container">
+            <div className="logout-container">
+                <img className="logout-icon" alt="Logout icon" />
+                <p className="logout-text">Logout</p>
+            </div>
+        </div>
     )
 }
 
