40c92115370e74e046828be9f8c42cbfa2bd7606
Teddi <teddikao@gmail.com>
2025-07-10 14:31:09 +0800

    Adds logout icon to account options
    Adds a logout icon to the account options within the dashboard navbar.
    
    This provides a visual cue for the logout action, improving the user experience.

--- frontend/src/shared/components/DashboardNavbar/components/AccountOptions.tsx
+++ frontend/src/shared/components/DashboardNavbar/components/AccountOptions.tsx
@@ -1,11 +1,16 @@
 import FlyoutMenu from "../../layout/FlyoutMenu";
+import "../../../styles/DashboardNavbar/account-options.scss";
 
 function AccountOptions() {
 	return (
 		<FlyoutMenu>
 			<div className="account-options-container">
 				<div className="logout-container">
-					<img className="logout-icon" alt="Logout icon" />
+					<img
+						className="logout-icon"
+						alt="Logout icon"
+						src="/icons/dashboard/navbar/accountLinks/logout.svg"
+					/>
 					<p className="logout-text">Logout</p>
 				</div>
 			</div>
