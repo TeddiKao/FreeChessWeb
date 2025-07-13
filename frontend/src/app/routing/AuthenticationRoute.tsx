0a24a3705542c8c6a92ba1e5a86acface1ce404d
Teddi <teddikao@gmail.com>
2025-07-13 20:32:24 +0800

    Update prrops used in global routing components

--- frontend/src/app/routing/AuthenticationRoute.tsx
+++ frontend/src/app/routing/AuthenticationRoute.tsx
@@ -1,8 +1,9 @@
 import { Navigate } from "react-router-dom";
 
 import useIsAuthenticated from "@auth/useIsAuthenticated";
+import { BaseWrapperProps } from "@shared/types/wrapper.types";
 
-function AuthenticationRoute({ children }) {
+function AuthenticationRoute({ children }: BaseWrapperProps) {
 	const isAuthenticated = useIsAuthenticated();
 
 	if (isAuthenticated === null) {
