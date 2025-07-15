c354b79cbeb7901851eb16dbdf6397069dbc4237
Teddi <teddikao@gmail.com>
2025-07-15 20:35:55 +0800

    Add null-checks for enhanced safety

--- backend/matchmaking/consumers.py
+++ backend/matchmaking/consumers.py
@@ -187,6 +187,9 @@ class MatchmakingConsumer(AsyncWebsocketConsumer):
 
                 if not matched_user and not player_in_queue and was_added_to_queue:
                     waiting_player_model: WaitingPlayer = await self.get_player_in_queue(self.scope["user"]);
+                    if not waiting_player_model:
+                        continue
+                    
                     waiting_player_user_model = await self.get_user_model_from_waiting_player(waiting_player_model)
                     waiting_player_matched_user = await waiting_player_model.get_matched_user();
 
@@ -197,8 +200,8 @@ class MatchmakingConsumer(AsyncWebsocketConsumer):
                     waiting_player_assigned_color = waiting_player_model.assigned_color
                     waiting_player_assigned_game_id = waiting_player_model.assigned_game_id
 
-                    white_player = waiting_player_user_model if waiting_player_assigned_color.lower() == "white" else waiting_player_matched_user
-                    black_player = waiting_player_user_model if waiting_player_assigned_color.lower() == "black" else waiting_player_matched_user
+                    white_player = waiting_player_user_model if waiting_player_assigned_color and waiting_player_assigned_color.lower() == "white" else waiting_player_matched_user
+                    black_player = waiting_player_user_model if waiting_player_assigned_color and waiting_player_assigned_color.lower() == "black" else waiting_player_matched_user
 
                     await self.channel_layer.group_send(
                         f"user_{waiting_player_user_model.id}",
