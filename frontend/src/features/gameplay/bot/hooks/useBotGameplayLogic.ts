27afaefd16fae8e9befba4980b0d22daf792da0a
Teddi <teddikao@gmail.com>
2025-07-18 20:53:25 +0800

    Integrate game end state management hook with main bot gameplay logic hook

--- frontend/src/features/gameplay/bot/hooks/useBotGameplayLogic.ts
+++ frontend/src/features/gameplay/bot/hooks/useBotGameplayLogic.ts
@@ -1,5 +1,14 @@
+import useBotGameEndState from "./useBotGameEndState";
+
 function useBotGameplayLogic() {
-    
+    const {
+        hasGameEnded,
+        gameEndedCause,
+        gameWinner,
+        setHasGameEnded,
+        setGameWinner,
+        setGameEndedCause,
+    } = useBotGameEndState();
 }
 
 export default useBotGameplayLogic;
