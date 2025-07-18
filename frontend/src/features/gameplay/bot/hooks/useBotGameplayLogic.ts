ab1bca404924995daf0a8f4c16a215fc30665cc1
Teddi <teddikao@gmail.com>
2025-07-18 20:32:51 +0800

    Remove uneccessary whitespace

--- frontend/src/features/gameplay/bot/hooks/useBotGameplayLogic.ts
+++ frontend/src/features/gameplay/bot/hooks/useBotGameplayLogic.ts
@@ -2,8 +2,6 @@ import useBotGameEndState from "./useBotGameEndState";
 
 function useBotGameplayLogic() {
     const { hasGameEnded, gameWinner, gameEndedCause } = useBotGameEndState();
-
-    
 }
 
 export default useBotGameplayLogic;
