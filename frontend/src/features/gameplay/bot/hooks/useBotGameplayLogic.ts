0f0c6d74eebc3e62ba958dc4de38318f7492dd87
Teddi <teddikao@gmail.com>
2025-07-19 10:13:20 +0800

    Add drag and click squares state management to bot gameplay logic hook

--- frontend/src/features/gameplay/bot/hooks/useBotGameplayLogic.ts
+++ frontend/src/features/gameplay/bot/hooks/useBotGameplayLogic.ts
@@ -1,5 +1,31 @@
-function useBotGameplayLogic() {
-    
+import useClickedSquaresState from "../../multiplayer/hooks/useClickedSquaresState";
+import useDraggedSquaresState from "../../multiplayer/hooks/useDraggedSquaresState";
+
+interface BotGameplayLogicHookProps {
+    gameId: number;
+}
+
+function useBotGameplayLogic({ gameId }: BotGameplayLogicHookProps) {
+    const {
+        clickedSquare,
+        setClickedSquare,
+        prevClickedSquare,
+        setPrevClickedSquare,
+    } = useClickedSquaresState();
+
+    const { draggedSquare, setDraggedSquare, droppedSquare, setDroppedSquare } =
+        useDraggedSquaresState();
+
+    return {
+        clickedSquare,
+        setClickedSquare,
+        prevClickedSquare,
+        setPrevClickedSquare,
+        draggedSquare,
+        setDraggedSquare,
+        droppedSquare,
+        setDroppedSquare,
+    };
 }
 
 export default useBotGameplayLogic;
