29e98de76b2691aa590174032553345eb80ed517
Teddi <teddikao@gmail.com>
2025-07-19 11:42:27 +0800

    Implement useBotGameplayLogic hook to manage bot game websocket connection.

--- frontend/src/features/gameplay/bot/hooks/useBotGameplayLogic.ts
+++ frontend/src/features/gameplay/bot/hooks/useBotGameplayLogic.ts
@@ -1,11 +1,23 @@
+import useWebsocketLifecycle from "@/shared/hooks/websocket/useWebsocketLifecycle";
 import useClickedSquaresState from "../../multiplayer/hooks/useClickedSquaresState";
 import useDraggedSquaresState from "../../multiplayer/hooks/useDraggedSquaresState";
+import useWebsocketWithLifecycle from "@/shared/hooks/websocket/useWebsocketWithLifecycle";
+import { parseWebsocketUrl } from "@/shared/utils/generalUtils";
 
 interface BotGameplayLogicHookProps {
     gameId: number;
 }
 
 function useBotGameplayLogic({ gameId }: BotGameplayLogicHookProps) {
+    const websocketUrl = parseWebsocketUrl("bot-game-server", {
+        gameId: gameId,
+    })
+    const { socketRef } = useWebsocketWithLifecycle({
+        url: websocketUrl,
+        enabled: true,
+        onMessage: handleOnMessage
+    });
+
     const {
         clickedSquare,
         setClickedSquare,
@@ -15,6 +27,10 @@ function useBotGameplayLogic({ gameId }: BotGameplayLogicHookProps) {
 
     const { draggedSquare, setDraggedSquare, droppedSquare, setDroppedSquare } =
         useDraggedSquaresState();
+
+    function handleOnMessage() {
+
+    }
 
     return {
         clickedSquare,
