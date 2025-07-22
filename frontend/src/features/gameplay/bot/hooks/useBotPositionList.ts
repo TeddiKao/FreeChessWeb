a739bb985ae9d052b1c362d71e9cbb63da489c9d
Teddi <teddikao@gmail.com>
2025-07-22 14:15:41 +0800

    Add position list state variables to bot game position list custom hooks

--- frontend/src/features/gameplay/bot/hooks/useBotPositionList.ts
+++ frontend/src/features/gameplay/bot/hooks/useBotPositionList.ts
@@ -1,5 +1,25 @@
-function useBotPositionList() {
+import { useState } from "react";
+import { PositionList } from "@shared/types/chessTypes/gameState.types";
 
+function useBotPositionList(gameId: number) {
+    const [positionList, setPositionList] = useState<PositionList>([]);
+    const [positionIndex, setPositionIndex] = useState<number>(0);
+
+    const parsedFEN = positionList[positionIndex]?.["position"];
+    const previousDraggedSquare =
+        positionList[positionIndex]?.["last_dragged_square"];
+    const previousDroppedSquare =
+        positionList[positionIndex]?.["last_dropped_square"];
+
+    return {
+        positionList,
+        setPositionList,
+        positionIndex,
+        setPositionIndex,
+        parsedFEN,
+        previousDraggedSquare,
+        previousDroppedSquare,
+    };
 }
 
 export default useBotPositionList;
