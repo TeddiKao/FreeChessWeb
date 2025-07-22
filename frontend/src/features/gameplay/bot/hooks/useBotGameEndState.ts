55c2eae8d28f8a855a312176861200e620a22a58
Teddi <teddikao@gmail.com>
2025-07-23 07:29:24 +0800

    Create custom hook for managing bot game end state

---
+++ frontend/src/features/gameplay/bot/hooks/useBotGameEndState.ts
@@ -0,0 +1,30 @@
+import { useState } from "react";
+import { PieceColor } from "@sharedTypes/chessTypes/pieces.types";
+import { BotCheckmateEventData } from "../types/botGameEvents.types";
+
+function useBotGameEndState() {
+	const [hasGameEnded, setHasGameEnded] = useState<boolean>(false);
+	const [gameEndedCause, setGameEndedCause] = useState<string>("");
+	const [gameWinner, setGameWinner] = useState<PieceColor | "">("");
+
+	function handleDraw(drawCause: string) {
+		setHasGameEnded(true);
+		setGameEndedCause(drawCause);
+	}
+
+	function handleCheckmate(eventData: BotCheckmateEventData) {
+		setHasGameEnded(true);
+		setGameEndedCause("Checkmate");
+		setGameWinner(eventData["winning_color"] as PieceColor);
+	}
+
+	return {
+		hasGameEnded,
+		gameEndedCause,
+		gameWinner,
+		handleDraw,
+		handleCheckmate,
+	};
+}
+
+export default useBotGameEndState;
