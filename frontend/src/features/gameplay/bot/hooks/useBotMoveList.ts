8f7269eca28e0168dbea1540bfaa4b68866f8967
Teddi <teddikao@gmail.com>
2025-07-23 07:16:52 +0800

    Create custom hook to manage bot game move list logic

--- frontend/src/features/gameplay/bot/hooks/useBotMoveList.ts
+++ frontend/src/features/gameplay/bot/hooks/useBotMoveList.ts
@@ -1,5 +1,21 @@
-function useBotMoveList() {
+import { MoveList } from "@shared/types/chessTypes/gameState.types";
+import { useEffect, useState } from "react";
+import { fetchBotGameMoveList } from "../botGameApiService";
 
+function useBotMoveList(gameId: number) {
+    const [moveList, setMoveList] = useState<MoveList>([]);
+
+    useEffect(() => {
+        updateMoveList();
+    }, [gameId]);
+
+    async function updateMoveList() {
+        const moveList = await fetchBotGameMoveList(gameId);
+
+        setMoveList(moveList);
+    }
+
+    return { moveList, setMoveList };
 }
 
 export default useBotMoveList;
