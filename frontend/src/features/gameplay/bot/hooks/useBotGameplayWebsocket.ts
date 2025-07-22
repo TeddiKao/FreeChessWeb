8900758e3180e4a601057f8bba8c8ef1b2818ad2
Teddi <teddikao@gmail.com>
2025-07-22 14:33:23 +0800

    Add socket ref and websocket URL to websocket custom hook

--- frontend/src/features/gameplay/bot/hooks/useBotGameplayWebsocket.ts
+++ frontend/src/features/gameplay/bot/hooks/useBotGameplayWebsocket.ts
@@ -1,5 +1,22 @@
-function useBotGameplayWebsocket() {
+import useWebsocketWithLifecycle from "@/shared/hooks/websocket/useWebsocketWithLifecycle";
+import { parseWebsocketUrl } from "@/shared/utils/generalUtils";
 
+function useBotGameplayWebsocket({
+    gameId,
+    handleOnMessage,
+}: {
+    gameId: number;
+    handleOnMessage: (event: MessageEvent) => void;
+}) {
+    const websocketUrl = parseWebsocketUrl("bot-game-server", {
+        gameId: gameId,
+    });
+    
+    const { socketRef } = useWebsocketWithLifecycle({
+        url: websocketUrl,
+        enabled: true,
+        onMessage: handleOnMessage,
+    });
 }
 
 export default useBotGameplayWebsocket;
