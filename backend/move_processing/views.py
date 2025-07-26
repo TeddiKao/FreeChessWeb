6daa5e73f511263f60e3ac8569c345684e861e01
Teddi <teddikao@gmail.com>
2025-07-26 19:45:35 +0800

    Fix issue which caused moves to fail to process

--- backend/move_processing/views.py
+++ backend/move_processing/views.py
@@ -1,13 +1,16 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
 
 from .utils.position_update import update_structured_fen
 
 class ProcessMoveView(APIView):
     def post(self, request):
         move_info = request.data.get("move_info")
-        structured_fen = move_info.get("structured_fen")
+        structured_fen = request.data.get("structured_fen")
 
         updated_structured_fen = update_structured_fen(structured_fen, move_info)
 
-        return Response(updated_structured_fen)
+        print(updated_structured_fen)
+
+        return Response(updated_structured_fen, status=status.HTTP_200_OK)
