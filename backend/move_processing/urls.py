50d5ae34da2939b2eff5724a80ec08e4c6501625
Teddi <teddikao@gmail.com>
2025-07-26 19:45:26 +0800

    Add missing trailing slash in URL

--- backend/move_processing/urls.py
+++ backend/move_processing/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import ProcessMoveView
 
 urlpatterns = [
-    path("process-move", ProcessMoveView.as_view(), name="process_move"),
+    path("process-move/", ProcessMoveView.as_view(), name="process_move"),
 ]
