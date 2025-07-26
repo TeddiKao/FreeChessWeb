a764c3a3c66bc2165975c7f7a110eb51bfb08fce
Teddi <teddikao@gmail.com>
2025-07-26 20:01:14 +0800

    Rename instances of current_fen to structured_fen for clarity

--- backend/result_detection/views.py
+++ backend/result_detection/views.py
@@ -8,16 +8,16 @@ from .utils.result_detection import get_is_checkmated, get_is_stalemated
 # Create your views here.
 class GetIsCheckmatedView(APIView):
 	def post(self, request):
-		current_fen = request.data.get("current_fen")
+		structured_fen = request.data.get("structured_fen")
 		king_color = request.data.get("king_color")
 
-		is_checkmated = get_is_checkmated(current_fen, king_color)
+		is_checkmated = get_is_checkmated(structured_fen, king_color)
 		return Response(is_checkmated, status=status.HTTP_200_OK)
 	
 class GetIsStalematedView(APIView):
 	def post(self, request):
-		current_fen = request.data.get("current_fen")
+		structured_fen = request.data.get("structured_fen")
 		king_color = request.data.get("king_color")
 
-		is_stalemated = get_is_stalemated(current_fen, king_color)
+		is_stalemated = get_is_stalemated(structured_fen, king_color)
 		return Response(is_stalemated, status=status.HTTP_200_OK)
