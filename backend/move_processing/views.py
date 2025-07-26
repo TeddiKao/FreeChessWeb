57993627d3d357de735814e4426eaae58eacae4e
Teddi <teddikao@gmail.com>
2025-07-26 19:33:28 +0800

    Create API endpoint for processing move

--- backend/move_processing/views.py
+++ backend/move_processing/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+from .utils.position_update import update_structured_fen
+
+class ProcessMoveView(APIView):
+    def post(self, request):
+        move_info = request.data.get("move_info")
+        structured_fen = move_info.get("structured_fen")
+
+        updated_structured_fen = update_structured_fen(structured_fen, move_info)
+
+        return Response(updated_structured_fen)
