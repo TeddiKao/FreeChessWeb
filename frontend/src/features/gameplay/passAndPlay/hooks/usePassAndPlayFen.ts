362fe2b49138d802994e604502be80e771f191a4
Teddi <teddikao@gmail.com>
2025-07-27 14:49:29 +0800

    Add initial parsed FEN fetching logic to parsed FEN custom hook

--- frontend/src/features/gameplay/passAndPlay/hooks/usePassAndPlayFen.ts
+++ frontend/src/features/gameplay/passAndPlay/hooks/usePassAndPlayFen.ts
@@ -1,8 +1,26 @@
 import { ParsedFEN } from "@/shared/types/chessTypes/gameState.types";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { fetchFen } from "../utils/passAndPlayApi";
 
 function usePassAndPlayFen() {
     const [parsedFEN, setParsedFEN] = useState<ParsedFEN | null>(null);
+
+    useEffect(() => {
+        getParsedFEN();
+    }, []);
+
+    async function getParsedFEN() {
+        const startingPositionFEN =
+            "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
+
+        try {
+            const fetchedFEN = await fetchFen(startingPositionFEN);
+            setParsedFEN(fetchedFEN);
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
 
     return { parsedFEN, setParsedFEN };
 }
