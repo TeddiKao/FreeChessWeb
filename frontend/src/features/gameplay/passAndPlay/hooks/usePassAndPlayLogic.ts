ddfc042903f82a4af8b69c0711dc44b97bb7f567
Teddi <teddikao@gmail.com>
2025-07-27 13:13:59 +0800

    Add clicked squares state management to pass and play logic hook

--- frontend/src/features/gameplay/passAndPlay/hooks/usePassAndPlayLogic.ts
+++ frontend/src/features/gameplay/passAndPlay/hooks/usePassAndPlayLogic.ts
@@ -1,5 +1,14 @@
-function usePassAndPlayLogic() {
+import useClickedSquaresState from "../../multiplayer/hooks/useClickedSquaresState";
 
+function usePassAndPlayLogic() {
+    const {
+        clickedSquare,
+        setClickedSquare,
+        prevClickedSquare,
+        setPrevClickedSquare,
+    } = useClickedSquaresState();
+
+    return { clickedSquare, setClickedSquare, prevClickedSquare, setPrevClickedSquare };
 }
 
 export default usePassAndPlayLogic;
